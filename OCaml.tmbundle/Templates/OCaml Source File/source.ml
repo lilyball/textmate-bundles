2105224fa9dd20f881df03088ae41f20f23a36c0
william <william@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-02-03 17:48:04 +0000

    Import of OCaml Templates (again).
    William D. Neumann
    
    
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@191 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ OCaml.tmbundle/Templates/OCaml Source File/source.ml
@@ -0,0 +1,14 @@
+<? $name = $_ENV["TM_NEW_FILE_BASENAME"] ?>
+<? $project = $_ENV["TM_PROJECT_FILEPATH"] ?>
+<? $company = $_ENV["TM_ORGANIZATION_NAME"] ?>
+<? $realname = chop(`niutil -readprop / /users/\$USER realname`) ?>
+(************************************************************************
+*
+*  <?= basename($_ENV["TM_NEW_FILE"]) . "\n" ?>
+*  <?= basename($project, ".tmproj") . "\n" ?>
+*
+*  Created by <?= $realname ?> on <?= date("j M Y") ?>.
+*  Copyright (c) <?= date("Y") ?> <?= $company ?>. All rights reserved.
+*
+************************************************************************)
+

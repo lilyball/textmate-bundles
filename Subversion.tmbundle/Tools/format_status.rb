1c8620ce0d71275a0b0f9ee3373ec40b01693713
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-02-16 06:24:36 +0000

    Experimentally use Jim Weirich's XML Builder to structure svn status output. I made the necessary changes to make this all work with Ruby 1.6.8. The output is not yet very pretty (except for error messages :).
    git-svn-id: http://macromates.com/svn/Bundles/trunk@287 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Subversion.tmbundle/Tools/format_status.rb
@@ -0,0 +1,46 @@
+bundle			= ENV['TM_BUNDLE_PATH']
+work_path		= ENV['WorkPath']
+pwd				= Dir.pwd
+#svn			= ENV['TM_SVN']
+#svn = "svn" if svn.nil?
+
+require (bundle + "/Tools/Builder.rb")
+
+
+mup = Builder::XmlMarkup.new(:target => STDOUT)
+mup.html {
+	mup.head {
+			mup.title("Subversion status")
+			mup.style( "@import 'file://"+bundle+"/Stylesheets/svn_style.css';", "type" => "text/css")
+	}
+
+	mup.body { 
+		mup.h1("Subversion status")
+		mup.hr
+		mup.div("class" => "command"){ mup.text!("checking "); mup.tt(work_path) }
+		STDOUT.flush
+		mup.hr
+
+		mup.table {
+			STDIN.each_line do |line|
+				
+				mup.tr {
+					if /^svn:/.match( line ).nil? then
+						match = /^(.*?)(?:\s+)(.*)\n/.match( line )
+						status	= match[1]
+						file	= match[2]
+					
+						if match.nil? then
+							mup.td(line)
+						else
+							mup.td(status)
+							mup.td { mup.a( file, "href" => 'txmt://open?url=file://' + file ) }
+						end 
+					else
+						mup.td { mup.div( line, "class" => "generic_error" ) }
+					end
+				}
+			end
+		}
+	}
+}

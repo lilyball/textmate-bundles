b479e7a81bd83468604941be4e2c458022703a84
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-02-25 00:15:32 +0000

    Separate output formatting from input parsing. Let's see if I can do this without breaking anything...
    git-svn-id: http://macromates.com/svn/Bundles/trunk@367 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Objective-C.tmbundle/bin/format_build.rb
+++ Objective-C.tmbundle/bin/format_build.rb
@@ -1,134 +1,9 @@
-bundle				= ENV['TM_BUNDLE_PATH']
+$bundle				= ENV['TM_BUNDLE_PATH']
 
-require (bundle + "/bin/Builder.rb")
-
-mup = Builder::XmlMarkup.new(:target => STDOUT)
+require ($bundle + "/bin/Builder.rb")
 
 # N.B. This code is not a model of Ruby clarity.
 # I'm still experimenting with Builder.rb.
-#
-# TODO This really ought to be factored into two parts:
-# (1) a parsing layer and (2) a separate presentation layer
-# that would be called from the parser.
-
-class << mup
-	
-	attr_accessor :current_class
-	attr_accessor :div_count
-	attr_accessor :next_div_name
-	attr_accessor :accumulated_prefix
-	
-	
-	def a_textmate!( path, line_number )
-		line_number = 1 if line_number.nil?
-		a( "href" => "txmt://open?url=file://#{path}&line=#{line_number}" ) {
-			text!( File.basename(path) + ":" + line_number.to_s )
-		}
-	end
-	
-	# accumulate content that will prefix the next div
-	def accumulate!(content)
-		@accumulated_prefix = '' unless defined?(@accumulated_prefix)
-		@accumulated_prefix << content
-	end
-	
-	# accumulative div -- block yields the div title
-	def new_div!( nclass, content = "", hide = false, &block )
-		div_id = nil
-		
-		@div_count = 0 unless defined?(@div_count)
-		@accumulated_prefix = '' unless defined?(@accumulated_prefix)
-		
-		unless nclass === @current_class
-			@div_count += 1
-			div_id = "xyz_" + @div_count.to_s
-			
-			# end the old div
-			end_div!
-			
-			# start the new div and the inner content div
-			@current_class = nclass
-			_start_tag( "div", "class" => nclass, "id" => div_id )
-
-			hide_if_hidden_style = hide ? "display: none;" : "";
-			show_if_hidden_style = hide ? "" : "display: none;";
-
-			# add show/hide toggle above the inner content div
-			div( "class" => "showhide" ) {
-								
-				a( "Hide Details", 'href' => "javascript:hideElement('#{div_id}')", 'id' => div_id + '_hide', 'style' => hide_if_hidden_style )
-				a( "Show Details", 'href' => "javascript:showElement('#{div_id}')", 'id' => div_id + '_show', 'style' => show_if_hidden_style )
-			}
-			
-			block.call
-
-			# show the user at least the title of the div
-			STDOUT.flush
-			
-			_start_tag( "div", "class" => "inner", "id" => div_id + "_c", 'style' => hide_if_hidden_style )
-			
-		end
-		
-		# output the div prefix for error/message boxes
-		unless @accumulated_prefix.empty? || @current_class == 'normal'
-			
-			@accumulated_prefix.each_line do |prefixline|
-				
-				# process any file paths
-				match = /(\/.+?):(\d+)?/.match(prefixline)
-				
-				if match.nil? then
-					text!( prefixline )
-				else
-					first = match.begin(1)
-					last = match.end(2)
-					last = match.end(1) if last.nil?
-					
-					text!(prefixline[0...(first)])
-					
-					a_textmate!( match[1], match[2] )
-					
-					text!(prefixline[last...(prefixline.length - 1)])
-				end
-				br
-			end
-			
-			@accumulated_prefix = ''
-		end
-		
-		text! content
-		
-		div_id
-	end
-
-	# wrap up any loose ends
-	def end_div!
-		if defined?( @current_class ) then
-			_end_tag("div")	# inner
-			_end_tag("div") # outer
-			@current_class = nil
-		end
-	end
-
-	def normal!(string)
-		
-		return if string.empty?
-		return if string === "\n"
-		
-		new_div!( "normal", string, false ) do
-			
-			if @next_div_name.nil?
-				title = "..."
-			else
-				title = @next_div_name
-				@next_div_name = nil
-			end
-			h2(title)
-		end
-		br
-	end
-		
-end
 
 
 SCRIPT = <<ENDSCRIPT
@@ -241,111 +116,220 @@ div.info {
 ENDSTYLE
 
 
-last_line = ""
 
-mup.html {
-	mup.head {
-		mup.title("Build With Xcode")
-		mup.style( STYLE, "type" => "text/css")
-		mup.script( SCRIPT, "language" => "JavaScript" )
-	}
+class Formatter
 
-	mup.body { 
-		mup.h1("Building With Xcode")
-		mup.hr
-		STDOUT.flush
+	def initialize
 
-		STDIN.each_line do |line|
+		@mup = Builder::XmlMarkup.new(:target => STDOUT)
+		
+		class << @mup
 			
-			# remember the current line for later
-			last_line = line
+			attr_accessor :current_class
+			attr_accessor :div_count
+			attr_accessor :next_div_name
+			attr_accessor :accumulated_prefix
 			
-			case line
-
-# TODO: xcodebuild generates lines like:
-# CompileC "file.o" Source/file.cpp normal ppc c++ com.apple.compilers.gcc.3_3 
-# We could parse these to provide build status delimited by file.
-# Then we might consider hiding the build details by default.
-
-				
-				# Handle error prefix text
-				when /^\s*((In file included from)|from)(\s*)(\/.*?):/
-					
-#					if File.exist?("#$3")
-						mup.accumulate!( line )
-#					else
-#						mup.normal!( line )
-#					end
-
-				# <path>:<line>:[column:] error description
-				when /^(.+?):(\d+):(?:\d*?:)?\s*(.*)/
-					path		= "#$1"
-					line_number = "#$2"
-					error_desc	= "#$3"
-				
-					# if the file doesn't exist, we probably snagged something that's not an error
-					if File.exist?(path)
-
-						# parse for "error", "warning", and "info" and use appropriate CSS classes					
-						cssclass = /^\s*(error|warning|info|message)/i.match(error_desc)
-					
-						cssclass = cssclass[0].downcase unless cssclass.nil?
-						cssclass = case cssclass
-							when nil
-								"error"
-							when "message"
-								"info"
-							else
-								cssclass
-						end
-										
-						mup.new_div!(cssclass) { mup.h2(cssclass) }
-					
-						mup.p {
-							mup.a_textmate!( path, line_number )
-							mup.text!(":" + error_desc)
-						}
-					else
-						mup.normal!( line )
-					end
-
-				when /^\s*(\s*)(\/.*?):/
-					
-					if File.exist?("#$2")
-						mup.accumulate!( line )
-					else
-						mup.normal!( line )
-					end
-									
-				# highlight each target name
-				when /^===(.*)===$/
-					mup.end_div!
-					mup.next_div_name = "#$1"
-				
-				else
-					mup.normal!( line )
+			def start_tag!(tag)
+				_start_tag( tag, nil )
 			end
 			
-		end
-		
-		# play sound on success/failure
-		success = /\*\* BUILD SUCCEEDED \*\*/.match(last_line)
-		success = success.nil? ? false : true
-		
-		sound = if success then
-			mup.new_div!("info") { mup.h2("Build Succeeded") }
-			'Harp.wav'
-		else
-			mup.new_div!("error") { mup.h2("Build Failed") }
-			'Whistle.wav'
-		end
-		mup.text!(last_line)
+			def end_tag!(tag)
+				_end_tag(tag)
+			end
+			
+			def a_textmate!( path, line_number )
+				line_number = 1 if line_number.nil?
+				a( "href" => "txmt://open?url=file://#{path}&line=#{line_number}" ) {
+					text!( File.basename(path) + ":" + line_number.to_s )
+				}
+			end
+			
+			# accumulate content that will prefix the next div
+			def accumulate!(content)
+				@accumulated_prefix = '' unless defined?(@accumulated_prefix)
+				@accumulated_prefix << content
+			end
+			
+			# accumulative div -- block yields the div title
+			def new_div!( nclass, content = "", hide = false, &block )
+				div_id = nil
+				
+				@div_count = 0 unless defined?(@div_count)
+				@accumulated_prefix = '' unless defined?(@accumulated_prefix)
+				
+				unless nclass === @current_class
+					@div_count += 1
+					div_id = "xyz_" + @div_count.to_s
+					
+					# end the old div
+					end_div!
+					
+					# start the new div and the inner content div
+					@current_class = nclass
+					_start_tag( "div", "class" => nclass, "id" => div_id )
 
-		%x{cd "#{bundle}"; bin/play Sounds/#{sound} &}
+					hide_if_hidden_style = hide ? "display: none;" : "";
+					show_if_hidden_style = hide ? "" : "display: none;";
+
+					# add show/hide toggle above the inner content div
+					div( "class" => "showhide" ) {
+										
+						a( "Hide Details", 'href' => "javascript:hideElement('#{div_id}')", 'id' => div_id + '_hide', 'style' => hide_if_hidden_style )
+						a( "Show Details", 'href' => "javascript:showElement('#{div_id}')", 'id' => div_id + '_show', 'style' => show_if_hidden_style )
+					}
+					
+					block.call
+
+					# show the user at least the title of the div
+					STDOUT.flush
+					
+					_start_tag( "div", "class" => "inner", "id" => div_id + "_c", 'style' => hide_if_hidden_style )
+					
+				end
+				
+				# output the div prefix for error/message boxes
+				unless @accumulated_prefix.empty? || @current_class == 'normal'
+					
+					@accumulated_prefix.each_line do |prefixline|
+						
+						# process any file paths
+						match = /(\/.+?):(\d+)?/.match(prefixline)
+						
+						if match.nil? then
+							text!( prefixline )
+						else
+							first = match.begin(1)
+							last = match.end(2)
+							last = match.end(1) if last.nil?
+							
+							text!(prefixline[0...(first)])
+							
+							a_textmate!( match[1], match[2] )
+							
+							text!(prefixline[last...(prefixline.length - 1)])
+						end
+						br
+					end
+					
+					@accumulated_prefix = ''
+				end
+				
+				text! content
+				
+				div_id
+			end
+
+			# wrap up any loose ends
+			def end_div!
+				if defined?( @current_class ) then
+					_end_tag("div")	# inner
+					_end_tag("div") # outer
+					@current_class = nil
+				end
+			end
+
+			def normal!(string)
+				
+				return if string.empty?
+				return if string === "\n"
+				
+				new_div!( "normal", string, false ) do
+					
+					if @next_div_name.nil?
+						title = "..."
+					else
+						title = @next_div_name
+						@next_div_name = nil
+					end
+					h2(title)
+				end
+				br
+			end
+				
+		end
 		
+	end
+	
+	# beginning of stream
+	def start
+		@mup.start_tag!("html")
+		@mup.head {
+			@mup.title("Build With Xcode")
+			@mup.style( STYLE, "type" => "text/css")
+			@mup.script( SCRIPT, "language" => "JavaScript" )
+		}
+		@mup.start_tag!("body")
+
+		@mup.h1("Building With Xcode")
+		@mup.hr
+		STDOUT.flush
+	end
+		
+	# end of stream
+	def complete
 		# wrap up any loose ends
-		mup.end_div!
-	}
-}
+		@mup.end_div!
+
+		@mup.end_tag!("body")
+		@mup.end_tag!("html")
+	end
+	
+	# error messages
+	# cssclass may be nil
+	def message_body( cssclass, path, line, error_desc )
+		
+		cssclass = cssclass.downcase
+		cssclass = case cssclass
+			when ""
+				"error"
+			when "message"
+				"info"
+			else
+				cssclass
+		end
+
+		@mup.new_div!(cssclass) { @mup.h2(cssclass) }
+	
+		@mup.p {
+			@mup.a_textmate!( path, line )
+			@mup.text!(":" + error_desc)
+		}
+
+	end
+	
+	# text that is part of a message to appear later
+	def message_prefix( line )
+		@mup.accumulate!( line )
+	end
+	
+	# Anything we don't parse specially
+	def build_noise( line )
+		@mup.normal!( line )
+	end
+	
+	def play_sound(sound)
+		%x{cd "#{$bundle}"; bin/play Sounds/#{sound} &}
+	end
+	
+	def target_name(name)
+		@mup.end_div!
+		@mup.next_div_name = name
+	end
+	
+	def success
+		@mup.new_div!("info") { @mup.h2("Build Succeeded") }
+		play_sound 'Harp.wav'
+
+	end
+	
+	def failure
+		@mup.new_div!("error") { @mup.h2("Build Failed") }
+		play_sound 'Whistle.wav'
+	end
+	
+end
 
 
+# On with the show
+load ($bundle + "/bin/parse_build.rb")
---
+++ Objective-C.tmbundle/bin/parse_build.rb
@@ -0,0 +1,86 @@
+
+#
+# Xcode build input parser
+#
+
+formatter = Formatter.new
+
+last_line = ""
+
+formatter.start
+
+STDIN.each_line do |line|
+	
+	# remember the current line for later
+	last_line = line
+	
+	case line
+
+# TODO: xcodebuild generates lines like:
+# CompileC "file.o" Source/file.cpp normal ppc c++ com.apple.compilers.gcc.3_3 
+# We could parse these to provide build status delimited by file.
+# Then we might consider hiding the build details by default.
+
+		
+		# Handle error prefix text
+		when /^\s*((In file included from)|from)(\s*)(\/.*?):/
+			
+#					if File.exist?("#$3")
+				formatter.message_prefix( line )
+#					else
+#						mup.normal!( line )
+#					end
+
+		# <path>:<line>:[column:] error description
+		when /^(.+?):(\d+):(?:\d*?:)?\s*(.*)/
+			path		= "#$1"
+			line_number = "#$2"
+			error_desc	= "#$3"
+		
+			# if the file doesn't exist, we probably snagged something that's not an error
+			if File.exist?(path)
+
+				# parse for "error", "warning", and "info" and use appropriate CSS classes					
+				cssclass = /^\s*(error|warning|info|message)/i.match(error_desc)
+				if cssclass.nil?
+					cssclass = "" 
+				else
+					cssclass = cssclass[1]
+				end
+
+				formatter.message_body( cssclass, path, line_number, error_desc )
+			else
+				formatter.build_noise( line )
+			end
+
+		when /^\s*(\s*)(\/.*?):/
+			
+			if File.exist?("#$2")
+				formatter.message_prefix( line )
+			else
+				formatter.build_noise( line )
+			end
+							
+		# highlight each target name
+		when /^===(.*)===$/
+			formatter.target_name( "#$1" )
+		
+		else
+			formatter.build_noise( line )
+	end
+	
+end
+
+# report success/failure
+success = /\*\* BUILD SUCCEEDED \*\*/.match(last_line)
+success = success.nil? ? false : true
+
+formatter.message_prefix(last_line)
+
+if success then
+	formatter.success
+else
+	formatter.failure
+end
+formatter.complete
+

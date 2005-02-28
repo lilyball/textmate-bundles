8ede96072a4ecd60fb492dc9cd76d4853942abc5
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-02-28 00:41:13 +0000

    • More quotes around $DOC_PATH...
    git-svn-id: http://macromates.com/svn/Bundles/trunk@387 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- ActionScript.tmbundle/Tools/get_doc_path.sh
+++ ActionScript.tmbundle/Tools/get_doc_path.sh
@@ -2,16 +2,16 @@
 
 DOC_PATH="`defaults 2>/dev/null read com.macromates.textmate.actionscript FlashDocumentationPath`"
 
-if ! [[ -d $DOC_PATH ]]; then
-	DOC_PATH=`osascript 2>/dev/null <<"EOF"
+if ! [[ -d "$DOC_PATH" ]]; then
+	DOC_PATH="`osascript 2>/dev/null <<"EOF"
 		tell app "Finder"
 			activate
 			return POSIX path of (choose folder with prompt "Select Flash Help Documentation Folder")
 		end tell
-	EOF`
+	EOF`"
 	osascript -e 'tell app "TextMate" to activate' &>/dev/null &
 
-	if ! [[ -d $DOC_PATH ]]; then exit; fi
+	if ! [[ -d "$DOC_PATH" ]]; then exit; fi
 
 	defaults write com.macromates.textmate.actionscript FlashDocumentationPath "$DOC_PATH"
 fi

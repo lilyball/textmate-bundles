a335ff823606aaa65130f5c0250d29fe8274fb45
eric <eric@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-03-13 01:27:38 +0000

    First draft of a hack that relies on the "find entities" script and formats the result as an HTML popup list of functions/special-items. Selecting an item jumps to that spot in the file.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@467 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Defaults.tmbundle/bin/nav2htmlpopup.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+# Hideous hack to take to output of entitynav.py and format it as a HTML popup.
+# (c) 2005. Eric Hsu.
+
+my $head = <<END; 
+<html>
+<head>
+<!--
+This file retrieved from the JS-Examples archives
+http://www.js-x.com
+1000s of free ready to use scripts, tutorials, forums.
+Author: James  - 0
+-->
+</head>
+<body onload="window.resizeTo(800,80);"><script language=javascript>
+<!-- 
+function jumpTo() { 
+var new_local = document.jump.local.value;
+top.location.replace(new_local);
+}
+-->
+</script>
+<form name="jump">
+<select name="local" onChange="jumpTo(); return false;">
+END
+
+my $foot = <<END;
+</select>
+</form>
+</body>
+</html>
+END
+
+my $url = 'txmt://open?url=file://' . $ENV{"TM_FILEPATH"};
+my $results="<option value=\"$url\">Special Items for " . $ENV{"TM_FILEPATH"} . "</option>";; 
+my ($line,$title);
+
+while (<>) {
+	($line,$title)=/^(.*?)\:(.*)$/;
+	$title = space_entitized($title);
+	$results .= "<option value=\"$url" . "&amp;line=" . $line . "\">$title</option>";
+}
+
+print $head . $results . $foot;
+
+sub space_entitized {
+	my $in = shift;
+	
+	$in =~ s/\t/    /g;
+	$in =~ s/\ /&nbsp;/g;
+	
+	return $in;
+}

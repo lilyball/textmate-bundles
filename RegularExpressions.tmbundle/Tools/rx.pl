a580f2da88e948d24251fff9e800dd7e053abe6e
eric <eric@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-04-04 21:30:20 +0000

    This is a Regular Expression tester. You copy the regexp to the clipboard, then highlight the text you want to check. Then it produces HTML output which color codes each subcapture.
    There might be a nicer way to grab the regexp, like maybe using Cocoadialog, but I just wanted to get this out there.
    
    I've also included David Lee's Ruby version which I can't get to work on my machine.
    
    
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@558 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ RegularExpressions.tmbundle/Tools/rx.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+# (c) 2005, Eric Hsu
+# Take a regular expression from the clipboard (no / brackets). Show the matches in the current document and perhaps show a unified diff of the result.
+# Quick hack would be always do a replace with nothing, and then to diff the result. This would only catch line changes, unfortunately.
+
+#Instead, let's just do a simple find of all the $1, $2, ... $9, and record their start/end positions. Then we'll insert special fixed-length codes everything. Then we'll go through and insert <div class="m1"> and </div> all around. We'll then make the class CSS cascades so you can see overlap, e.g. ((t{2})(s{3})).
+
+
+my $bundle = $ENV{'TM_BUNDLE_PATH'};
+my $css= $bundle . "/Tools";
+
+my $regexp = `pbpaste`;
+chomp($regexp);
+my @text=<DATA>;
+$_=join("",@text);
+
+my $results;
+my $textpieces;
+
+while (/$regexp/g) {
+
+	# The @- (@LAST_MATCH_START) array holds the offsets of the beginnings of any submatches, 
+	#  and @+ (@LAST_MATCH_END) holds the offsets of the ends.
+	#  @-[0] is the whole regexp match start.
+
+	my @match_start=@-;
+	my @match_end=@+;
+	my @match_edges=sort (@match_start, @match_end);
+
+	my $start=0;
+	my $piece;
+	my $match_no;
+	my $end;
+
+# blow up into escaped little pieces between the match edges.
+	foreach $end (@match_edges) {
+		$piece=substr($_, $start, ($end-$start));
+		$piece =~ s/&/&amp;/g; # silly; make sure this comes before the next two.
+		$piece =~ s/</&lt;/g;
+		$piece =~ s/>/&gt;/g;
+
+		$textpieces{$start}=$piece;
+		$start=$end;
+	}
+
+	# as a hack, we'll insert a header before line N by making a hash entry at N-2^{$match_no+1} and a footer at N+2^{$match_no+1}; this way, later matches will be nested within earlier matches. 
+
+	foreach $match_no (0..($#match_start)) {
+		$start= shift @match_start;
+		$end= shift @match_end;
+		$textpieces{$start-(2^($match_no))}= "<div class=\"m$match_no\">";		
+		$textpieces{$end+(2^($match_no))}= "</div>";		
+	}
+}
+
+foreach (sort {$a <=> $b} keys %textpieces) {
+	$results .= $textpieces{$_};
+}
+
+$regexp =~  s/&/&amp;/g; 
+$regexp =~  s/</&lt;/g;
+$regexp =~  s/>/&gt;/g;
+
+print <<END;
+<html><head>
+<link rel="stylesheet" type="text/css" href="file://$css/regexp.css" />
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8"></head><body>
+<h3>Results for $regexp</h3>
+<tt>$results</tt>
+</head><body>
+END
+
+__DATA__
+# BibTeX files have a default view of citation with raw bibtex appended.
+# You can just ask for the citation. 
+
+
---
+++ RegularExpressions.tmbundle/Tools/rx_helper.rb
@@ -0,0 +1,114 @@
+#!/usr/local/bin/ruby
+
+# REGEXP HELPER
+# David Lee (c) 2005. 
+
+# License terms: undecided. But i take no responsibility for anything. Pretend
+# you're reading the disclaimer part of a Microsoft EULA, that's how little I
+# expect to be held accountable for anything.
+
+=begin rdoc
+
+We expect to slurp everything from stdin through a pipe, though you can use it
+as a command line tool. First line of input is expected to be a regular
+expression. If we're doing matching, leave out the /slashes/, eg:
+
+(.+)z
+
+If we're doing substitution, use slashes to identify the expression as such:
+
+/old/new/ 
+
+Every line after the first is match fodder, used to test the regexp.
+
+To set the sucker up as a TextMate command:
+
+- save this somewhere and chmod +x it so it can run
+- change the shebang to point to your local install of ruby 
+  (type 'which ruby' to find it)
+- create a new Command in Textmate (alt-ctrl-cmd-c)
+- add the action /path_to_script/rx_helper.rb
+- it should operate on the selection and display output text in a tooltip
+- bind to the keybinding of your choice (i like alt-ctrl-cmd-x)
+- enjoy!
+
+David Lee
+=end
+
+class RX_Helper
+  def initialize
+    @tests     = []
+    @successes = []
+    @failures  = []
+    while input_txt = gets
+      if !@rx_type 
+        prepare_rx(input_txt) 
+      else
+        @tests << input_txt 
+      end
+    end
+    puts @rx_match
+    process_rx
+    
+    puts "Successful Matches:\n===================\n" unless !@successes[0]
+    @successes.each{|x| puts x};
+    
+    puts "\nFailed Matches:\n===============\n"       unless !@failures[0]
+    @failures.each{|x| puts x}; puts "\n"
+
+  end
+  
+  def prepare_rx(txt) # set up regexp strings
+    if md = txt.match(/\/([^\/]+)\/([^\/]+)\//)
+      @rx_type, @rx_match, @rx_subst = 'substitution', md[1], md[2]
+    else
+      @rx_type, @rx_match            = 'match', txt.chomp!
+    end
+  end
+  
+  def process_rx
+    case @rx_type
+    when 'match'
+      while line = @tests.shift()
+        begin
+        if match = line.match(@rx_match)
+          @successes.push line.chop + ' : matched expression'
+          n = 0
+  				while match[n += 1]
+  					@successes.push "\tmatch number [" + n.to_s + '] : ' + match[n]
+  				end
+  			else
+  			  @failures.push line.chop + ' : failed'
+  			end
+  			rescue RegexpError
+  			  puts 'regex invalid. ' + $!
+  			  exit
+  			end
+      end
+    when 'substitution'
+      puts 'no match' unless @rx_match
+      puts 'no subst' unless @rx_subst 
+        
+      while line = @tests.pop()
+        subd = line
+        begin
+          match = line.sub(@rx_match, @rx_subst)
+          if match != line
+            @successes.push line.chop + ' : matched expression' +
+              "\n\t=> " + match.to_s
+          else
+            @failures.push  line.chop + ' : failed'
+          end
+  			rescue RegexpError
+  			  puts 'regex invalid. ' + $!
+  			  exit
+        end
+      end
+    else
+    
+    end
+  end
+  
+end
+
+RX_Helper.new

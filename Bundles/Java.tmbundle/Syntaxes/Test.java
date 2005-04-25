ae870c7c56f25eae2e4e366708f89e4295fdfff6
blalor <blalor@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-04-25 21:26:17 +0000

    • update Java and XML bundles to take advantage of new syntax/scope system • added theme settings for same
    git-svn-id: http://macromates.com/svn/Bundles/trunk@728 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/Java.tmbundle/Syntaxes/Test.java
@@ -0,0 +1,36 @@
+package foo;
+
+/**
+ * <<Class summary>>
+ *
+ * @author   &lt;&gt;
+ * @version $Rev$
+ */
+public final class Test {
+    /** Version control identifier strings. */
+    public static final String[] RCS_ID = {
+        "$URL: http://macromates.com/svn/Bundles/trunk/Bundles/Java.tmbundle/Templates/Java%20Class/class-insert.java $",
+        "$Id$",
+    };
+    
+	// {{{ Test constructor
+    /**
+     * 
+     */
+    public Test() {
+        boolean b = true;
+    }
+	// }}}
+	
+	// {{{ getFoo
+	/**
+	 * Returns foo.
+	 *
+	 * @return the value of Foo.
+	 */
+	public String getFoo(boolean baz) {
+	    
+	}
+	// }}}
+	
+}

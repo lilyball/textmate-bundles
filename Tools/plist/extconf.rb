35f74890f274d2d81811b7d4df60af6c269aa80e
kevin <kevin@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-04-28 23:41:51 +0000

    Make extconf.rb work right under ruby 1.6
    git-svn-id: http://macromates.com/svn/Bundles/trunk@799 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/plist/extconf.rb
+++ Tools/plist/extconf.rb
@@ -1,4 +1,5 @@
 require 'mkmf'
 $CFLAGS.gsub!("-arch i386", "")
-$LIBS += ' -framework CoreFoundation'
+#$LIBS += ' -framework CoreFoundation'
+$LDFLAGS += ' -framework CoreFoundation'
 create_makefile("plist")

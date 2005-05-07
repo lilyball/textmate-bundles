2ec8e4c58281d481763bbdf1b55ecbbb04592279
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-05-07 06:26:45 +0000

    • Quote more characters in quote_filename_for_shell. • Do not send raw filenames to the output without HTML-quoting them first. Converted svn_commit to Builder.rb as a side effect.
    Commit now survives a commit of the entire XHTML bundle, with random single and double quotes and 8-bit characters added to some of the filenames for good measure.
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@865 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Tools/svn_commit.rb
+++ Bundles/Subversion.tmbundle/Tools/svn_commit.rb
@@ -8,81 +8,94 @@ bundle			= ENV['TM_BUNDLE_PATH']
 CURRENT_DIR		= Dir.pwd + "/"
 
 require (bundle + '/Tools/shelltokenize.rb')
+require (bundle + "/Tools/Builder.rb")
 
-puts "<html>
-<head>
-<title>Subversion status</title>
-<style type=\"text/css\">
-  @import 'file://#{bundle}/Stylesheets/svn_style.css';
-</style>
-</head>
+mup = Builder::XmlMarkup.new(:target => STDOUT)
 
-<body>"
+mup.html {
+	mup.head {
+			mup.title("Subversion status")
+			mup.style( "@import 'file://"+bundle+"/Stylesheets/svn_style.css';", "type" => "text/css")
+	}
 
-puts "<h1>Subversion Commit</h1>"
-puts "<hr>"
+	mup.body { 
+		mup.h1("Subversion Commit")
+		STDOUT.flush
+		mup.hr
 
-# Ignore files without changes
+		# Ignore files without changes
 #puts TextMate::selected_paths_for_shell
-status_command = %Q{"#{svn}" status #{TextMate::selected_paths_for_shell}}
+		status_command = %Q{"#{svn}" status #{TextMate::selected_paths_for_shell}}
 #puts status_command
-status_output = %x{#{status_command}}
+		status_output = %x{#{status_command}}
 #puts status_output
-paths = status_output.scan(/^(.)....(\s+)(.*)\n/)
+		paths = status_output.scan(/^(.)....(\s+)(.*)\n/)
 
 
-def matches_to_paths(matches)
-	paths = matches.collect {|m| m[2] }
-	paths.collect{|path| path.sub(/^#{CURRENT_DIR}/, "") }
-end
+		def matches_to_paths(matches)
+			paths = matches.collect {|m| m[2] }
+			paths.collect{|path| path.sub(/^#{CURRENT_DIR}/, "") }
+		end
 
-# Ignore files with '?', but report them to the user
-unknown_paths = paths.select { |m| m[0] == '?' }
+		# Ignore files with '?', but report them to the user
+		unknown_paths = paths.select { |m| m[0] == '?' }
 
-if unknown_paths and unknown_paths.size > 0 then
-	puts "These files are not added to the repository, and so will not be committed:<p><ul><li>#{matches_to_paths(unknown_paths).join("</li><li>")}</li></ul><p>"
-end
+		if unknown_paths and unknown_paths.size > 0 then
+			mup.div( "class" => "info" ) {
+				mup.text! "These files are not added to the repository, and so will not be committed:"		
+				mup.ul{ matches_to_paths(unknown_paths).each{ |path| mup.li(path) } }
+			}
+		end
 
-# Fail if we have conflicts -- svn commit will fail, so let's
-# error out before the user gets too involved in the commit
-conflict_paths = paths.select { |m| m[0] == 'C' }
+		# Fail if we have conflicts -- svn commit will fail, so let's
+		# error out before the user gets too involved in the commit
+		conflict_paths = paths.select { |m| m[0] == 'C' }
 
-if conflict_paths and conflict_paths.size > 0 then
-	puts "Cannot continue; there are merge conflicts in files:<p><ul><li>#{matches_to_paths(conflict_paths).join("</li><li>")}</li></ul><p>"
-	puts "Canceled."
-	exit -1
-end
+		if conflict_paths and conflict_paths.size > 0 then
+			mup.div( "class" => "error" ) {
+				mup.text! "Cannot continue; there are merge conflicts in files:"		
+				mup.ul{ matches_to_paths(conflict_paths).each{ |path| mup.li(path) } }
+				mup.text! "Canceled."
+			}	
+			exit -1
+		end
 
-# Remove the unknown paths from the commit
-commit_matches = paths - unknown_paths
+		# Remove the unknown paths from the commit
+		commit_matches = paths - unknown_paths
 
-if commit_matches.nil? or commit_matches.size == 0
-	puts "File(s) not modified; nothing to commit."
-	exit 0
-end
+		if commit_matches.nil? or commit_matches.size == 0
+			mup.div( "class" => "info" ) {
+				mup.text! "File(s) not modified; nothing to commit."
+				mup.ul{ matches_to_paths(unknown_paths).each{ |path| mup.li(path) } }
+			}
+			exit 0
+		end
 
-STDOUT.flush
+		STDOUT.flush
 
-commit_paths_array = matches_to_paths(commit_matches)
+		commit_paths_array = matches_to_paths(commit_matches)
 
-commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
+		commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
 
-#puts commit_path_text
-commit_args = %x{"#{commit_tool}" #{commit_path_text}}
+		commit_args = %x{"#{commit_tool}" #{commit_path_text}}
 
-status = $CHILD_STATUS
-if status != 0
-	puts "Canceled (#{status >> 8})."
-	exit -1
-end
+		status = $CHILD_STATUS
+		if status != 0
+			mup.div( "class" => "error" ) {
+				mup.text! "Canceled (#{status >> 8})."
+			}	
+			exit -1
+		end
 
-puts "<div class=\"command\">"
-puts %Q{<strong>#{svn} commit </strong>#{commit_args.gsub("' '", "'<br>'").gsub("'\"'\"'", "'")}}
-puts "</div>"
-puts "<p> <pre>"
-STDOUT.flush
+		mup.div("class" => "command"){ mup.strong(%Q{#{svn} commit}); mup.text!(commit_args) }
+		
+		mup.pre {
+			STDOUT.flush
 
-fork { exec "#{svn} commit #{commit_args}" }
-Process.wait
+			IO.popen("#{svn} commit #{commit_args}", "r+") do |pipe|
+				pipe.each {|line| mup.text! line }
+			end
+		}
+	}
+}
 
-puts "</pre></body></html>"

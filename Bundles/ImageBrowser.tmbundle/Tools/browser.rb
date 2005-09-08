f125899be4d3435a30899627960e154440ab7bce
rich <rich@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-09-08 08:19:14 +0000

    Using new TM_BUNDLE_SUPPORT variable and directory structure. Should be backwards compatible (falls back to TM_BUNDLE_PATH if TM_BUNDLE_SUPPORT is unavailable). Will hopefully send all changes this time :-P
    git-svn-id: http://macromates.com/svn/Bundles/trunk@1714 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/ImageBrowser.tmbundle/Tools/browser.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-PROJECT_ROOT = ARGV[0] # Where to browse for images
-RESOURCES_DIR = ARGV[1] + "/Resources" # Where to find ImageBrowser's images
-require ARGV[1] + "/Tools/image_size"
-require 'erb'
-
-erb = ERB.new(IO.read(ARGV[1] + '/Resources/browser.rhtml'))
-erb.run

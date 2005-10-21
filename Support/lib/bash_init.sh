18ff1d6a04c10d57261995d1e2b85150f066c9a7
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-10-21 03:10:22 +0000

    • only source standard profile/login files when there's no user (TextMate specific) bash_init.sh • no longer but bundle_directory/bin in the PATH (since support/bin is what you want to use) • added a pre (bash) function to escape html, e.g.: “cat «file»|pre” will wrap the file in <pre>…</pre> and escape <. >, and & characters in the output
    git-svn-id: http://macromates.com/svn/Bundles/trunk@1972 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/bash_init.sh
+++ Support/lib/bash_init.sh
@@ -1,13 +1,19 @@
 # This script simulates the behavior of a login shell
 # This is done to give the user a full PATH
 
-# First read the system-wide profile
-[ -f /etc/profile ] && . /etc/profile               &>/dev/null
+: ${TM_BASH_INIT:=$HOME/Library/Application Support/TextMate/bash_init.sh}
 
-# Now find the first local profile, just like a normal login shell
-if   [ -f ~/.bash_profile ]; then . ~/.bash_profile &>/dev/null
-elif [ -f ~/.bash_login ];   then . ~/.bash_login   &>/dev/null
-elif [ -f ~/.profile ];      then . ~/.profile      &>/dev/null
+if [ ! -f "$TM_BASH_INIT" ]; then
+
+	# First read the system-wide profile
+	[ -f /etc/profile ] && . /etc/profile               &>/dev/null
+
+	# Now find the first local profile, just like a normal login shell
+	if   [ -f ~/.bash_profile ]; then . ~/.bash_profile &>/dev/null
+	elif [ -f ~/.bash_login ];   then . ~/.bash_login   &>/dev/null
+	elif [ -f ~/.profile ];      then . ~/.profile      &>/dev/null
+	fi
+
 fi
 
 if [[ -d "$TM_SUPPORT_PATH/bin" ]]; then
@@ -17,27 +23,33 @@ if [[ -d "$TM_SUPPORT_PATH/bin" ]]; then
 	fi
 fi
 
-if [[ -d "$TM_BUNDLE_PATH" && -d "$TM_BUNDLE_PATH/bin" ]]; then
-   PATH="$TM_BUNDLE_PATH/bin:$PATH"
-fi
-
 if [[ -d "$TM_BUNDLE_SUPPORT" && -d "$TM_BUNDLE_SUPPORT/bin" ]]; then
    PATH="$TM_BUNDLE_SUPPORT/bin:$PATH"
 fi
 
 export PATH
 
-if [[ -e "$HOME/Library/Application Support/TextMate/bash_init.sh" ]]; then
-	. "$HOME/Library/Application Support/TextMate/bash_init.sh"
+if [[ -f "$TM_BASH_INIT" ]]; then
+	. "$TM_BASH_INIT"
 fi
 
+# force TM to refresh current file and project drawer
 rescan_project () {
-	# force TM to refresh current file and project drawer
 	osascript &>/dev/null \
 	   -e 'tell app "SystemUIServer" to activate' \
 	   -e 'tell app "TextMate" to activate' &
 }
 
+# use this as a filter (|pre) when you want 
+# raw output to show as such in the HTML output
+pre () {
+	echo -n '<pre>'
+	perl -pe 's/&/&amp;/g; s/</&lt;/g; s/>/&gt;/g'
+	echo '</pre>'
+}
+
+# this will check for the presence of a command and
+# prints an (HTML) error + exists if it's not there
 require_cmd () {
 	if ! type -p "$1" >/dev/null; then
 		cat <<EOF

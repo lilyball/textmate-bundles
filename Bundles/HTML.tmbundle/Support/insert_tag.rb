169e5edcdd18aad3c920ec5e43a037136e4cc351
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-10-21 02:35:47 +0000

    • tags w/o closer like img, meta etc. now have a tab stop before /> — br and hr excluded
    git-svn-id: http://macromates.com/svn/Bundles/trunk@1971 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/HTML.tmbundle/Support/insert_tag.rb
+++ Bundles/HTML.tmbundle/Support/insert_tag.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 # single tags
-single = /^img|br|hr|meta|link|input|base|area|col|frame|param$/i
+single_no_arguments = /^br|hr$/i
+single = /^img|meta|link|input|base|area|col|frame|param$/i
 
 # if we have something selected, turn it into a tag,
 # if we are just in a word, use that, otherwise - a blank
@@ -9,10 +10,12 @@ single = /^img|br|hr|meta|link|input|base|area|col|frame|param$/i
 if(ENV['TM_SELECTED_TEXT'].to_s.strip != '')
 	tag = ENV['TM_SELECTED_TEXT'].strip[/^\S+/]
 	if(single.match(tag))
-		print("<#{tag}#{ENV['TM_SELECTED_TEXT'].strip[/^\S+(\s+.*)/, 1]} />$0")
+		print("<#{tag}#{ENV['TM_SELECTED_TEXT'].strip[/^\S+(\s+.*)/, 1]} $1/>")
+	elsif(single_no_arguments.match(tag))
+		print("<#{tag}#{ENV['TM_SELECTED_TEXT'].strip[/^\S+(\s+.*)/, 1]} />")
 	else
-		print("<#{tag}#{ENV['TM_SELECTED_TEXT'].strip[/^\S+(\s+.*)/, 1]}>$1</#{tag}>$0")
+		print("<#{tag}#{ENV['TM_SELECTED_TEXT'].strip[/^\S+(\s+.*)/, 1]}>$1</#{tag}>")
 	end
 else
-	print("#{ENV['TM_SELECTED_TEXT']}<${1:p}>$2</${1/\\s.*//}>$0")
+	print("#{ENV['TM_SELECTED_TEXT']}<${1:p}>$2</${1/\\s.*//}>")
 end

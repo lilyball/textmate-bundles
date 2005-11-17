8df9aaa556cc35dbd9c86607e36e5e401933d6f9
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-11-17 23:41:01 +0000

    • removed two bundles which I consider replaced by “default” stuff
    For UnComment, it's the Toggle Comment script (Source bundle), which though not having as many features as the original script, works with scopes and thus support mixed documents and distributed configuration of comment characters.
    
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2180 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Defaults.tmbundle/bin/special_items/exceptions.py
+++
@@ -1,7 +0,0 @@
-"""exceptions for special_items module"""
-
-class UsageException(Exception): pass
-class HandlerException(Exception): pass
-class HandlerNoEntitiesException(HandlerException): pass
-class SortException(Exception): pass
-class ParseException(Exception): pass

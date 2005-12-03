6b9561e48ef2797b891aea5beec6024b8b47bbcd
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-12-03 07:52:04 +0000

    • obj-c class pattern was aggregated incorrectly
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2381 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Objective-C.tmbundle/Support/Collect Cocoa Definitions.sh
+++ Bundles/Objective-C.tmbundle/Support/Collect Cocoa Definitions.sh
@@ -2,11 +2,10 @@
 # Cocoa Functions
 find /System/Library/Frameworks/{AppKit,Foundation}.framework -name \*.h -exec grep '^[A-Z][A-Z_]* [^;]* \**NS[A-Z][A-Za-z]* *(' '{}' \;|perl -pe 's/.*?\s\*?(NS\w+)\s*\(.*/$1/'|sort|uniq|./list_to_regexp.rb >/tmp/functions.txt
 
-# Cocoa Classes
-find /System/Library/Frameworks/{AppKit,Foundation}.framework -name \*.h -exec grep '@interface NS[A-Za-z]*' '{}' \;|perl -pe 's/.*?(NS[A-Za-z]+).*/$1/'|sort|uniq|./list_to_regexp.rb >/tmp/classes.txt
-
-# Cocoa Protocols
-find /System/Library/Frameworks/{AppKit,Foundation}.framework -name \*.h -exec grep '@protocol NS[A-Za-z]*' '{}' \;|perl -pe 's/.*?(NS[A-Za-z]+).*/$1/'|sort|uniq|./list_to_regexp.rb >>/tmp/classes.txt
+# Cocoa Protocols Classes
+{ find /System/Library/Frameworks/{AppKit,Foundation}.framework -name \*.h -exec grep '@interface NS[A-Za-z]*' '{}' \;|perl -pe 's/.*?(NS[A-Za-z]+).*/$1/';
+  find /System/Library/Frameworks/{AppKit,Foundation}.framework -name \*.h -exec grep '@protocol NS[A-Za-z]*' '{}' \;|perl -pe 's/.*?(NS[A-Za-z]+).*/$1/';
+}|sort|uniq|./list_to_regexp.rb >/tmp/classes.txt
 
 # Cocoa Types
 find /System/Library/Frameworks/{AppKit,Foundation}.framework -name \*.h -exec grep 'typedef .* _*NS[A-Za-z]*' '{}' \;|perl -pe 's/.*?(NS[A-Za-z]+);.*/$1/'|perl -pe 's/typedef .*? _?(NS[A-Za-z0-9]+) \{.*/$1/'|grep -v typedef|sort|uniq|./list_to_regexp.rb >/tmp/types.txt

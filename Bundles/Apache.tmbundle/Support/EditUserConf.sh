05c410c29edb8a4409922c27be0dee26d9a243d5
simon <simon@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2005-12-04 21:55:58 +0000

    Added Keychain support for running apachectl from within TM.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2395 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/Apache.tmbundle/Support/EditUserConf.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+SEARCH_DIR="/private/etc/httpd/users"
+SEARCH_FILE_EXT="conf"
+
+getDirContentsList()
+{
+	#Makes a list of all the files found in the specified directory, then assigns it to the variable $REPLY.
+	local fileList=""	
+	for file in "$1"/*
+	do
+		if [ -f "$file" ]
+			then
+			# Process files only..
+			local ext=${file##*.}
+			if [ "$ext" == "$SEARCH_FILE_EXT" ]
+				then
+				# Extract the name and add it to the list.
+				local curFile=`basename "$file"`
+				#echo $curFile
+				fileList=`expr "$fileList\"$curFile\"$2"` 
+			fi
+		fi  
+	done	
+	#To return a variable you need to specify a global variable to read.
+	REPLY=${fileList%$2*}
+}
+	
+getDirContentsList $SEARCH_DIR ","
+	
+if [ "$REPLY" == "" ]
+	then
+	#There's nothing to pick from so exit
+	exit 0;
+fi
+	
+selectedFile=`exec osascript <<EOF | tr "\r" "\n"
+	tell app "SystemUIServer"
+	   	activate	    
+		set file_list to { $REPLY }
+		set open_list to (choose from list file_list with prompt "Select the user.conf files you wish to open")
+	end tell
+EOF`
+	
+if [ "$selectedFile" == "false" ]
+	then
+	open -a TextMate
+	exit 0;
+fi
+
+open -a TextMate "$SEARCH_DIR/$selectedFile";
+
+exit 0;

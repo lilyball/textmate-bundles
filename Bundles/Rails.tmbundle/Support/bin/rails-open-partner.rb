f73d877f992b61b9c0d43263889ba9a650d91539
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-02-19 09:55:26 +0000

    • removed the use of the find shell command in favor of collect with File.exists? — it doesn't really seem faster for me, but it ought to be
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2717 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Rails.tmbundle/Support/bin/rails-open-partner.rb
+++ Bundles/Rails.tmbundle/Support/bin/rails-open-partner.rb
@@ -7,7 +7,7 @@ parents = [case path
   when %r{/test/functional/} then 'app/controllers'
   when %r(/test/|(_test\.rb$)) then %w(app lib)
 end].flatten
- 
+
 file_name = File.basename path
 if parents.any? { |parent| parent =~ %r{test(/|$)} }
   file_name.gsub!(/\.rb$/, '_test.rb')
@@ -18,10 +18,9 @@ end
 def shell_escape (str)
   "'" + str.gsub(/'/, "'\\\\''") + "'"
 end
- 
+
 find_parents = parents.collect { |parent| File.join(ENV['TM_PROJECT_DIRECTORY'], parent) }
-find_parents = find_parents.find_all { |file| File.exists?(file) }
- 
-matches = `find #{find_parents.collect { |str| shell_escape str } * ' '} -name #{shell_escape file_name} -maxdepth 1`.strip.split("\n").collect {|match| match.strip} unless find_parents.empty?
+candidates = find_parents.collect { |dir| File.join(dir, file_name) }
+matches = candidates.find_all { |file| File.exists?(file) }
 
 `mate #{matches.collect { |str| shell_escape str } * ' '} &>/dev/null &` unless matches.empty?

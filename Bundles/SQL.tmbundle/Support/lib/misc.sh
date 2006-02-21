f3bcdbb9a2be3c45f3d4f28987f84d7bd9092624
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-02-21 22:20:37 +0000

    • add helper function to determine users database. Currently it has a (clipped) Make Default button, but it’s non-functioning ATM (I figured it should write the choice to the defaults database) • make the Table Browser and SQL Query commands use this new function (and remove the PostgreSQL command) • update documentation
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2739 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/SQL.tmbundle/Support/lib/misc.sh
@@ -0,0 +1,24 @@
+database_choice () {
+
+	if [[ -n "$TM_DB_SERVER" ]]; then
+		echo -n "$TM_DB_SERVER"
+	elif ! type -p mysql >/dev/null && type -p psql8 >/dev/null; then
+		echo -n "postgresql"
+	elif type -p mysql >/dev/null && ! type -p psql8 >/dev/null; then
+		echo -n "mysql"
+	else
+		res=$(CocoaDialog dropdown --title "Select Database" \
+		 	--text "Which database should be used?" \
+			--button1 Okay \
+			--button2 Cancel \
+		   --button3 "Make Default" \
+			--string-output \
+			--no-newline \
+			--items mysql postgresql)
+
+		if [[ "${res:0:4}" == "Okay" ]]; then
+			echo -n "${res:5}"
+		fi
+	fi
+
+}

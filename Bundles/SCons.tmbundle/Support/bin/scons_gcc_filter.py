656617eb888a51d5fd734c2120f01b79ce1c82ea
mitch <mitch@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-03-03 18:43:39 +0000

    Added basic support for building SCons projects.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2814 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/SCons.tmbundle/Support/bin/scons_gcc_filter.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+"""Filter for scons output, adds TextMate hyperlinks for gcc error msgs."""
+
+import re
+import sys
+import os
+import fnmatch
+import cgi
+
+class SConsGCCFilter(object):
+    stlIgnore = ["iosfwd", "streambuf", "streambuf_iterator.h", "basic_ios.h",
+                 "basic_string.h", "istream", "istream.tcc", 
+                 "ostream", "ostream.tcc",
+                 "fstream.tcc",
+                 ]
+                 
+    def __init__(self, currdir=None):
+        self._content = ""
+        self._index = 0
+        if currdir is None:
+            currdir = os.getcwd()
+        self._currdir = currdir
+        
+        self._ignorePatterns = []
+        self._initIgnorePatterns()
+        self._errExpr = re.compile(
+            r"^(?P<preamble>"
+            r"(?P<pathname>[/A-Za-z0-9_.+]*):"
+            r"(?P<line_num>\d+):\s*"
+            r"(?P<errwarn>(error|warning)):"
+            r")"
+            r"(?P<error_msg>.*(\n  .*)*)",
+            re.MULTILINE)
+            
+    def _initIgnorePatterns(self):
+        """Initialize with settings from TM_IGNORE_WARNINGS, if any."""
+        value = os.environ.get("TM_IGNORE_WARNINGS")
+        if value is not None:
+            # Better hope nobody uses a filename containing ':'.
+            patterns = value.split(":")
+            for p in patterns:
+                self._addIgnorePath(p)
+        
+    def _addIgnorePath(self, pattern):
+        """Ignore (or rather, don't highlight) warnings from files 
+        whose names match the glob pattern."""
+        print "<i>Will not highlight warnings in %r</i><br/>" % cgi.escape(pattern)
+        self._ignorePatterns.append(pattern)
+             
+    def _isIgnoredPath(self, pathname):
+        for pattern in self._ignorePatterns:
+            if fnmatch.fnmatch(pathname, pattern):
+                return True
+        return False
+
+    def _isIgnoredSTLWarning(self, filename):
+        # GCC 3.3 -Weffc++ 
+        # produces some warnings because of code generated from
+        # STL.  Uncomment the return statement to pass-through these warnings
+        #return False
+        result = ((filename in self.stlIgnore) or 
+                  fnmatch.fnmatch(filename, "stl_*.h"))
+        return result
+        
+    def feed(self, newContent):
+        self._content += newContent
+        
+    def _worthHighlighting(self, pathname, isError):
+        filename = os.path.basename(pathname)
+        result = (isError or not 
+                  (self._isIgnoredPath(pathname) or 
+                   self._isIgnoredSTLWarning(filename)))
+        return result
+        
+    def filter(self, consumeAll=False):
+        resultList = []
+        
+        currdir = self._currdir
+        content = self._content
+        search = self._errExpr.search
+        while True:
+            match = search(content)
+            if match is None:
+                if consumeAll:
+                    resultList.append(content)
+                    self._content = ""
+                else:
+                    # Error msgs start near beginning of a line, so flush
+                    # all unmatched, complete lines.
+                    iLine = content.rfind("\n")
+                    if (iLine >= 0):
+                        resultList.append(content[:iLine+1])
+                        content = content[iLine+2:]
+                    # Save the rest for later consumption.
+                    self._content = content
+                break
+            else:
+                resultList.append(content[:match.start()])
+                pathname = match.group("pathname")
+                isError = (match.group("errwarn") == "error")
+                if self._worthHighlighting(pathname, isError):
+                    msg = match.group("error_msg")
+                    lineNum = match.group("line_num")
+                    errorURL = ("txmt://open?url=file://%s/%s&line=%s" %
+                                (currdir, pathname, lineNum))
+                    resultList.append('<a href="%s">' % errorURL)
+                    resultList.append(match.group("preamble"))
+                    resultList.append('</a>')
+                    resultList.append(msg)
+                    resultList.append("<br/>")
+                else:
+                    resultList.append(match.group(0))
+            
+                content = content[match.end()+1:]
+                
+        result = "".join(resultList).replace("\n", "<br/>\n")
+        return result
+    
+def main():
+    f = SConsGCCFilter()
+    f.feed(sys.stdin.read())
+    print f.filter(consumeAll=True)
+
+if __name__ == "__main__":
+    main()
---
+++ Bundles/SCons.tmbundle/Support/bin/scons_html_wrapper.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""Runs scons and wraps its output as HTML."""
+
+import sys
+import os
+import select
+from scons_gcc_filter import SConsGCCFilter
+import cgi
+
+def findSConstructDir():
+    """Search upward from the current dir for the top-level SConstruct dir."""
+    result = None
+    d = os.getcwd()
+    while (d != "/") and not result:
+        candidate = os.path.join(d, "SConstruct")
+        if os.path.exists(candidate) and os.path.isfile(candidate):
+            result = d
+        d = os.path.dirname(d)
+    return result or os.getcwd()
+
+def write(s):
+    """Write a string to stdout, flushing immediately."""
+    if s:
+        sys.stdout.write(s)
+        sys.stdout.flush()
+    
+def filterOutput(cmd):
+    """Run a shell cmd, reformatting output as HTML."""
+    theFilter = SConsGCCFilter(findSConstructDir())
+    
+    childIn, childOut, childErr = os.popen3(cmd, 0)
+    infd = childIn.fileno()
+    outfd = childOut.fileno()
+    errfd = childErr.fileno()
+    inSet = [outfd, errfd]
+    errSet = [infd, outfd, errfd]
+    
+    while True:
+        ins, outs, errs = select.select(inSet, [], errSet)
+        if errs:
+            print "<b>Error descriptors: <i>%s</i><b>" % repr(errs)
+            for item in errs:
+                errSet.remove(item)
+            if not errSet:
+                break
+        for fd in ins:
+            content = os.read(fd, 65536)
+            if not content:
+                inSet.remove(fd)
+            else:
+                theFilter.feed(content)
+        if ins:
+            write(theFilter.filter())
+        if not inSet:
+            childIn.close()
+            childOut.close()
+            childErr.close()
+            write(theFilter.filter(consumeAll=True))
+            break
+        
+    
+def runSCons():
+    """Run SCons in a not-very-flexible way."""
+    cmd = "scons -u 2>&1"
+    print "<i># Working dir: %s</i><br/>" % cgi.escape(os.getcwd())
+    print "<b>%s</b><br/>" % cgi.escape(cmd)
+    filterOutput(cmd)
+    
+def main():
+    runSCons()
+    
+if __name__ == "__main__":
+    main()

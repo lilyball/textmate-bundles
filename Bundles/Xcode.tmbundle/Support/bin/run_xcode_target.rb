9e2e6ec5954f3a65f024719f0aa48033905d8f44
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-03-07 20:31:53 +0000

    • read the target to run (in case of multiple targets) as the XC_TARGET_NAME environment variable. Credits to Michael De Rosa for the changes.
    Long term the target should be read from the user specific project file, but so far no-one has stepped up to that challenge :)
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2832 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Xcode.tmbundle/Support/bin/run_xcode_target.rb
+++ Bundles/Xcode.tmbundle/Support/bin/run_xcode_target.rb
@@ -7,23 +7,7 @@ def shell_escape (str)
   str.gsub(/[{}()`'"\\; $<>&]/, '\\\\\&')
 end
 
-prefs = PropertyList::load(File.new("#{ENV['HOME']}/Library/Preferences/com.apple.Xcode.plist"))
-dir = prefs['PBXProductDirectory'] || File.dirname($project_dir) + "/build"
-
-proj       = PropertyList::load(File.new($project_dir + "/project.pbxproj"))
-objs       = proj['objects']
-rootObj    = objs[proj['rootObject']]
-
-if Xcode.supports_configurations? then
-  userFile      = $project_dir + "/#{ENV['USER']}.pbxuser"
-  user          = PropertyList::load(File.new(userFile)) if File.exists?(userFile)
-  activeConfig  = user && user[proj['rootObject']]['activeBuildConfigurationName']
-  dir += "/#{activeConfig || "Development"}"
-end
-
-targets = rootObj['targets'].reject { |t| not ['com.apple.product-type.tool', 'com.apple.product-type.application'].include?(objs[t]['productType']) }
-if targets.size == 1
-  target = objs[targets[0]]
+def run_target (dir,objs,target)
   productKey = target['productReference']
   product = objs[productKey]
   file = product['path']
@@ -47,10 +31,44 @@ if targets.size == 1
       -e 'end tell'
     }
   end
+end
+
+prefs = PropertyList::load(File.new("#{ENV['HOME']}/Library/Preferences/com.apple.Xcode.plist"))
+dir = prefs['PBXProductDirectory'] || File.dirname($project_dir) + "/build"
+
+proj       = PropertyList::load(File.new($project_dir + "/project.pbxproj"))
+objs       = proj['objects']
+rootObj    = objs[proj['rootObject']]
+
+if Xcode.supports_configurations? then
+  userFile      = $project_dir + "/#{ENV['USER']}.pbxuser"
+  user          = PropertyList::load(File.new(userFile)) if File.exists?(userFile)
+  activeConfig  = user && user[proj['rootObject']]['activeBuildConfigurationName']
+  dir += "/#{activeConfig || "Development"}"
+end
+
+targets = rootObj['targets'].reject { |t| not ['com.apple.product-type.tool', 'com.apple.product-type.application'].include?(objs[t]['productType']) }
+if targets.size == 1
+  target = objs[targets[0]]
+  run_target(dir,objs,target)
 elsif targets.size == 0
   puts "The project had no recognizable target to run.\n\n"
   rootObj['targets'].each { |t| puts objs[t].inspect.gsub(/, /, ",\n ") }
 else
-  puts "The project had multiple targets.\nDidn't know which to pick.\n\n"
-  rootObj['targets'].each { |t| puts objs[t].inspect.gsub(/, /, ",\n ") }
+  unless ENV['XC_TARGET_NAME']
+    puts "The project had multiple targets. Didn't know which to pick."
+    puts "Try setting project's XC_TARGET_NAME variable."
+    targets.each { |t| puts objs[t].inspect }
+    exit
+  end
+  puts "Will try to run target #{ENV['XC_TARGET_NAME']}"
+  found_target = targets.find { |t| objs[t]['name'] == ENV['XC_TARGET_NAME']}
+  if found_target
+    run_target(dir,objs,objs[found_target])
+  else
+      puts "No such target: #{ENV['XC_TARGET_NAME']}"
+      targets.each { |t| puts objs[t].inspect }
+      exit
+  end
 end
+

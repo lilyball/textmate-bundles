c17468563437d37c863ca5060d5331c5e27ad2cc
canadaduane <canadaduane@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-03-16 17:10:52 +0000

    Fixed that migration mirrored add_column was adding an extra blank line in the self.down method
    git-svn-id: http://macromates.com/svn/Bundles/trunk@2859 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Rails.tmbundle/Support/bin/insert_add_column_or_create_table.rb
+++ Bundles/Rails.tmbundle/Support/bin/insert_add_column_or_create_table.rb
@@ -43,7 +43,7 @@ if self_down = buffer.find { /^(\s*)def self\.down\b/ }
           column_params = insert_text[0]
           
           insert_text = "add_column \"#{table_name}\", \"#{column_name}\"#{column_params}\n"
-          buffer.lines.insert self_down_end[1], prepend("\n" + insert_text, indentation + "  ")
+          buffer.lines.insert self_down_end[1], prepend(insert_text, indentation + "  ")
         else
           puts "The db/schema.rb does not have a column matching \"#{column_name}\" within create_table \"#{table_name}\"."
           TextMate.exit_show_tool_tip

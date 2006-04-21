3998daa49da032f42035e9229df016c675403934
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-04-21 23:36:52 +0000

    This macro/script will scan the current source for function calls (using a regexp) then call man (looking in section 2 and 3) for each function found, harvest #include lines from that man page (in the SYNOPSIS), and then insert these in the top of the source. Key equivalent is ctrl-#.
    Potential improvements:
     1) use a look-up table instead of calling man because:
        a) faster
        b) more precise, e.g. the printf man page also lists stdarg.h under vprintf
        c) more stuff can be added (STL, Cocoa, etc.)
     2) inserted includes should insert extra blank line when the source has no includes, and when the source only has system includes, it should insert immediately below, not n blank lines below
     3) make it use XML input, so we can strip comments, strings, and keywords from what gets parsed for function calls
     4) also find use of data structures in the source (vector, map, string, etc.)
    
    I welcome user contributions, as I probably won’t work on this myself.
    
    
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3071 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/C.tmbundle/Support/bin/insert_missing_includes.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+MARK = [0xFFFC].pack("U").freeze
+def esc (txt); txt.gsub(/[$`\\]/, '\\\\\0'); end
+parts = STDIN.read.split(MARK)
+src = parts.join
+
+# find all function calls
+functions = src.scan(/(?:(?!->).(?!@|\.).|^\s*)\b([a-z]+)(?=\()/).flatten.to_set
+functions = functions - %w{ if while for switch sizeof sizeofA }
+
+# collect paths to all man files involved
+paths = functions.collect do |func|
+  %x{ man 2>/dev/null -WS2:3 #{func} }.scan /.+/
+end.flatten.sort.uniq
+
+# harvest includes from man files
+includes = Set.new
+paths.each do |path|
+  File.open(path) do |io|
+    in_synopsis = false
+    io.each_line do |line|
+      if in_synopsis then
+        case line
+        when /^\.In (\S+)$/:              includes << $1
+        when /^\.\w+ #include <(\S+)>$/:  includes << $1
+        when /^\.Sh /:                    break
+        end
+      elsif line =~ /^\.Sh SYNOPSIS/
+        in_synopsis = true
+      end
+    end
+  end
+end
+
+# figure out what we already included
+included = src.scan(/^\s*#\s*(?:include|import)\s+<(\S+)>/).flatten.to_set
+
+new_includes = (includes - included).collect do |inc|
+  "#include <#{inc}>\n"
+end.join
+
+parts[0].sub!(/\A (?:
+  ^ \s* (?:
+     \/\/ .*                            # line comments
+   | \/\* (?m:.*?) \*\/                 # comment blocks
+   | \# \s* (?:include|import) \s+ <.*  # system includes
+   |                                    # blank lines
+  ) \s* $ \n )*/x, '\0' + new_includes)
+
+print parts.collect { |part| esc part }.join('$0')

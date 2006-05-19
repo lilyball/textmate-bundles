b4daa3e20f519673b0bdd391036679aeba1adf23
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-05-19 16:24:55 +0000

    • remove all unnecessary code • converted tabs to spaces and made things align (it had a mixture, and things weren’t aligning with the de-facto tab size of 2 (for Ruby)) — I know it’s frowned upon to do this for checkins, but the source for this is short enough to follow the changes when doing a diff
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3361 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/progress.rb
+++ Support/lib/progress.rb
@@ -3,41 +3,21 @@ require 'English'
 
 module TextMate
 
-	def TextMate.call_with_progress( args, &block )
-		output_filepath	= args[:output_filepath]		# path to open after execution
-		
-		title			= args[:title] || 'Progress'
-		message			= args[:message] || 'Frobbing the widget...'
-		
-		cocoa_dialog	= "#{ENV['TM_SUPPORT_PATH']}/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog"
+  def TextMate.call_with_progress( args, &block )
 
-		tempdir = "/tmp/TextMate_progress_cmd_tmp.#{$PID}"
-		Dir.mkdir(tempdir)
-		Dir.chdir(tempdir)
-		pipe = IO.popen( %Q("#{cocoa_dialog}" progressbar --indeterminate --title "#{title}" --text "#{message}"), "w+")
-		begin
-			pipe.puts ""
-			data = block.call
+    title         = args[:title] || 'Progress'
+    message       = args[:message] || 'Frobbing the widget...'
 
-			puts data if data
+    cocoa_dialog  = "#{ENV['TM_SUPPORT_PATH']}/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog"
 
-# 			if data != nil
-# 				File.open(output_filepath, "w") do |file|
-# 					file.write(data)
-# 				end
-# 			end
+    pipe = IO.popen(%Q("#{cocoa_dialog}" progressbar --indeterminate --title "#{title}" --text "#{message}"), "w+")
+    begin
+      data = block.call
+      puts data if data
+    ensure
+      pipe.close
+    end
 
-# this is very fragile
-#			%x({ open -a "$(ps xw -o command|grep '\\.app/Contents/MacOS/TextMate'|grep -v grep|sed 's/\\(.*\\.app\\).*/\\1/')" "#{output_filepath}"; sleep 30; rm -rf "#{tempdir}"; } </dev/null &>/dev/null &)
-		ensure
-
-			pipe.close
-			
-			%x{rm -rf "#{tempdir}"}
-		end
-
-		sleep 0.1
-		
-	end
+  end
 
 end

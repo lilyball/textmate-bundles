aac7ffce35945fdfa79e9f2bc45133edbaaffb68
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-05-19 17:20:53 +0000

    Integrated patch from Trevor Harmon (http://macromates.com/ticket/show?ticket_id=48F737BE)
    TextMate will now use the diff command specified in your subversion config, or set as the TM_SVN_DIFF_CMD environment variable.
    
    If one of these options are used, and the result from diff’ing is only the “standard” subversion header, the output is suppressed. If there is no output at all, a “No differences.” tool tip will show (as usual.) This means that if you are using fmdiff [1] then you should remove the lines where it prints status information to stderr (as it will prevent TM from suppressing the output window.)
    
    [1] http://ssel.vub.ac.be/ssel/internal:fmdiff
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3362 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/svn_diff.rb
+++ Bundles/Subversion.tmbundle/Support/svn_diff.rb
@@ -4,29 +4,42 @@ $LOAD_PATH << ENV['TM_SUPPORT_PATH'] + "/lib"
 require 'progress'
 
 module Subversion
-	def Subversion.diff_active_file( revision, command )
-		svn			= ENV['TM_SVN'] || 'svn'
-		target_path	= ENV['TM_FILEPATH']
-#		revision	= ARGV[0]
-#		command		= ARGV[1]
-		output_path	= File.basename(target_path) + ".diff"
+  def Subversion.diff_active_file( revision, command )
+    svn         = ENV['TM_SVN'] || 'svn'
+    diff_cmd    = ENV['TM_SVN_DIFF_CMD']
+    diff_arg    = diff_cmd ? "--diff-cmd #{diff_cmd}" : ''
 
-		TextMate::call_with_progress(:title => command,
-									:message => "Accessing Subversion Repository…",
-									:output_filepath => output_path) do
-			have_data = false
-			
-			# idea here is to stream the data rather than submit it in one big block
-			%x{"#{svn}" diff --diff-cmd diff "-r#{revision}" "#{target_path}"}.each_line do |line|
-				have_data = true unless line.empty?
-				puts line
-			end
-			
-			if not have_data then
-				# switch to tooltip output to report lack of differences
-				puts "No differences found."
-				exit 206;
-			end
-		end
-	end
+    target_path = ENV['TM_FILEPATH']
+    svn_header  = /\AIndex: #{target_path}\n=+\n\z/
+
+    TextMate::call_with_progress(:title => command, :message => "Accessing Subversion Repository…") do
+      res = %x{"#{svn}" 2>&1 diff "-r#{revision}" #{diff_arg} "#{target_path}"}
+
+      if $? != 0
+        print res
+        exit 206      # The command failed; show its output as a tooltip
+      elsif res.empty?
+        puts "No differences found."
+        exit 206      # Report lack of differences as a tooltip
+      elsif (custom_diff? or diff_cmd) and res =~ svn_header
+        exit 200      # Suppress output, as we only got a svn header (so likely diff-cmd opened its own window)
+      else
+        print res
+      end
+    end
+
+  end
+
+  # Returns true if ~/.subversion/config contains an uncommented entry for diff-cmd
+  def Subversion.custom_diff?
+    config_file = ENV['HOME'] + "/.subversion/config"
+      
+    if File.exists?(config_file)
+      IO.foreach(config_file) do |line|
+        return true if line =~ /^\s?diff-cmd\s?=\s?(.*)/
+      end
+    end
+    
+    return false
+  end  
 end

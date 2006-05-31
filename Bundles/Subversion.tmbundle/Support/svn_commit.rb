lilyball/textmate-bundles

commit ef13a3b7bbf5c3eaa4d03667b155637515db420a

• Ask and ye shall receive (something) I: Brad Choate requests diffs. The path of least resistance: double-click the row, up pops a diff against the pristine copy. New argument to enable this: --diff-cmd "/path/to/svn" (or /path/to/cvs, etc.). • Ask and ye shall receive (something) II: Fred B. wants 'R' to highlight the same way as 'D' (for Mercurial). ('R' is 'replace' in Subversion, but given apparent rarity of that condition, we're highlighting it unconditionally for now.) • Clean up color choosing. • Do not require the paths to be last on the command line.
git-svn-id: http://macromates.com/svn/Bundles/trunk@3350 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

Bundles/Subversion.tmbundle/Support/svn_commit.rb
@@ -85,7 +85,7 @@ mup.html {
 
 		commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
 
-		commit_args = %x{"#{commit_tool}" --status #{commit_status} #{commit_path_text}}
+		commit_args = %x{"#{commit_tool}" --diff-cmd "#{svn}" --status #{commit_status} #{commit_path_text}}
 
 		status = $CHILD_STATUS
 		if status != 0

Support/bin/CommitWindow.app/Contents/Resources/English.lproj/MainMenu.nib/classes.nib
@@ -2,7 +2,7 @@
     IBClasses = (
         {CLASS = CWTextView; LANGUAGE = ObjC; SUPERCLASS = NSTextView; }, 
         {
-            ACTIONS = {cancel = id; commit = id; }; 
+            ACTIONS = {cancel = id; commit = id; doubleClickFileTable = id; }; 
             CLASS = CommitWindowController; 
             LANGUAGE = ObjC; 
             OUTLETS = {
@@ -15,6 +15,7 @@
                 fPreviousSummaryPopUp = NSPopUpButton; 
                 fRequestText = NSTextField; 
                 fStatusColumn = NSTableColumn; 
+                fTableView = NSTableView; 
                 fWindow = NSWindow; 
             }; 
             SUPERCLASS = NSWindowController; 

Tools/CommitWindowProject/CommitWindowController.h
@@ -23,9 +23,12 @@
 	IBOutlet NSButton *				fCancelButton;
 	IBOutlet NSButton *				fOKButton;
 
+	IBOutlet NSTableView *			fTableView;
 	IBOutlet NSTableColumn *		fCheckBoxColumn;
 	IBOutlet NSTableColumn *		fStatusColumn;
 	IBOutlet NSTableColumn *		fPathColumn;
+
+	NSString *						fDiffCommand;
 	
 	NSArray *						fFileStatusStrings;
 }

Tools/CommitWindowProject/CommitWindowController.m
@@ -75,40 +75,55 @@
 			argument	= [args objectAtIndex:i];
 			fFileStatusStrings = [[argument componentsSeparatedByString:@":"] retain];
 		}
+		else if( [argument isEqualToString:@"--diff-cmd"] )
+		{
+			// Next argument should be a colon-seperated list of status strings, one for each path
+			if( i >= (argc - 1) )
+			{
+				fprintf(stderr, "commit window: missing text: --diff-cmd \"/usr/bin/svn\"\n");
+				[self cancel:nil];
+			}
+			
+			i += 1;
+			argument	= [args objectAtIndex:i];
+			fDiffCommand = [argument retain];
+		}
 		else
 		{
 			NSMutableDictionary *	dictionary	= [fFilesController newObject];
-			BOOL					itemSelectedForCommit;
 
 			[dictionary setObject:[argument stringByAbbreviatingWithTildeInPath] forKey:@"path"];
-			if( fFileStatusStrings != nil )
-			{
-				// Deselect external commits by default
-				if([[fFileStatusStrings objectAtIndex:[[fFilesController content] count]] hasPrefix:@"X"])
-				{
-					itemSelectedForCommit = NO;
-				}
-				else
-				{
-					itemSelectedForCommit = YES;
-				}
-				[dictionary setObject:[NSNumber numberWithBool:itemSelectedForCommit] forKey:@"commit"]; 
-			}
-	
 			[fFilesController addObject:dictionary];
 		}
 	}
 	
 	//
 	// Done processing arguments, now add status to each item
+	// 								and choose default commit state
 	//
 	if( fFileStatusStrings != nil )
 	{
-		NSArray * files = [fFilesController arrangedObjects];
-		for( i = 0; i < MIN([files count], [fFileStatusStrings count]); i += 1 )
+		NSArray *	files = [fFilesController arrangedObjects];
+		int			count = MIN([files count], [fFileStatusStrings count]);
+		
+		for( i = 0; i < count; i += 1 )
 		{
 			NSMutableDictionary *	dictionary = [files objectAtIndex:i];
-			[dictionary setObject:[fFileStatusStrings objectAtIndex:i] forKey:@"status"];
+			NSString *				status = [fFileStatusStrings objectAtIndex:i];
+			BOOL					itemSelectedForCommit;
+			
+			[dictionary setObject:status forKey:@"status"];
+
+			// Deselect external commits by default
+			if([status hasPrefix:@"X"])
+			{
+				itemSelectedForCommit = NO;
+			}
+			else
+			{
+				itemSelectedForCommit = YES;
+			}
+			[dictionary setObject:[NSNumber numberWithBool:itemSelectedForCommit] forKey:@"commit"]; 
 		}
 	}
 	
@@ -119,6 +134,9 @@
 	// Populate previous summary menu
 	//
 	[self populatePreviousSummaryMenu];
+
+	[fTableView setTarget:self];
+	[fTableView setDoubleAction:@selector(doubleClickRowInTable:)];
 
 	//
 	// Map the enter key to the OK button
@@ -250,13 +268,11 @@
 	[fCommitMessage setString:summary];
 }
 
-// Save, in an LRU list, the most recent commit summary
+// Save, in a MRU list, the most recent commit summary
 - (void) saveSummary
 {
 	NSUserDefaults *  	defaults		= [NSUserDefaults standardUserDefaults];
 	NSString *			latestSummary	= [fCommitMessage string];
-	
-//	NSLog(@"%s%@", _cmd, latestSummary);
 	
 	// avoid empty string
 	if( ! [latestSummary isEqualToString:@""] )
@@ -374,6 +390,21 @@
 	exit(-128);
 }
 
+- (IBAction) doubleClickRowInTable:(id)sender
+{
+	if(fDiffCommand != nil)
+	{
+		NSString *	filePath	= [[[fFilesController arrangedObjects] objectAtIndex:[sender selectedRow]] objectForKey:@"path"];
+		NSString *	command		= [NSString stringWithFormat:@"\"%@\" diff \"%@\" | \"%s/bin/mate\" &> /dev/null &",
+									fDiffCommand,
+									filePath,
+									getenv("TM_SUPPORT_PATH")];
+
+		system([command UTF8String]);
+	}
+	
+}
+
 //
 // Attempt to mirror the appearance of the status and info displays
 //
@@ -389,20 +420,11 @@
 		NSColor *				foreColor;
 		NSColor *				backColor;
 
-		backColor = BackColorFromStatus(status);
+		ColorsFromStatus( status, &foreColor, &backColor );
 		
 		[cell setDrawsBackground:YES];
 		[cell setBackgroundColor:backColor];
-		
-		if( tableColumn == fStatusColumn )
-		{
-			foreColor = ForeColorFromStatus(status);
-			[cell setTextColor:foreColor];
-		}
-		else
-		{
-			[cell setTextColor:[NSColor blackColor]];
-		}
+		[cell setTextColor:foreColor];
 	}
 }
 

Tools/CommitWindowProject/English.lproj/MainMenu.nib/classes.nib
@@ -2,7 +2,7 @@
     IBClasses = (
         {CLASS = CWTextView; LANGUAGE = ObjC; SUPERCLASS = NSTextView; }, 
         {
-            ACTIONS = {cancel = id; commit = id; }; 
+            ACTIONS = {cancel = id; commit = id; doubleClickFileTable = id; }; 
             CLASS = CommitWindowController; 
             LANGUAGE = ObjC; 
             OUTLETS = {
@@ -15,6 +15,7 @@
                 fPreviousSummaryPopUp = NSPopUpButton; 
                 fRequestText = NSTextField; 
                 fStatusColumn = NSTableColumn; 
+                fTableView = NSTableView; 
                 fWindow = NSWindow; 
             }; 
             SUPERCLASS = NSWindowController; 

commit 783e4f502268b7117d0b4dbd4812ecdbc4c7dd1e

- svn_commit.rb: revert to previous revision. Fixes svn commit line broken by r3350.
I don't know what --diff-cmd was doing here... I see it in my commit window with a checkbox :)


git-svn-id: http://macromates.com/svn/Bundles/trunk@3487 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

Bundles/Subversion.tmbundle/Support/svn_commit.rb
@@ -85,7 +85,7 @@ mup.html {
 
 		commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
 
-		commit_args = %x{"#{commit_tool}" --diff-cmd "#{svn}" --status #{commit_status} #{commit_path_text}}
+		commit_args = %x{"#{commit_tool}" --status #{commit_status} #{commit_path_text}}
 
 		status = $CHILD_STATUS
 		if status != 0
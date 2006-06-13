b331c9b5b476cc5485e4f51da863c6b254b2c16a
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-13 03:24:26 +0000

    Upgrades for svn commit, status, and update:
    • These commands now handle multicolumn status correctly. Among other benefits, property status is now distinguishable from file status.
    • Multicolumn arguments to CommitWindow are now passed such that the status for other files will be shown correctly. However, CommitWindow does not yet show or highlight the multicolumn status correctly (you'll see only the first character of a multicolumn status item).
    • New CSS style similar to the new Xcode style. Not sure about the colors. Logo needs tweaking, too.
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3621 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/format_status.rb
+++ Bundles/Subversion.tmbundle/Support/format_status.rb
@@ -1,5 +1,27 @@
 
-# status or update?
+# Includes
+support = ENV['TM_SUPPORT_PATH']
+require (support + "/bin/Builder.rb")
+require (support + "/bin/shelltokenize.rb")
+
+# Arguments
+bundle				= ENV['TM_BUNDLE_SUPPORT']
+work_path			= ENV['WorkPath']
+work_paths			= TextMate.selected_paths_array
+ignore_file_pattern = /(\/.*)*(\/\..*|\.(tmproj|o|pyc)|Icon)/
+
+
+
+strip_path_prefix	= work_path # Dir.pwd
+svn			= ENV['TM_SVN']
+svn = "svn" if svn.nil?
+
+work_path = work_paths[0] if work_path.nil? and (not work_paths.nil?) and (work_paths.size == 1)
+work_path ||= '(selected files)'
+
+#
+# Status or update?
+#
 $is_status = false
 
 ARGV.each {|arg| $is_status = true if arg == "--status"}
@@ -10,86 +32,107 @@ else
 	"update"
 end
 
-bundle				= ENV['TM_BUNDLE_SUPPORT']
-support				= ENV['TM_SUPPORT_PATH']
-work_path			= ENV['WorkPath'] || "(selected files)"
-ignore_file_pattern = /(\/.*)*(\/\..*|\.(tmproj|o|pyc)|Icon)/
-
-strip_path_prefix	= work_path # Dir.pwd
-svn			= ENV['TM_SVN']
-svn = "svn" if svn.nil?
-
-require (support + "/bin/Builder.rb")
-
-
 mup = Builder::XmlMarkup.new(:target => STDOUT)
 
 class << mup
+	
+	StatusColumnNames = ['File', 'Property', 'Lock', 'History', 'Switched', 'Repository Lock']
 	
 	StatusMap = {	'A' => 'added',
 					'D' => 'deleted',
 					'G' => 'merged',
 					'U' => 'updated',
 					'M' => 'modified',
+					'L' => 'locked',
+					'B' => 'broken',
 					'R' => 'replaced',
 					'C' => 'conflict',
 					'!' => 'missing',
+					'+' => 'added',
 					'"' => 'typeconflict',
 					'?' => 'unknown',
-					'I' => 'ignore',
-					'X' => 'external' }
+					'I' => 'ignored',
+					'X' => 'external',
+					' ' => 'none'}
+	
+	# this may be more dynamic in the future
+	# it also possibly should not be stashed in mup
+	def status_column_count
+		# update has three columns vs. up to eight for status as of 1.3.x
+		# But, for status, if we assume eight, we might have compatibility
+		# issues with earlier versions which had fewer columns (unless they
+		# had more padding than I recall). The last three columns seem to be
+		# network status-only, so this isn't a big deal for now.
+		$is_status ? 5 : 3
+	end
 	
 	def td_status!(status)
-		status.each_byte { |s| c = s.chr; td(c, "class" => StatusMap[c]) }
+		status_column_count.times do |i|
+			
+			c = status[i].chr
+			status_class = StatusMap[c] || 'dunno'
+			td(c, "class" => status_class, "title" => StatusColumnNames[i] + " " + status_class.capitalize)
+		end
 	end
 end
 
 mup.html {
 	mup.head {
-			mup.title("Subversion #{command_name}")
-			mup.style( "@import 'file://"+bundle+"/Stylesheets/svn_style.css';
-						@import 'file://"+bundle+"/Stylesheets/svn_status_style.css';", "type" => "text/css")
+			mup.title("Subversion #{command_name.capitalize}")
+			mup.style( "@import 'file://"+bundle+"/Stylesheets/svn_status_style.css';", "type" => "text/css")
 	}
 
-	mup.body { 
-		mup.h1("Subversion #{command_name} for '#{File.basename(work_path)}'")
+	mup.body {
+		mup.h1 do 
+			mup.img( :src => "file://"+bundle+"/Stylesheets/subversion_logo.tiff",
+						:height => 21,
+						:width => 32 )
+			mup.text " #{command_name.capitalize} for "
+			mup << "&ldquo;"
+			mup.text "#{File.basename(work_path)}"
+			mup << "&rdquo;"
+		end
 #		mup.hr
-		mup.div("class" => "command"){ mup.strong("#{svn} #{command_name}"); mup.text!(" " + work_path) } #mup.text!("checking "); 
+#		mup.div("class" => "command"){ mup.strong("#{svn} #{command_name}"); mup.text!(" " + work_path) } #mup.text!("checking "); 
 		STDOUT.flush
-		mup.hr
-
-		mup.table("class" => "status") {
-			STDIN.each_line do |line|
+		
+		mup.div( "class" => "section" ) do
+			mup.table("class" => "status") {
+			
+				match_columns		= '.' * mup.status_column_count
+				unknown_file_status	= '?' + (' ' * (mup.status_column_count - 1))
+			
+				STDIN.each_line do |line|
 				
-				mup.tr {
-					if /^svn:/.match( line ).nil? then
-						
-						match = if $is_status then
-							/^(.....)(?:\s+)(.*)\n/.match( line )
-						else
-							/^(.)(?:\s+)(.*)\n/.match( line )
-						end
-						
-						if match.nil? then
-							mup.td("")
-							mup.td(line)
-						else
-							status	= match[1]
-							file	= match[2]
-
-							if status == '?    ' and ignore_file_pattern =~ file
-							    # This is a file that we don't want to know about
-							    nil
+					# ignore lines consisting only of whitespace
+					next if line.squeeze.strip.empty?
+					
+					# build the row
+					mup.tr {
+						if /^svn:/.match( line ).nil? then
+							match = /^(#{match_columns})(?:\s+)(.*)\n/.match( line )
+							if match.nil? then
+								mup.td(:colspan => (mup.status_column_count + 1).to_s ) do
+									mup.div(:class => 'info') { mup.text(line) }
+								end
 							else
-    							mup.td_status!(status)
-    							mup.td { mup.a( file.sub( /^#{strip_path_prefix}\//, ""), "href" => 'txmt://open?url=file://' + file, "class" => "pathanme" ) }
-                            end
-						end 
-					else
-						mup.td { mup.div( line, "class" => "error" ) }
-					end
-				}
-			end
-		}
+								status	= match[1]
+								file	= match[2]
+
+								if status == unknown_file_status and ignore_file_pattern =~ file
+								    # This is a file that we don't want to know about
+								    nil
+								else
+	    							mup.td_status!(status)
+	    							mup.td { mup.a( file.sub( /^#{strip_path_prefix}\//, ""), "href" => 'txmt://open?url=file://' + file, "class" => "pathanme" ) }
+	                            end
+							end 
+						else
+							mup.td { mup.div( line, "class" => "error" ) }
+						end
+					}
+				end
+			}
+		end
 	}
 }
--- Bundles/Subversion.tmbundle/Support/svn_commit.rb
+++ Bundles/Subversion.tmbundle/Support/svn_commit.rb
@@ -5,7 +5,8 @@ svn				= ENV['TM_SVN']
 commit_tool		= ENV['CommitWindow']
 bundle			= ENV['TM_BUNDLE_SUPPORT']
 support			= ENV['TM_SUPPORT_PATH']
-ignore_file_pattern = /(\/.*)*(\/\..*|\.(tmproj|o|pyc)|Icon)/
+
+IgnoreFilePattern = /(\/.*)*(\/\..*|\.(tmproj|o|pyc)|Icon)/
 
 CURRENT_DIR		= Dir.pwd + "/"
 
@@ -15,109 +16,127 @@ require (support + "/bin/Builder.rb")
 mup = Builder::XmlMarkup.new(:target => STDOUT)
 
 mup.html {
-	mup.head {
-			mup.title("Subversion commit")
-			mup.style( "@import 'file://"+bundle+"/Stylesheets/svn_style.css';", "type" => "text/css")
-	}
+	mup.head do
+			mup.title("Subversion Commit")
+			mup.style( "@import 'file://"+bundle+"/Stylesheets/svn_status_style.css';", "type" => "text/css")
+	end
 
 	mup.body { 
-		mup.h1("Subversion Commit")
-		STDOUT.flush
-		mup.hr
-
-		# Ignore files without changes
-#puts TextMate::selected_paths_for_shell
-		status_command = %Q{"#{svn}" status #{TextMate::selected_paths_for_shell}}
-#puts status_command
-		status_output = %x{#{status_command}}
-#puts status_output
-		paths = status_output.scan(/^(.)....(\s+)(.*)\n/)
-
-
-		def matches_to_paths(matches)
-			paths = matches.collect {|m| m[2] }
-			paths.collect{|path| path.sub(/^#{CURRENT_DIR}/, "") }
+		mup.h1 do 
+			mup.img( :src => "file://"+bundle+"/Stylesheets/subversion_logo.tiff",
+						:height => 21,
+						:width => 32 )
+			mup.text " Commit"
 		end
-
-		def matches_to_status(matches)
-			matches.collect {|m| m[0]}
-		end
-
-		# Ignore files with '?', but report them to the user
-		unknown_paths = paths.select { |m| m[0] == '?' }
-        unknown_to_report_paths = paths.select{ |m| m[0] == '?' and not ignore_file_pattern =~ m[2]}
-		if unknown_to_report_paths and unknown_to_report_paths.size > 0 then
-		    
-			mup.div( "class" => "info" ) {
-				mup.text! "These files are not added to the repository, and so will not be committed:"		
-				mup.ul{ matches_to_paths(unknown_to_report_paths).each{ |path| mup.li(path) } }
-			}
-		end
-
-		# Ignore files with '?', but report them to the user
-		missing_paths = paths.select { |m| m[0] == '!' }
-        missing_to_report_paths = paths.select{ |m| m[0] == '!' and not ignore_file_pattern =~ m[2]}
-		if missing_to_report_paths and missing_to_report_paths.size > 0 then
-		    
-			mup.div( "class" => "info" ) {
-				mup.text! "These files are missing from the working copy:"		
-				mup.ul{ matches_to_paths(missing_to_report_paths).each{ |path| mup.li(path) } }
-			}
-		end
-
-		# Fail if we have conflicts -- svn commit will fail, so let's
-		# error out before the user gets too involved in the commit
-		conflict_paths = paths.select { |m| m[0] == 'C' }
-
-		if conflict_paths and conflict_paths.size > 0 then
-			mup.div( "class" => "error" ) {
-				mup.text! "Cannot continue; there are merge conflicts in files:"		
-				mup.ul{ matches_to_paths(conflict_paths).each{ |path| mup.li(path) } }
-				mup.text! "Canceled."
-			}	
-			exit -1
-		end
-
-		# Remove the unknown paths from the commit
-		commit_matches = paths - unknown_paths
-
-		# Remove the missing paths from the commit
-		commit_matches = commit_matches - missing_paths
-
-		if commit_matches.nil? or commit_matches.size == 0
-			mup.div( "class" => "info" ) {
-				mup.text! "File(s) not modified; nothing to commit."
-				mup.ul{ matches_to_paths(unknown_paths).each{ |path| mup.li(path) } }
-			}
-			exit 0
-		end
-
-		STDOUT.flush
-
-		commit_paths_array = matches_to_paths(commit_matches)
-		commit_status = matches_to_status(commit_matches).join(":")
-
-		commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
-
-		commit_args = %x{"#{commit_tool}" --diff-cmd "#{svn}" --status #{commit_status} #{commit_path_text}}
-
-		status = $CHILD_STATUS
-		if status != 0
-			mup.div( "class" => "error" ) {
-				mup.text! "Canceled (#{status >> 8})."
-			}	
-			exit -1
-		end
-
-		mup.div("class" => "command"){ mup.strong(%Q{#{svn} commit}); mup.text!(commit_args) }
 		
-		mup.pre {
+		STDOUT.flush
+
+		mup.div( :class => 'section' ) do
+
+			# Ignore files without changes
+#puts TextMate::selected_paths_for_shell
+			status_command = %Q{"#{svn}" status #{TextMate::selected_paths_for_shell}}
+#puts status_command
+			status_output = %x{#{status_command}}
+#puts status_output
+			paths = status_output.scan(/^(.....)(\s+)(.*)\n/)
+
+			def matches_to_paths(matches)
+				paths = matches.collect {|m| m[2] }
+				paths.collect{|path| path.sub(/^#{CURRENT_DIR}/, "") }
+			end
+
+			def matches_to_status(matches)
+				# collect the status, and replace prefix spaces with underscores so command-line argument passing works later
+				matches.collect {|m| m[0]}.map {|m| m.rstrip.gsub(/\s/, '_')}
+			end
+
+			def select_files_with_status_prefix(paths, char, exclude_ignores = false)
+				paths.select do |m|
+					index = m[0].index(char)
+					select_it = (index != nil) and (index == 0) 
+					if select_it and exclude_ignores
+						select_it = (not IgnoreFilePattern =~ m[2])
+					end
+					select_it
+				end
+			end
+
+			# Ignore files with '?', but report them to the user String.index
+			unknown_paths = select_files_with_status_prefix(paths, '?', true)
+	        unknown_to_report_paths = select_files_with_status_prefix(paths, '?', false)
+			if unknown_to_report_paths and unknown_to_report_paths.size > 0 then
+				mup.div( "class" => "info" ) {
+					mup.text! "These files are not under version control, and so will not be committed:"		
+					mup.ul{ matches_to_paths(unknown_to_report_paths).each{ |path| mup.li(path) } }
+				}
+			end
+
+			# Ignore files with '!', but report them to the user
+			missing_paths = select_files_with_status_prefix(paths, '!', true)
+	        missing_to_report_paths = select_files_with_status_prefix(paths, '!', false)
+			if missing_to_report_paths and missing_to_report_paths.size > 0 then
+	    
+				mup.div( "class" => "info" ) {
+					mup.text! "These files are missing from the working copy:"		
+					mup.ul{ matches_to_paths(missing_to_report_paths).each{ |path| mup.li(path) } }
+				}
+			end
+
+			# Fail if we have conflicts -- svn commit will fail, so let's
+			# error out before the user gets too involved in the commit
+			conflict_paths = select_files_with_status_prefix(paths, 'C', false)
+
+			if conflict_paths and conflict_paths.size > 0 then
+				mup.div( "class" => "error" ) {
+					mup.text! "Cannot continue; there are merge conflicts in files:"		
+					mup.ul{ matches_to_paths(conflict_paths).each{ |path| mup.li(path) } }
+					mup.text! "Canceled."
+				}	
+				exit -1
+			end
+
+			# Remove the unknown paths from the commit
+			commit_matches = paths - unknown_paths
+
+			# Remove the missing paths from the commit
+			commit_matches = commit_matches - missing_paths
+
+			if commit_matches.nil? or commit_matches.size == 0
+				mup.div( "class" => "info" ) {
+					mup.text! "File(s) not modified; nothing to commit."
+					mup.ul{ matches_to_paths(unknown_paths).each{ |path| mup.li(path) } }
+				}
+				exit 0
+			end
+
 			STDOUT.flush
 
-			IO.popen("#{svn} commit --non-interactive --force-log #{commit_args}", "r+") do |pipe|
-				pipe.each {|line| mup.text! line }
+			commit_paths_array = matches_to_paths(commit_matches)
+			commit_status = matches_to_status(commit_matches).join(":")
+
+			commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
+
+			commit_args = %x{"#{commit_tool}" --diff-cmd "#{svn}" --status #{commit_status} #{commit_path_text}}
+
+			status = $CHILD_STATUS
+			if status != 0
+				mup.div( "class" => "error" ) {
+					mup.text! "Canceled (#{status >> 8})."
+				}	
+				exit -1
 			end
-		}
+
+			mup.div("class" => "command"){ mup.strong(%Q{#{svn} commit}); mup.text!(commit_args) }
+	
+			mup.pre {
+				STDOUT.flush
+
+				IO.popen("#{svn} commit --non-interactive --force-log #{commit_args}", "r+") do |pipe|
+					pipe.each {|line| mup.text! line }
+				end
+			}
+		end
 	}
 }
 

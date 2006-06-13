9c0bfd4f36dd0bdbaa38acea96e57904f574f5ed
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-13 12:43:31 +0000

    • pass the block directly to popen, thus make the result of call_with_progress the result of the block passed in.
    This is more flexible (than calling ‘puts result’,) and the one call-site didn’t rely on the result being printed.
    
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3623 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/progress.rb
+++ Support/lib/progress.rb
@@ -7,16 +7,8 @@ module TextMate
 
     title         = args[:title] || 'Progress'
     message       = args[:message] || 'Frobbing the widget...'
-
     cocoa_dialog  = "#{ENV['TM_SUPPORT_PATH']}/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog"
-
-    pipe = IO.popen(%Q("#{cocoa_dialog}" progressbar --indeterminate --title "#{title}" --text "#{message}"), "w+")
-    begin
-      data = block.call
-      puts data if data
-    ensure
-      pipe.close
-    end
+    IO.popen(%Q("#{cocoa_dialog}" progressbar --indeterminate --title "#{title}" --text "#{message}"), "w+", &block)
 
   end
 

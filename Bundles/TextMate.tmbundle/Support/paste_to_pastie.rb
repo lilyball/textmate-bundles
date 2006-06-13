e9c18cc7b1ee8a5966be7861dfe0cf0398199bc7
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-13 14:06:45 +0000

    • move out code which pastes to server • capitalize actions in the list and call them “destinations for URL”
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3625 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/TextMate.tmbundle/Support/paste_to_pastie.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby -rjcode -Ku
+
+require 'cgi'
+require 'progress'
+require 'fileutils'
+require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/doctohtml.rb"
+
+TextMate.call_with_progress(:title => "Paste to Pastie", :message => "Contacting Server “pastie.caboo.se”…") do
+  text_file, html_file = `mktemp -t tm_paste && mktemp -t tm_paste`.split("\n")
+
+  xml = STDIN.read
+
+  text = CGI::unescapeHTML(xml.gsub(/<[^>]+>/, ''))
+  open(text_file, "w") { |io| io.write(text); }
+
+  html = document_to_html(xml)
+  open(html_file, "w") { |io| io.write(html); }
+
+  print %x{
+    curl http://pastie.caboo.se/pastes/create \
+    	-s -L -o /dev/null -w "%{url_effective}" \
+    	-H "Expect:" \
+    	-F "paste[parser]=plaintext" \
+    	-F "paste[body]=<#{text_file}" \
+    	-F "paste[textmate_html]=<#{html_file}"    
+  }
+
+  File.unlink(text_file)
+  File.unlink(html_file)
+end

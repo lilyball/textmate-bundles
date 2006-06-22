0707230dbeb3bf305af07218f6e4aa1b7b63d9c9
cskiadas <cskiadas@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-22 04:05:21 +0000

    Add support for Camino.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3758 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/browser.rb
+++ Support/lib/browser.rb
@@ -1,6 +1,6 @@
 class Browser  
   def Browser.load_url(url)
-    unless omniweb_did_load(url) or safari_did_load(url) then
+    unless omniweb_did_load(url) or safari_did_load(url) or camino_did_load(url) then
       %x{open '#{url}'}
     end
   end
@@ -29,6 +29,21 @@ APPLESCRIPT}
     			if the_url is "#{url}" then
     				activate
     				do JavaScript "window.location.reload();" in first document
+    				return true
+    			end if
+    		end if
+    	end tell
+APPLESCRIPT}
+    res =~ /true/
+  end
+  def Browser.camino_did_load(url)
+    res = %x{ps -xc|grep -sq Camino && osascript <<'APPLESCRIPT'
+    	tell app "Camino"
+    		if windows is not { }
+    			set the_url to URL of first window
+    			if the_url is "#{url}" then
+    				activate
+    				do javascript "window.location.reload();"
     				return true
     			end if
     		end if

38ab760da7a20a42e4302d87d9bd2b0c5940c24c
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-25 01:36:02 +0000

    • read TM’s preferences plist to get file/folder patterns instead of of ‘defaults’ • read default binary and text file type extensions from TM
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3787 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/textmate.rb
+++ Support/lib/textmate.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'plist'
 
 module TextMate
 
@@ -7,23 +8,25 @@ module TextMate
       @file_pattern = load_pattern('OakFolderReferenceFilePattern', '!(/\.(?!htaccess)[^/]*|\.(tmproj|o|pyc)|/Icon\r)$')
       @folder_pattern = load_pattern('OakFolderReferenceFolderPattern', '!.*/(\.[^/]*|CVS|_darcs|\{arch\}|blib|.*~\.nib|.*\.(framework|app|pbproj|pbxproj|xcode(proj)?|bundle))$')
 
-      @text_types = [ ]           # prefs_for_key('OakProjectTextFiles')
-      @binary_types = [ ".nib" ]  # prefs_for_key('OakProjectBinaryFiles')
+      @text_types = prefs_for_key('OakProjectTextFiles')     || [ ]
+      @binary_types = prefs_for_key('OakProjectBinaryFiles') || [ ".nib" ]
     end
 
-    # # this can’t be used ATM because of range error from plist
-    # def prefs_for_key (key)
-    #   prefs_file = "#{ENV['HOME']}/Library/Preferences/com.macromates.textmate.plist"
-    #   File.open(prefs_file) do |f|
-    #     return PropertyList::load(f)[key]
-    #   end
-    # end
+    def prefs_for_key (key)
+      prefs_file = "#{ENV['HOME']}/Library/Preferences/com.macromates.textmate.plist"
+      File.open(prefs_file) do |f|
+        return PropertyList::load(f)[key]
+      end
+    end
 
     def load_pattern (key, default_pattern)
-      str = %x{defaults 2>/dev/null read com.macromates.textmate #{key}}.gsub(/\\\\/, '\\').chomp
-      str = default_pattern if(str == "")
-      str = str[1..-1] if(negate = str[0] == ?!)
-      return { :regexp => Regexp.new(str), :negate => negate }
+      str = prefs_for_key(key)
+      str = default_pattern if str.to_s.empty?
+      if str[0] == ?! then
+        { :regexp => Regexp.new(str[1..-1]), :negate => true }
+      else
+        { :regexp => Regexp.new(str), :negate => false }
+      end
     end
 
     def binary? (file)
@@ -78,4 +81,7 @@ module TextMate
 
 end
 
-# TextMate.each_text_file { |f| puts f }
+# if $0 == __FILE__ then
+#   ENV['TM_PROJECT_DIRECTORY'] = "/tmp"
+#   TextMate.each_text_file { |f| puts f }
+# end

af9333a4a44ac683c05384d4db4d38b08a3b0437
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-25 17:06:44 +0000

    • do not escape } as part of snippet escaping (this is only when the text ends up as default value for a placeholder)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3794 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/escape.rb
+++ Support/lib/escape.rb
@@ -2,11 +2,10 @@
 
 # properly escapes argument for inclusion in a shell command
 def e_sh(argument)
-	argument.gsub(/([^a-zA-Z0-9_.])/, "\\\\\\1")
+	argument.gsub(/[^a-zA-Z0-9_.]/, "\\\\\\0")
 end
-
 
 # properly escapes a string for insertion as a snippet
 def e_sn(str)
-	str.to_s.gsub(/([}$`\\])/, "\\\\\\1")
+	str.to_s.gsub(/[$`\\]/, "\\\\\\0")
 end

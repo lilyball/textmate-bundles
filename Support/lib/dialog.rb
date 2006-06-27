b2edd50a4ddb338152bb28ca8fd8cc13edb8cbfc
cskiadas <cskiadas@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-27 03:19:32 +0000

    Add support for user-specified Okay-Cancel buttons. Add yes-no box.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3844 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/dialog.rb
+++ Support/lib/dialog.rb
@@ -3,7 +3,7 @@ module Dialog
   module_function
 
   def request_string(options = Hash.new,&block)
-    _options = self.default_hash
+    _options = self.default_hash(options)
     _options["type"] = "inputbox"
     _options["title"] = options[:title] || "Enter String"
     _options["informative-text"] = options[:prompt] || ""
@@ -11,7 +11,7 @@ module Dialog
     dialog(_options,&block)
   end
   def request_secure_string(options = Hash.new,&block)
-    _options = self.default_hash
+    _options = self.default_hash(options)
     _options["type"] = "secure-inputbox"
     _options["title"] = options[:title] || "Enter Password"
     _options["informative-text"] = options[:prompt] || ""
@@ -25,12 +25,19 @@ module Dialog
     elsif items.length == 1 then
       block_given? yield items[0] : return items[0]
     else
-      _options = self.default_hash
+      _options = self.default_hash(options)
       _options["type"] = "dropdown"
       _options["title"] = options[:title] || "Select Item"
       _options["text"] = options[:prompt] || ""
       dialog(_options,&block)
     end
+  end
+  def yes_no(options = Hash.new,&block)
+    _options = Hash.new
+    _options["type"] = "yesno-msgbox"
+    _options["title"] = options[:title] || "Yes or No?"
+    _options["text"] =  options[:prompt] || "Please answer Yes or No."
+    _options["informative-text"] = options[:information]
   end
 
   private
@@ -39,13 +46,15 @@ module Dialog
     type = options.delete("type")
     str = ""
     options.each_pair do |key, value|
-      str << " --#{e_sh key} "
-      str << Array(value).map { |s| e_sh s }.join(" ")
+      unless value == nil do
+        str << " --#{e_sh key} "
+        str << Array(value).map { |s| e_sh s }.join(" ")
+      end
     end
     cd = ENV['TM_SUPPORT_PATH'] + '/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog'
     result = %x{#{e_sh cd} 2>/dev/console #{e_sh type} #{str} --float}
     return_value, result = result.to_a.map{|line| line.chomp}
-    if return_value == "Cancel" then
+    if return_value == options["button2"] then
       if block_given? then
         raise SystemExit
       else
@@ -55,11 +64,11 @@ module Dialog
       block_given? ? yield result : result
     end
   end
-  def default_hash
-    return {
-      "string-output" => "",
-      "button1" => "Ok",
-      "button2" => "Cancel"
-    }
+  def default_hash(user_options = Hash.new)
+    options = Hash.new
+    options["string-output"] == ""
+    options["button1"] = user_options[:button1] || "Okay"
+    options["button2"] = user_options[:button2] || "Cancel"
+    options
   end
 end

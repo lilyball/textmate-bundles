f3db8fbde780180d6866444db9037ac2120a470e
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-28 01:19:19 +0000

    • grab extension from language grammar in use and send to pastie
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3876 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/TextMate.tmbundle/Support/paste_to_pastie.rb
+++ Bundles/TextMate.tmbundle/Support/paste_to_pastie.rb
@@ -1,9 +1,37 @@
 #!/usr/bin/env ruby -rjcode -Ku
 
+$: << ENV['TM_SUPPORT_PATH'] + '/lib'
+
 require 'cgi'
 require 'progress'
+require "textmate"
+require "plist"
 require 'fileutils'
 require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/doctohtml.rb"
+
+def find_language_ext
+  ext = File.extname(ENV['TM_FILENAME'].to_s).sub(/\A\./, '')
+  return ext unless ext.empty?
+
+	bundle_dirs = [
+		File.expand_path('~/Library/Application Support/TextMate/Bundles'),
+		'/Library/Application Support/TextMate/Bundles',
+		TextMate.app_path + '/Contents/SharedSupport/Bundles'
+	]
+
+  scope = ENV['TM_SCOPE'].to_s.sub(/ .*/, '')
+  bundle_dirs.each do |dir|
+    Dir.glob(dir + '/*.tmbundle/Syntaxes/*.{plist,tmLanguage}') do |filename|
+      File.open(filename) do |io|
+        plist = PropertyList.load(io)
+        if scope == plist['scopeName'].to_s then
+          return Array(plist['fileTypes']).first.to_s
+        end
+      end
+    end
+  end
+  return 'txt'
+end
 
 TextMate.call_with_progress(:title => "Paste to Pastie", :message => "Contacting Server “pastie.caboo.se”…") do
   text_file, html_file = `/usr/bin/mktemp -t tm_paste && /usr/bin/mktemp -t tm_paste`.split("\n")
@@ -17,7 +45,7 @@ TextMate.call_with_progress(:title => "Paste to Pastie", :message => "Contacting
   open(html_file, "w") { |io| io.write(html) }
 
   author = "#{`niutil -readprop / "/users/$USER" realname`.chomp} (#{ENV['USER']})"
-  ext = File.extname(ENV['TM_FILENAME'].to_s).sub(/\A\./, '')
+  ext = find_language_ext
 
   print %x{
     curl http://pastie.caboo.se/pastes/create \

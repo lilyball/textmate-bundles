2065359b5a099529e33efe04b08dca9635a8b0c1
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-28 04:26:50 +0000

    • remove empty tags (<>) from the plain text variant • send HTML variant as gzipped to pastie • prefer file extension from language grammar over current file name extension (since Edit in TextMate will not always give the file a type specific extension)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3877 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/TextMate.tmbundle/Support/paste_to_pastie.rb
+++ Bundles/TextMate.tmbundle/Support/paste_to_pastie.rb
@@ -3,6 +3,7 @@
 $: << ENV['TM_SUPPORT_PATH'] + '/lib'
 
 require 'cgi'
+require 'zlib'
 require 'progress'
 require "textmate"
 require "plist"
@@ -10,27 +11,28 @@ require 'fileutils'
 require "#{ENV['TM_BUNDLE_SUPPORT']}/lib/doctohtml.rb"
 
 def find_language_ext
-  ext = File.extname(ENV['TM_FILENAME'].to_s).sub(/\A\./, '')
-  return ext unless ext.empty?
-
 	bundle_dirs = [
 		File.expand_path('~/Library/Application Support/TextMate/Bundles'),
 		'/Library/Application Support/TextMate/Bundles',
 		TextMate.app_path + '/Contents/SharedSupport/Bundles'
 	]
 
-  scope = ENV['TM_SCOPE'].to_s.sub(/ .*/, '')
-  bundle_dirs.each do |dir|
-    Dir.glob(dir + '/*.tmbundle/Syntaxes/*.{plist,tmLanguage}') do |filename|
-      File.open(filename) do |io|
-        plist = PropertyList.load(io)
-        if scope == plist['scopeName'].to_s then
-          return Array(plist['fileTypes']).first.to_s
+  if scope = ENV['TM_SCOPE'] then
+    scope = scope.split(' ').first
+    bundle_dirs.each do |dir|
+      Dir.glob(dir + '/*.tmbundle/Syntaxes/*.{plist,tmLanguage}') do |filename|
+        File.open(filename) do |io|
+          plist = PropertyList.load(io)
+          if scope == plist['scopeName'].to_s then
+            return Array(plist['fileTypes']).first || 'txt'
+          end
         end
       end
     end
   end
-  return 'txt'
+
+  ext = File.extname(ENV['TM_FILENAME'].to_s).sub(/\A\./, '')
+  ext.empty? ? 'txt' : ext
 end
 
 TextMate.call_with_progress(:title => "Paste to Pastie", :message => "Contacting Server “pastie.caboo.se”…") do
@@ -38,11 +40,13 @@ TextMate.call_with_progress(:title => "Paste to Pastie", :message => "Contacting
 
   xml = STDIN.read
 
-  text = CGI::unescapeHTML(xml.gsub(/<[^>]+>/, ''))
-  open(text_file, "w") { |io| io.write(text) }
+  open(text_file, 'w') do |io|
+    io.write CGI::unescapeHTML(xml.gsub(/<[^>]*>/, ''))
+  end
 
-  html = document_to_html(xml)
-  open(html_file, "w") { |io| io.write(html) }
+  Zlib::GzipWriter.open(html_file) do |gz|
+    gz.write document_to_html(xml)
+  end
 
   author = "#{`niutil -readprop / "/users/$USER" realname`.chomp} (#{ENV['USER']})"
   ext = find_language_ext
@@ -55,7 +59,7 @@ TextMate.call_with_progress(:title => "Paste to Pastie", :message => "Contacting
     	-F "paste[display_name]=#{author}" \
     	-F "paste[file_extension]=#{ext}" \
     	-F "paste[body]=<#{text_file}" \
-    	-F "paste[textmate_html]=<#{html_file}"
+    	-F "paste[textmate_html_gz]=<#{html_file}"
   }
 
   File.unlink(text_file)

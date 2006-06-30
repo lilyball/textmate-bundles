2b36df6bae3bffe1b33d5577feae84bd1b810bdb
cskiadas <cskiadas@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-06-30 14:08:39 +0000

    No need for the #!/use/bin... line
    git-svn-id: http://macromates.com/svn/Bundles/trunk@3898 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/escape.rb
+++ Support/lib/escape.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 # properly escapes argument for inclusion in a shell command
 def e_sh(argument)
 	argument.gsub(/[^a-zA-Z0-9_.]/, "\\\\\\0")
@@ -7,7 +5,7 @@ end
 
 # properly escapes a string for insertion as a snippet
 def e_sn(str)
-	str.to_s.gsub(/[$`\\]/, "\\\\\\0")
+	str.to_s.gsub(/[\$\`\\]/, "\\\\\\0")
 end
 
 # URL escape a string but preserve slashes (idea being we have a file system path that we want to use with file://)

f1909b80b939679b55678da4b3cdd7772380c032
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-07-14 13:34:31 +0000

    • All diff commands should try selected files before trying the active path. Multiple selections are supported.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@4212 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/svn_diff.rb
+++ Bundles/Subversion.tmbundle/Support/svn_diff.rb
@@ -1,33 +1,39 @@
 #!/usr/bin/env ruby -w
 
 $LOAD_PATH << ENV['TM_SUPPORT_PATH'] + "/lib"
+$LOAD_PATH << ENV['TM_SUPPORT_PATH'] + "/bin"
 require 'progress'
+require 'exit_codes'
+require 'shelltokenize'	# for TextMate::selected_paths_array
 
 module Subversion
   def Subversion.diff_active_file( revision, command )
     svn         = ENV['TM_SVN'] || 'svn'
     diff_cmd    = ENV['TM_SVN_DIFF_CMD']
     diff_arg    = diff_cmd ? "--diff-cmd #{diff_cmd}" : ''
+#	target_path = ENV['TM_FILEPATH']
 
-    target_path = ENV['TM_FILEPATH']
-    svn_header  = /\AIndex: #{Regexp.escape(target_path)}\n=+\n\z/
+	TextMate::selected_paths_array.each do |target_path|
+	    svn_header  = /\AIndex: #{Regexp.escape(target_path)}\n=+\n\z/
 
-    TextMate::call_with_progress(:title => command, :message => "Accessing Subversion Repository…") do
-      res = %x{"#{svn}" 2>&1 diff "-r#{revision}" #{diff_arg} "#{target_path}"}
+	    TextMate::call_with_progress(:title => command, :message => "Accessing Subversion Repository…") do
+	      res = %x{"#{svn}" 2>&1 diff "-r#{revision}" #{diff_arg} "#{target_path}"}
 
-      if $? != 0
-        print res
-        exit 206      # The command failed; show its output as a tooltip
-      elsif res.empty?
-        puts "No differences found."
-        exit 206      # Report lack of differences as a tooltip
-      elsif (custom_diff? or diff_cmd) and res =~ svn_header
-        exit 200      # Suppress output, as we only got a svn header (so likely diff-cmd opened its own window)
-      else
-        print res
-      end
-    end
-
+	      if $? != 0
+			# The command failed; show its output as a tooltip
+	        print res
+			TextMate::exit_show_tool_tip
+	      elsif res.empty?
+			# Report lack of differences as a tooltip
+	        TextMate::exit_show_tool_tip("No differences found.")
+	      elsif (custom_diff? or diff_cmd) and res =~ svn_header
+			# Suppress output, as we only got a svn header (so likely diff-cmd opened its own window)
+	        TextMate::exit_discard
+	      else
+	        print res
+	      end
+	    end
+	end
   end
 
   # Returns true if ~/.subversion/config contains an uncommented entry for diff-cmd

faf6a409802b58353be759b04b87aa192dd18c57
bchoate <bchoate@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-07-17 18:40:44 +0000

    Correction for loading last slide.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@4384 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/S5.tmbundle/Support/lib/s5.rb
+++ Bundles/S5.tmbundle/Support/lib/s5.rb
@@ -53,6 +53,7 @@ class S5 < String
       slide += line + "\n"
     end
     @slides.push(slide) unless slide.strip.empty?
+    @current_slide_number = slide_num + 1 unless @current_slide_num
 
     require 'cgi'
     # set values for template

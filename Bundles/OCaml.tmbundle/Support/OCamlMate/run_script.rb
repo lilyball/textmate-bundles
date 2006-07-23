030b78aa7f349ee2a9b8fe8fa880208aa2ec969f
dpowers <dpowers@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-07-23 17:11:47 +0000

    improved OCaml Run command based on ruby/pymate
    git-svn-id: http://macromates.com/svn/Bundles/trunk@4482 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/OCaml.tmbundle/Support/OCamlMate/run_script.rb
@@ -0,0 +1,108 @@
+#!/usr/bin/env ruby
+
+require "#{ENV["TM_SUPPORT_PATH"]}/lib/escape"
+
+require 'open3'
+require 'cgi'
+require 'fcntl'
+
+$RUBYMATE_VERSION = "$Revision: 4106 $"
+
+def esc(str)
+  CGI.escapeHTML(str).gsub(/\n/, '<br/>')
+end
+
+class UserScript
+  def initialize
+    @ocamlc = ARGV[0]
+    @srcfile = ARGV[1]
+    @dstfile = ARGV[2]
+
+    if ENV.has_key? 'TM_FILEPATH' then
+      @path = ENV['TM_FILEPATH']
+      @display_name = File.basename(@path)
+    else
+      @path = '-'
+      @display_name = 'untitled'
+    end
+  end
+
+
+  def compile
+    # compile it
+    output = `\"#{@ocamlc}\" -o \"#{@dstfile}\" str.cma unix.cma \"#{@srcfile}\" 2>&1`
+    
+    if output != ""
+      output.each_line() do |line|
+        if line =~ /^File "(?:.*?)", (line ([0-9]+), characters [0-9]+-[0-9]+):/
+          location, line = $1, $2
+          print "<span>File \"#{@path}\", <a style=\"color: blue;\" href=\"txmt://open?url=file://#{e_url(@path)}&line=#{line}\">#{location}</a>:</span><br/>"
+        else
+          print esc(line)
+        end
+      end
+      
+      exit 1
+    end
+  end
+
+
+  def run
+    # run it if the compile suceeded
+    stdin, stdout, stderr = Open3.popen3(@dstfile)
+    Thread.new { stdin.write @content; stdin.close } unless ENV.has_key? 'TM_FILEPATH'
+
+    [stdout, stderr]
+  end
+
+  attr_reader :display_name, :path
+  
+end
+
+error = ""
+STDOUT.sync = true
+
+script = UserScript.new
+map = {
+  'SCRIPT_NAME'       => script.display_name,
+  'BUNDLE_SUPPORT'    => "tm-file://#{ENV['TM_BUNDLE_SUPPORT'].gsub(/ /, '%20')}",
+}
+puts DATA.read.gsub(/\$\{([^}]+)\}/) { |m| map[$1] }
+
+script.compile
+stdout, stderr = script.run
+descriptors = [ stdout, stderr ]
+
+descriptors.each { |fd| fd.fcntl(Fcntl::F_SETFL, Fcntl::O_NONBLOCK) }
+until descriptors.empty?
+  select(descriptors).shift.each do |io|
+    str = io.read
+    if str.to_s.empty? then
+      descriptors.delete io
+      io.close
+    elsif io == stdout then
+      print esc(str)
+    elsif io == stderr then
+      print "<span style='color: red'>#{esc str}</span>"
+    elsif io == stack_dump then
+      error << str
+    end
+  end
+end
+
+puts '</div></pre></div>'
+puts error
+puts '<div id="exception_report" class="framed">Program exited.</div>'
+puts '</body></html>'
+
+__END__
+<html>
+  <head>
+    <title>OCamlMate</title>
+    <link rel="stylesheet" href="${BUNDLE_SUPPORT}/pastel.css" type="text/css">
+  </head>
+<body>
+  <div id="script_output" class="framed">
+  <pre><strong>OCamlMate</strong>
+<strong>${SCRIPT_NAME}</strong>
+<div id="actual_output" style="word-wrap: break-word;">

d2b8724c47d15e19e5d5ef0ab93e6905de76f4db
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-07-29 13:38:43 +0000

    • do not shell-escape the individual bytes in multi-byte characters • make dpowers happy and cut down the number of escapes in the replacement string
    git-svn-id: http://macromates.com/svn/Bundles/trunk@4522 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/escape.rb
+++ Support/lib/escape.rb
@@ -1,16 +1,16 @@
 # escape text to make it useable in a shell script as one “word” (string)
 def e_sh(str)
-	str.to_s.gsub(/[^a-zA-Z0-9_.\/]/, "\\\\\\0")
+	str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\x7F-\xFF])/, '\\')
 end
 
 # escape text for use in a TextMate snippet
 def e_sn(str)
-	str.to_s.gsub(/[$`\\]/, "\\\\\\0")
+	str.to_s.gsub(/(?=[$`\\])/, '\\')
 end
 
 # escape text for use in an AppleScript string
 def e_as(str)
-	str.to_s.gsub(/["\\]/, "\\\\\\0")
+	str.to_s.gsub(/(?=["\\])/, '\\')
 end
 
 # URL escape a string but preserve slashes (idea being we have a file system path that we want to use with file://)

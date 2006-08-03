48f9b13461dc4a29b39dc346fc71ffdd2ac1de99
bchoate <bchoate@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-08-03 06:26:23 +0000

    Support for additional placeholders.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@4544 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/HTML.tmbundle/Support/tminclude.rb
+++ Bundles/HTML.tmbundle/Support/tminclude.rb
@@ -16,6 +16,10 @@ module TextMate
       @@argument_regexp = /\s*#([^#]+?)#\s*=\s*(?:(["'])([^\2]*?)\2|(\S+))\s*/m
       @@depth = 0
       @@global_vars = {}
+      @@time = Time.now
+      @@ctime = nil
+      @@mtime = nil
+      @@docsize = nil
 
       def parse_arguments(arg_str, vars)
         arg_str.scan(@@argument_regexp) do | var, quote, val, val2 |
@@ -24,7 +28,7 @@ module TextMate
       end
 
       def replace_variables(content, vars)
-        content.gsub(/(#(?:(?:\w| )+?)#)/) do | expr |
+        content.gsub(/(#\w+(?:\s+[^#]+?)?#)/m) do | expr |
           # normalize variable... strip '#' prefix/suffix/parameter, downcase
           var = expr.gsub(/^\#|\#$/, '')
           if var =~ / +(.+)/
@@ -36,7 +40,16 @@ module TextMate
           var.downcase!
           out = vars[var] || @@global_vars[var] || expr
           if out.kind_of?(Method)
-            out.arity == 0 ? out.call : out.call(attribute)
+            case out.arity
+              when 0
+                out.call
+              when 1
+                out.call(attribute)
+              when -1
+                attribute.nil? ? out.call : out.call(attribute)
+              else
+                expr
+            end
           else
             out
           end
@@ -96,23 +109,76 @@ module TextMate
         end
         # Here are some of BBEdit's global variables. We may choose
         # to cherry pick these for support...
+        @@global_vars['dont_update'] = ''
         @@global_vars['localpath'] = ENV['TM_FILEPATH']
         @@global_vars['shortusername'] = ENV['USER']
         # lazily invoke this one, since it has cost associated with it...
         @@global_vars['username'] = method("var_username")
 
-        # abbrev_date # format: Sun, Aug 15, 2004
-        # base
-        # basename # 'test' for file test.html; 'test.foo' for test.foo.html
-        # base_url, bodytext, charset,
-        # compdate # format: 15-Aug-04
-        # creationdate # format: 15-Aug-04
-        # creationtime
-        # dirpath, docsize, doctitle, dont_update, file_extension, filename,
-        # generator, gmtime, language, link, localtime, longdate, machine,
-        # meta, modifieddate, modifiedtime, monthdaynum, path, prefix,
-        # real_url, relative, root, rootpath, server, shortdate, 
-        # time, title, yearnum
+        @@global_vars['localtime'] = method("var_localtime")
+        @@global_vars['gmtime'] = method("var_gmtime")
+        @@global_vars['longdate'] = @@time.strftime("%A, %B %e, %Y").sub(/  /, ' ')
+        @@global_vars['compdate'] = @@time.strftime("%d-%b-%y")
+        @@global_vars['monthnum'] = @@time.strftime("%m")
+        @@global_vars['monthdaynum'] = @@time.strftime("%d")
+        @@global_vars['shortdate'] = @@time.strftime("%m/%d/%y")
+        @@global_vars['abbrev_date'] = @@time.strftime("%a, %b %e, %Y").sub(/  /, ' ')
+        @@global_vars['yearnum'] = @@time.year
+        @@global_vars['yearnum'] = @@time.strftime("%X")
+        @@global_vars['generator'] = "TextMate"
+        @@global_vars['docsize'] = @@docsize  # well... not really
+
+        if filename = ENV['TM_FILENAME']
+          @@global_vars['filename'] = filename
+          @@global_vars['basename'] = filename.sub(/\.\w+$/, '')
+          if filename =~ /(\.\w+$)/
+            @@global_vars['file_extension'] = $1
+          else
+            @@global_vars['file_extension'] = ''
+          end
+
+          if ctime = File.ctime(ENV['TM_FILEPATH'])
+            @@ctime = ctime
+            @@global_vars['creationdate'] = method('var_creationdate')
+            @@global_vars['creationtime'] = method('var_creationtime')
+          end
+          if mtime = File.mtime(ENV['TM_FILEPATH'])
+            @@mtime = mtime
+            @@global_vars['modifieddate'] = method('var_modifieddate')
+            @@global_vars['modifiedtime'] = method('var_modifiedtime')
+          end
+        end
+
+        # Unsupported...
+        # base, base_url, bodytext, charset, dirpath, doctitle, language,
+        # link, machine, meta, path, prefix, real_url, relative, root,
+        # rootpath, server, title
+      end
+
+      # Dynamic variables
+
+      def var_creationtime(format = "%c")
+        @@ctime ? @@ctime.strftime(format) : nil
+      end
+
+      def var_creationdate(format = "%d-%b-%y")
+        @@ctime ? @@ctime.strftime(format) : nil
+      end
+
+      def var_modifiedtime(format = "%c")
+        @@mtime ? @@mtime.strftime(format) : nil
+      end
+
+      def var_modifieddate(format = "%d-%b-%y")
+        @@mtime ? @@mtime.strftime(format) : nil
+      end
+
+      def var_localtime(format = "%c")
+        @@time.strftime(format)
+      end
+
+      def var_gmtime(format = "%c")
+        @@time.gmtime.strftime(format)
       end
 
       def var_username
@@ -146,7 +212,12 @@ module TextMate
         vars = {}
         init_global_vars()
         doc = STDIN.readlines.join
+        @@docsize = doc.length
         @@depth = 1
+        if doc =~ /\#dont_update\#/
+          print "This document cannot be updated because it is protected."
+          exit 206
+        end
         print process_document(doc, vars)
       end
 

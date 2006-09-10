35fb7ad4228971423ba3c5f0d34ffd5e8efd348d
Soryu <Soryu@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-09-10 16:13:43 +0000

    • Move the hyperlinks to the Source to the ‘Line’ column and thus make the ‘Code’ column selectable.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5151 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/format_blame.rb
+++ Bundles/Subversion.tmbundle/Support/format_blame.rb
@@ -86,12 +86,12 @@ begin
          end
          
          puts '<tr>'
-         puts  '<td class="linecol"><span'+ line_id.to_s + '>'+ linecount.to_s + "</span></td>\n" +
+         puts  '<td class="linecol"><span'+ line_id.to_s + '><a href="' +
+            make_tm_link( $full_file, linecount) +'"'+$close+'>'+ linecount.to_s + "</a></span></td>\n" +
                '<td class="revcol' +curr_add+'" title="'+ formated_date( $3 ) + (revision_comment[revision].nil? ? '' : "\n" + html_escape(revision_comment[revision])) + '">' + $1 + "</td>\n" +
                '<td class="namecol'+curr_add+'" >' + $2 + "</td>\n" +
-               '<td class="codecol'+curr_add+color+'" ><a href="' +
-                  make_tm_link( $full_file, linecount) +'"'+$close+'>'+ htmlize( $4 ) +
-               "</a></td></tr>\n\n"
+               '<td class="codecol'+curr_add+color+'" >'+ htmlize( $4 ) +
+               "</td></tr>\n\n"
 
          linecount += 1
          

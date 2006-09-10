f4b203c1fcd226ef7231cff215989c48c0cf2ff6
Soryu <Soryu@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-09-10 14:15:16 +0000

    • The tooltip with the commit text now only pops up when hovering the mouse over the revision column
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5149 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/format_blame.rb
+++ Bundles/Subversion.tmbundle/Support/format_blame.rb
@@ -88,8 +88,8 @@ begin
          puts '<tr>'
          puts  '<td class="linecol"><span'+ line_id.to_s + '>'+ linecount.to_s + "</span></td>\n" +
                '<td class="revcol' +curr_add+'" title="'+ formated_date( $3 ) + (revision_comment[revision].nil? ? '' : "\n" + html_escape(revision_comment[revision])) + '">' + $1 + "</td>\n" +
-               '<td class="namecol'+curr_add+'" title="'+ formated_date( $3 ) + (revision_comment[revision].nil? ? '' : "\n" + html_escape(revision_comment[revision])) + '">' + $2 + "</td>\n" +
-               '<td class="codecol'+curr_add+color+'" title="'+ formated_date( $3 ) + (revision_comment[revision].nil? ? '' : "\n" + html_escape(revision_comment[revision])) + '"><a href="' +
+               '<td class="namecol'+curr_add+'" >' + $2 + "</td>\n" +
+               '<td class="codecol'+curr_add+color+'" ><a href="' +
                   make_tm_link( $full_file, linecount) +'"'+$close+'>'+ htmlize( $4 ) +
                "</a></td></tr>\n\n"
 

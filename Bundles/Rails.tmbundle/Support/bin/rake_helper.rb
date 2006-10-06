1ccebb7c8cffb737e9e4a8a87bf03358b1b7f983
canadaduane <canadaduane@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-10-06 06:50:23 +0000

    Added 'Load Fixtures' rake command. The 'Migrate Database' menu is now just 'Database'.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5382 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Rails.tmbundle/Support/bin/rake_helper.rb
+++ Bundles/Rails.tmbundle/Support/bin/rake_helper.rb
@@ -58,33 +58,38 @@ puts "<span style='color:red; font-size: 1.2em'>#{command}</span><br>"
 styles = ["table {padding-left: 2em;}", "td {padding-right: 1.5em;}", ".time {color: #f99; font-weight: bold}"]
 
 report = "<style>#{styles.join}</style>"
-report += "<h1>Migration Report</h1>"
-inside_table = false
-output.each_line do |line|
-  case line
-    when /^==\s+/
-      # Replace == headings with <h2></h2>
-      line.gsub!(/^==\s+([^=]+)[=\s]*$/, "<h2>\\1</h2>")
-      # Replace parenthetical times with time class
-      line.gsub!(/(\([\d\.]+s\))/, "<span class=\"time\">\\1</span>")
-      # Show details inside table
-      if !inside_table
-        line << "<table>"
-      else
-        line << "</table>"
-      end
-      inside_table = !inside_table
-    when /^--\s+(.+)$/
-      # Show command inside table cell
-      line = "<tr><td>#{$1}</td>"
-    when /^\s+->(.+)$/
-      # Show execution time inside table cell
-      line = "<td class=\"time\">#{$1}</td></tr>\n"
-    else
-      line += "<br/>"
-  end
-  report << line
-end
-report << "</table>" if inside_table
 
+case task
+when "db:migrate", "migrate"
+  report += "<h1>Migration Report</h1>"
+  inside_table = false
+  output.each_line do |line|
+    case line
+      when /^==\s+/
+        # Replace == headings with <h2></h2>
+        line.gsub!(/^==\s+([^=]+)[=\s]*$/, "<h2>\\1</h2>")
+        # Replace parenthetical times with time class
+        line.gsub!(/(\([\d\.]+s\))/, "<span class=\"time\">\\1</span>")
+        # Show details inside table
+        if !inside_table
+          line << "<table>"
+        else
+          line << "</table>"
+        end
+        inside_table = !inside_table
+      when /^--\s+(.+)$/
+        # Show command inside table cell
+        line = "<tr><td>#{$1}</td>"
+      when /^\s+->(.+)$/
+        # Show execution time inside table cell
+        line = "<td class=\"time\">#{$1}</td></tr>\n"
+      else
+        line += "<br/>"
+    end
+    report << line
+  end
+  report << "</table>" if inside_table
+else
+  report += output.to_a.join("<br>")
+end
 puts report

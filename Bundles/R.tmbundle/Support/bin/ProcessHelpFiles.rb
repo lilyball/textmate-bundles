d57be926683b69509373bc73fbbec1dd68be7976
cskiadas <cskiadas@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-10-07 00:38:56 +0000

    Adding lots of snippets in lots of nice little menus. More to come.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5386 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/R.tmbundle/Support/bin/ProcessHelpFiles.rb
+++ Bundles/R.tmbundle/Support/bin/ProcessHelpFiles.rb
@@ -11,7 +11,18 @@ resources.each do |file|
   results << terms[0][0] unless terms.empty?
   #.map{|line| line.gsub(/\n|(  )/,"")}
 end
-results.gsub!(/^(?:(?:[#!\(\{]|(?:[\w.]+[\$\[\s]+)).*|[\w.:]+|\"[\w.]*\".*|.*value|)\n/,"")
-results.gsub!(/^\s+/,"")
-results.gsub!(/\n(?![\w.]+\()/," ")
+results.gsub!(/\s*#+(\s.*|)$/,"")   # Removes comments. Need to watch out for something like "#"
+results.gsub!(/^\s*[\w.:]+\s*\n/,"") # Removes stupid lines like a:b
+results.gsub!(/^(\.|<)\w.*|.*(\?topic|@name|survexp\.uswhite).*/,"") # removes function names starting with a dot, as well as other random stuff
+results.gsub!(/.*value\n/,"") # Removes lines like: tsp(x) <- value
+results.gsub!(/^\w\s+.*\n/,"") # Removes lines like: R CMD ...
+results.gsub!(/^[\w.]*(?:\[|\$).*\n/,"")  # Removes lines starting with: x[[...]] or x$name
+results.gsub!(/^[\w.:+-\/><\s;!&|"]*\n/,"") # Removes things like: time1 + time2
+results.gsub!(/^(?:if|for|while|function)\(.*\n/,"") # Removes definitions of if/for/while/function
+results.gsub!(/\n(?![\w.]+\()/," ") # Brings together arguments on different lines
+results.gsub!(/[\t ]+/," ")
+results.gsub!(/\s+$/,"")
+puts "Suspicious lines:" 
+pp results.scan(/^(.*[^ \w\(].\)) +(\w.*)/) #,"\\1\n\\2" # Try to fix some lines that got together
+puts "=" * 40
 puts results.split("\n").sort.uniq.join("\n")

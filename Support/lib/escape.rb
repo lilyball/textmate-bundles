508f050d13a3383b3f1b1d55ca641ceb978deaea
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-10-24 18:50:02 +0000

    • escape line feeds as a quoted linefeed (since putting \ before a linefeed just means the line should continue on the next with no \n between the two)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5607 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/escape.rb
+++ Support/lib/escape.rb
@@ -1,6 +1,6 @@
 # escape text to make it useable in a shell script as one “word” (string)
 def e_sh(str)
-	str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\x7F-\xFF])/, '\\')
+	str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\x7F-\xFF\n])/, '\\').gsub(/\n/, "'\n'")
 end
 
 # escape text for use in a TextMate snippet

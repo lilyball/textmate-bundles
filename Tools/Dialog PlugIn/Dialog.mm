5efd2a21f30e69c50e80ecaa65e958aca2b1199c
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-10-25 23:32:09 +0000

    • handle exceptions thrown when instantiating the nib and make sure tm_dialog always will exist immediately, when it fails to show a window
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5630 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/Dialog PlugIn/Dialog.mm
+++ Tools/Dialog PlugIn/Dialog.mm
@@ -6,7 +6,6 @@ NSLock* Lock = [NSLock new];
 {
 }
 - (id)initWithPlugInController:(id <TMPlugInController>)aController;
-- (void)dealloc;
 @end
 
 @interface NibLoader : NSObject
@@ -16,6 +15,7 @@ NSLock* Lock = [NSLock new];
 	NSWindow* window;
 	BOOL isModal;
 	BOOL center;
+	BOOL didLock;
 }
 - (NSDictionary*)instantiateNib:(NSNib*)aNib;
 @end
@@ -28,7 +28,6 @@ NSLock* Lock = [NSLock new];
 		parameters = [someParameters retain];
 		isModal = flag;
 		center = shouldCenter;
-		[Lock lock];
 	}
 	return self;
 }
@@ -42,8 +41,20 @@ NSLock* Lock = [NSLock new];
 	[topLevelObjects release];
 	[parameters release];
 
-	[Lock unlock];
 	[super dealloc];
+}
+
+- (void)lock
+{
+	[Lock lock];
+	didLock = YES;
+}
+
+- (void)unlock
+{
+	if(didLock)
+		[Lock unlock];
+	didLock = NO;
 }
 
 - (void)setWindow:(NSWindow*)aWindow
@@ -61,6 +72,7 @@ NSLock* Lock = [NSLock new];
 {
 	enumerate(topLevelObjects, id object)
 	{
+		// if we do not manually unbind, the object in the nib will keep us retained, and thus we will never reach dealloc
 		if([object isKindOfClass:[NSObjectController class]])
 			[object unbind:@"contentObject"];
 	}
@@ -69,6 +81,7 @@ NSLock* Lock = [NSLock new];
 	if(isModal)
 		[NSApp stopModal];
 
+	[self unlock];
 	[self autorelease];
 }
 
@@ -93,6 +106,10 @@ NSLock* Lock = [NSLock new];
 	[window orderOut:self];
 	[self cleanupAndRelease];
 
+	// remove ourself as observer, since there is a slight chance this object will never be released (when nib instantiation throws an exception)
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+
+	// post dummy event, since the system has a tendency to stall the next event, after replying to a DO message where the receiver has disappeared, posting this dummy event seems to solve it
 	[NSApp postEvent:[NSEvent otherEventWithType:NSApplicationDefined location:NSZeroPoint modifierFlags:0 timestamp:0.0f windowNumber:0 context:nil subtype:0 data1:0 data2:0] atStart:NO];
 }
 
@@ -100,10 +117,13 @@ NSLock* Lock = [NSLock new];
 {
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(connectionDidDie:) name:NSPortDidBecomeInvalidNotification object:nil];
 
-	BOOL didInstantiate = [aNib instantiateNibWithOwner:self topLevelObjects:&topLevelObjects];
-	if(!didInstantiate)
-	{
-		NSLog(@"%s failed to instantiate nib", _cmd);
+	BOOL didInstantiate = NO;
+	@try {
+	 	didInstantiate = [aNib instantiateNibWithOwner:self topLevelObjects:&topLevelObjects];
+	}
+	@catch(NSException* e) {
+		// note that our retain count is actually too high if we reach this branch -- so no RAII idioms for Cocoa, which is why we have the didLock variable, etc.
+		NSLog(@"%s failed to instantiate nib (%@)", _cmd, [e reason]);
 	}
 
 	[topLevelObjects retain];
@@ -112,24 +132,33 @@ NSLock* Lock = [NSLock new];
 		if([object isKindOfClass:[NSWindow class]])
 			[self setWindow:object];
 	}
-
-	if(center)
+	
+	if(window)
 	{
-		if(NSWindow* keyWindow = [NSApp keyWindow])
+		[self lock];
+
+		if(center)
 		{
-			NSRect frame = [window frame], parentFrame = [keyWindow frame];
-			[window setFrame:NSMakeRect(NSMidX(parentFrame) - 0.5 * NSWidth(frame), NSMidY(parentFrame) - 0.5 * NSHeight(frame), NSWidth(frame), NSHeight(frame)) display:NO];
+			if(NSWindow* keyWindow = [NSApp keyWindow])
+			{
+				NSRect frame = [window frame], parentFrame = [keyWindow frame];
+				[window setFrame:NSMakeRect(NSMidX(parentFrame) - 0.5 * NSWidth(frame), NSMidY(parentFrame) - 0.5 * NSHeight(frame), NSWidth(frame), NSHeight(frame)) display:NO];
+			}
+			else
+			{
+				[window center];
+			}
 		}
-		else
-		{
-			[window center];
-		}
+
+		[window makeKeyAndOrderFront:self];
+		if(isModal && window)
+			[NSApp runModalForWindow:window];
 	}
-
-	[window makeKeyAndOrderFront:self];
-
-	if(isModal && window)
-		[NSApp runModalForWindow:window];
+	else
+	{
+		NSLog(@"%s didn't find a window in nib", _cmd);
+		[self cleanupAndRelease];
+	}
 
 	return parameters;
 }
@@ -142,11 +171,6 @@ NSLock* Lock = [NSLock new];
 	if(self = [super init])
 		[NSThread detachNewThreadSelector:@selector(vendObject:) toTarget:self withObject:nil];
 	return self;
-}
-
-- (void)dealloc
-{
-	[super dealloc];
 }
 
 - (int)textMateDialogServerProtocolVersion
@@ -170,6 +194,8 @@ NSLock* Lock = [NSLock new];
 	}
 
 	NibLoader* nibOwner = [[NibLoader alloc] initWithParameters:someParameters modal:flag center:shouldCenter];
+	if(!nibOwner)
+		NSLog(@"%s couldn't create nib loader", _cmd);
 	[nibOwner performSelectorOnMainThread:@selector(instantiateNib:) withObject:nib waitUntilDone:YES];
 	[Lock lock];
 	[Lock unlock];

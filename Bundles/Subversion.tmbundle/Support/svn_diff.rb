88d900e8524fa3359fa06aedc054c877f83c5e45
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-10-29 11:31:10 +0000

    • do not use exit_show_tool_tip (or discard) inside the loop which iterates over selected files (could cause diff to be shown as tool tip or no diff to be shown at all, if first selected file had no differences) • convert source to soft tabs with a tab size of 2 (so that’s why so many lines have been changed — it was a mix of tabs of size 4 and soft tabs, so it really had to be done)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5713 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/svn_diff.rb
+++ Bundles/Subversion.tmbundle/Support/svn_diff.rb
@@ -4,36 +4,37 @@ $LOAD_PATH << ENV['TM_SUPPORT_PATH'] + "/lib"
 $LOAD_PATH << ENV['TM_SUPPORT_PATH'] + "/bin"
 require 'progress'
 require 'exit_codes'
-require 'shelltokenize'	# for TextMate::selected_paths_array
+require 'shelltokenize' # for TextMate::selected_paths_array
 
 module Subversion
   def Subversion.diff_active_file( revision, command )
     svn         = ENV['TM_SVN'] || 'svn'
     diff_cmd    = ENV['TM_SVN_DIFF_CMD']
     diff_arg    = diff_cmd ? "--diff-cmd #{diff_cmd}" : ''
-#	target_path = ENV['TM_FILEPATH']
+#   target_path = ENV['TM_FILEPATH']
 
-	TextMate::selected_paths_array.each do |target_path|
-	    svn_header  = /\AIndex: #{Regexp.escape(target_path)}\n=+\n\z/
+    error       = ""
+    result      = ""
 
-	    TextMate::call_with_progress(:title => command, :message => "Accessing Subversion Repository…") do
-	      res = %x{"#{svn}" 2>&1 diff "-r#{revision}" #{diff_arg} "#{target_path}"}
+    TextMate::call_with_progress(:title => command, :message => "Accessing Subversion Repository…") do
+      TextMate::selected_paths_array.each do |target_path|
+        svn_header  = /\AIndex: #{Regexp.escape(target_path)}\n=+\n\z/
+        res = %x{"#{svn}" 2>&1 diff "-r#{revision}" #{diff_arg} "#{target_path}"}
 
-	      if $? != 0
-			# The command failed; show its output as a tooltip
-	        print res
-			TextMate::exit_show_tool_tip
-	      elsif res.empty?
-			# Report lack of differences as a tooltip
-	        TextMate::exit_show_tool_tip("No differences found.")
-	      elsif (custom_diff? or diff_cmd) and res =~ svn_header
-			# Suppress output, as we only got a svn header (so likely diff-cmd opened its own window)
-	        TextMate::exit_discard
-	      else
-	        print res
-	      end
-	    end
-	end
+        if $? != 0
+          # The command failed; show its output as a tooltip
+          error << res
+        elsif (custom_diff? or diff_cmd) and res =~ svn_header
+          # Suppress output, as we only got a svn header (so likely diff-cmd opened its own window)
+        else
+          result << res
+        end
+      end
+
+      TextMate::exit_show_tool_tip(error)                     unless error.empty?
+      TextMate::exit_show_tool_tip("No differences found.")   if result.empty?
+      puts result
+    end
   end
 
   # Returns true if ~/.subversion/config contains an uncommented entry for diff-cmd

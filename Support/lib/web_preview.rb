3d5fa53d9d22c15136cb44f7d4c3e54e8d4f8434
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-10-29 10:52:55 +0000

    • update web_preview.rb with new HTML etc. • make webpreview.sh use web_preview.rb • make RubyMate use web_preview.rb
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5712 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/web_preview.rb
+++ Support/lib/web_preview.rb
@@ -1,54 +1,76 @@
-require "cgi"
+require 'erb'
 
-def html_header(tm_html_title, tm_html_lang = "", tm_extra_head = "")
-	tm_html_theme = %x{bash -c #{e_sh ". #{e_sh ENV['TM_SUPPORT_PATH']}/lib/webpreview.sh && selected_theme"}}.strip
-	if File.exist? ENV["TM_FILEPATH"].to_s
-	  tm_extra_head += "<base href='tm-file://#{CGI.escape(ENV["TM_FILEPATH"])}'/>"
-	end
-	tm_css = File.read("#{ENV['TM_SUPPORT_PATH']}/css/webpreview.css").
-	              gsub("TM_SUPPORT_PATH", ENV['TM_SUPPORT_PATH'])
-	puts <<END_HTML_HEAD
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+HTML_TEMPLATE = <<-HTML
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+  "http://www.w3.org/TR/html4/strict.dtd">
+<html>
 <head>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<title>#{tm_html_title}</title>
-	<style type="text/css" media="screen">
-		#{tm_css}
-	</style>
-	<script src="file://#{ENV['TM_SUPPORT_PATH']}/script/default.js" type="text/javascript" language="javascript" charset="utf-8"></script>
-	<script src="file://#{ENV['TM_SUPPORT_PATH']}/script/webpreview.js" type="text/javascript" language="javascript" charset="utf-8"></script>
-	#{tm_extra_head}
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+  <title><%= window_title %></title>
+  <link rel="stylesheet" href="file://<%= support_path %>/themes/default/style.css"   type="text/css" charset="utf-8" media="screen">
+  <link rel="stylesheet" href="file://<%= support_path %>/themes/bright/style.css"    type="text/css" charset="utf-8" media="screen">
+  <link rel="stylesheet" href="file://<%= support_path %>/themes/dark/style.css"      type="text/css" charset="utf-8" media="screen">
+  <link rel="stylesheet" href="file://<%= support_path %>/themes/shiny/style.css"     type="text/css" charset="utf-8" media="screen">
+  <link rel="stylesheet" href="file://<%= support_path %>/themes/halloween/style.css" type="text/css" charset="utf-8" media="screen">
+  <script src="file://<%= support_path %>/script/default.js"    type="text/javascript" charset="utf-8"></script>
+  <script src="file://<%= support_path %>/script/webpreview.js" type="text/javascript" charset="utf-8"></script>
+  <%= html_head %>
 </head>
-<body id="tm_webpreview_body" class="#{tm_html_theme}">
-	<div id="tm_webpreview_header">
-		<p class="headline">#{tm_html_title}</p>
-		<p class="type">#{tm_html_lang}</p>
-		<img class="teaser" src="file://#{ENV['TM_SUPPORT_PATH']}/images/gear2.png" alt="teaser" />
-		<div id="theme_switcher">
-			<form action="#" onsubmit="return false;">
-				Theme: 
-				<select onchange="selectTheme(this.value);" id="theme_selector">
-					<option>bright</option>
-					<option>dark</option>
-					<option value="default">no colors</option>
-				</select>
-			</form>
-		</div>
-	</div>
-	<div id="tm_webpreview_content" class="#{tm_html_theme}">
-END_HTML_HEAD
+<body id="tm_webpreview_body" class="<%= html_theme %>">
+  <div id="tm_webpreview_header">
+    <img id="gradient" src="file://<%= support_path %>/themes/<%= html_theme %>/images/header.png">
+    <p class="headline"><%= page_title %></p>
+    <p class="type"><%= sub_title %></p>
+    <img id="teaser" src="file://<%= support_path %>/themes/<%= html_theme %>/images/teaser.png" alt="teaser">
+    <div id="theme_switcher">
+      <form action="#" onsubmit="return false;">
+        Theme: 
+        <select onchange="selectTheme(this.value);" id="theme_selector">
+          <option value="bright"   >bright    </option>
+          <option value="dark"     >dark      </option>
+          <option value="shiny"    >shiny     </option>
+          <option value="halloween">halloween </option>
+        </select>
+        <script type="text/javascript" charset="utf-8">
+          document.getElementById('theme_selector').value = '<%= html_theme %>';
+        </script>
+      </form>
+    </div>
+  </div>
+	<div id="tm_webpreview_content" class="<%= html_theme %>">
+HTML
+
+def selected_theme
+	res = %x{ defaults 2>/dev/null read com.macromates.textmate.webpreview SelectedTheme }.chomp
+  $? == 0 ? res : 'bright'
+end
+
+def html_head(options = { })
+  window_title = options[:window_title] || options[:title]    || 'Window Title'
+  page_title   = options[:page_title]   || options[:title]    || 'Page Title'
+  sub_title    = options[:sub_title]    || ENV['TM_FILENAME'] || 'untitled'
+
+  html_theme   = selected_theme
+  support_path = ENV['TM_SUPPORT_PATH']
+
+  html_head    = options[:html_head]    || ''
+
+  if options[:fix_href] && File.exist?(ENV['TM_FILEPATH'].to_s)
+    require "cgi"
+    html_head << "<base href='tm-file://#{CGI.escape(ENV['TM_FILEPATH'])}'>"
+	end
+
+  ERB.new(HTML_TEMPLATE).result binding
+end
+
+# compatibility function
+def html_header(tm_html_title, tm_html_lang = "", tm_extra_head = "")
+  puts html_head(:title => tm_html_title, :sub_title => tm_html_lang, :html_head => tm_extra_head)
 end
 
 def html_footer
-	puts <<HTML
+	puts <<-HTML
 	</div>
-	<!-- <div id="tm_webpreview_footer">
-		<p>TextMate Web Preview Window</p>
-	</div> -->
-	<script type="text/javascript">window.location.hash = "scroll_to_here";</script>
 </body>
 </html>
 HTML
--- Support/lib/webpreview.sh
+++ Support/lib/webpreview.sh
@@ -7,86 +7,17 @@
 # Further Notes:
 # This is in flux right (see http://macromates.com/wiki/Suggestions/StylingHTMLOutput) now as I'm updating the Web Preview (HTML output). `html.sh` is the old version. To make for a smooth transition I threw out all the old (now partly incompatible and as I see it hardly used) functions and kind of started from scratch.
 
-selected_theme() {
-	RES=$(defaults 2>/dev/null read com.macromates.textmate.webpreview SelectedTheme)
-	if [[ $? == 0 ]]
-		then echo "$RES"
-		else echo "bright"
-	fi
-}
-
 # Generate HTML header up to and including the body tag. Also includes the default stylesheet and javascript.
 # USAGE: html_header [page title] [page info, like Bundle Name, shown at the top right]
 html_header() {
-	TM_HTML_TITLE=${1}
-	TM_HTML_THEME=$(selected_theme)
-	case "$TM_HTML_THEME" in
-		bright)  SEL_BRIGHT='selected="selected"';;
-		dark)    SEL_DARK='selected="selected"';;
-		shiny)    SEL_SHINY='selected="selected"';;
-		halloween) SEL_HALLOWEEN='selected="halloween"';;
-	esac
-
-	if [[ -n $2 ]]; then
-		TM_HTML_LANG=$2
-	fi
-	# if [[ -f "$TM_FILEPATH" ]]; then
-	#   TM_EXTRA_HEAD="<base href='tm-file://${TM_FILEPATH// /%20}'/>"
-	# fi
-	# TM_CSS=`cat "${TM_SUPPORT_PATH}/themes/${TM_HTML_THEME}/style.css" | sed "s|TM_SUPPORT_PATH|${TM_SUPPORT_PATH}|"`
-	cat <<HTML
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<title>${TM_HTML_TITLE}</title>
-	<link rel="stylesheet" href="file://${TM_SUPPORT_PATH}/themes/default/style.css" type="text/css" media="screen" charset="utf-8"/>
-	<link rel="stylesheet" href="file://${TM_SUPPORT_PATH}/themes/bright/style.css" type="text/css" media="screen" charset="utf-8"/>
-	<link rel="stylesheet" href="file://${TM_SUPPORT_PATH}/themes/dark/style.css" type="text/css" media="screen" charset="utf-8"/>
-	<link rel="stylesheet" href="file://${TM_SUPPORT_PATH}/themes/shiny/style.css" type="text/css" media="screen" charset="utf-8"/>
-	<link rel="stylesheet" href="file://${TM_SUPPORT_PATH}/themes/halloween/style.css" type="text/css" media="screen" charset="utf-8"/>
-	<!-- <link rel="stylesheet" href="file://${TM_SUPPORT_PATH}/themes/${TM_HTML_THEME}/style.css" type="text/css" media="screen" charset="utf-8"/> -->
-	<script src="file://${TM_SUPPORT_PATH}/script/default.js" type="text/javascript" language="javascript" charset="utf-8"></script>
-	<script src="file://${TM_SUPPORT_PATH}/script/webpreview.js" type="text/javascript" language="javascript" charset="utf-8"></script>
-	${TM_EXTRA_HEAD}
-</head>
-<body id="tm_webpreview_body" class="${TM_HTML_THEME}">
-	<div id="tm_webpreview_header">
-		<img id="gradient" src="file://${TM_SUPPORT_PATH}/themes/${TM_HTML_THEME}/images/header.png" />
-		<p class="headline">${TM_HTML_TITLE}</p>
-		<p class="type">${TM_HTML_LANG}</p>
-		<img id="teaser" src="file://${TM_SUPPORT_PATH}/themes/${TM_HTML_THEME}/images/teaser.png" />
-		<!-- <div id="actions"><a href="#" onclick="hide_header(); return false;">x</a></div> -->
-		<div id="theme_switcher">
-			<form action="#" onsubmit="return false;">
-				Theme: 
-				<select onchange="selectTheme(this.value);" id="theme_selector">
-					<option ${SEL_BRIGHT}>bright</option>
-					<option ${SEL_DARK}  >dark</option>
-					<option ${SEL_SHINY} >shiny</option>
-					<option ${SEL_HALLOWEEN} >halloween</option>
-				</select>
-			</form>
-		</div>
-	</div>
-	<div id="tm_webpreview_content" class="${TM_HTML_THEME}">
-HTML
+	export HTML_TITLE=$1
+	export HTML_SUB_TITLE=$2
+	"${TM_RUBY:-ruby}" -r"$TM_SUPPORT_PATH/lib/web_preview.rb" -e 'html_header(ENV["HTML_TITLE"], ENV["HTML_SUB_TITLE"])'
 }
 
 # Generate HTML footer.
 # USAGE: html_footer
 html_footer() {
-	cat << HTML
-	</div>
-	<!-- <div id="tm_webpreview_footer">
-		<p>TextMate Web Preview Window</p>
-	</div>
-	<script type="text/javascript">window.location.hash = "scroll_to_here";</script>
-	 -->
-</body>
-</html>
-HTML
+	echo $'	</div>\n</body>\n</html>'
 }
 

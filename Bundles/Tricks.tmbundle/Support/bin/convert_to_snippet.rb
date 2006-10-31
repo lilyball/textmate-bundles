c3e1bbedd968ed5dd2ea7cc33b50fe7db1b27e7c
canadaduane <canadaduane@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-10-31 18:44:13 +0000

    Initial import of the Tricks bundle. Added the MASC (multiple arbitrary simultaneous carets) feature previously available in my (Duane Johnson's) bundle.  Not bound to any keys by default.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5807 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/Tricks.tmbundle/Support/bin/convert_to_snippet.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require 'strscan'
+
+input = STDIN.read.gsub(/([\$\`\\])/, '\\\\\1')
+output = String.new
+counter = 1
+scanner = StringScanner.new(input)
+while scanner.scan(/(.*?)‹(.*?)›/m)
+  prefix, contents = scanner[1], scanner[2]
+  output << prefix + "$#{counter+=1}${1:#{contents}}"
+end
+output << "$0" if counter > 1
+output << input[(scanner.pos rescue 0)..-1]
+
+print output

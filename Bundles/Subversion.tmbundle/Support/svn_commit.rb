02a0b167cde7a3c8875e2e36524c190b0637fe70
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-11-03 03:23:19 +0000

    • Separate the commit logic from the presentation. This is the first step toward fixing the status window's commit output.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5899 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/svn_commit.rb
+++ Bundles/Subversion.tmbundle/Support/svn_commit.rb
@@ -76,6 +76,73 @@ else
 	mup = Builder::XmlMarkup.new(:target => STDOUT)
 end
 
+
+class SVNCommitTransaction
+	attr_accessor :paths_to_commit
+	attr_accessor :svn_tool
+	attr_accessor :diff_tool
+	attr_accessor :commit_window_tool
+	attr_accessor :status_helper_tool
+
+private	
+	def matches_to_paths(matches)
+		paths = matches.collect {|m| m[2] }
+		paths.collect{|path| path.sub(/^#{CurrentDir}/, "") }
+	end
+
+	def matches_to_status(matches)
+		# collect the status, and replace prefix spaces with underscores so command-line argument passing works later
+		matches.collect {|m| m[0]}.map {|m| m.rstrip.gsub(/\s/, '_')}
+	end
+
+public
+	def initialize(paths_to_commit)
+		@paths_to_commit	= paths_to_commit
+	end
+
+	def preflight
+		# Ignore files without changes
+		status_command = %Q{"#{@svn_tool}" status #{@paths_to_commit.quote_for_shell_arguments}}
+		# puts status_command + "\n" #DEBUG
+
+		status_output = %x{#{status_command}}
+		# puts status_output + "\n" #DEBUG
+
+		paths = status_output.scan(/^(.....)(\s+)(.*)\n/)
+
+		@commit_matches = paths
+
+		return false if @commit_matches.nil? or (@commit_matches.size == 0)
+		true
+	end
+	
+	def ask_user_for_arguments
+		commit_paths_array = matches_to_paths(@commit_matches)
+		commit_status = matches_to_status(@commit_matches).join(":")
+
+		commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
+		
+		ENV['TM_SUPPORT_PATH'] = support if $console_output
+		@commit_args = %x{"#{@commit_window_tool}" --diff-cmd "#{@svn_tool},diff,--diff-cmd,#{@diff_tool}" \
+		 					--status #{commit_status} \
+							--action-cmd "?:Add,#{@svn_tool},add" \
+							--action-cmd "A:Mark Executable,#{@status_helper_tool},propset,svn:executable,true" \
+							--action-cmd "A,M,D,C:Revert,#{@status_helper_tool},revert" \
+							--action-cmd "C:Resolved,#{@status_helper_tool},resolved" \
+							#{commit_path_text}}
+		$CHILD_STATUS
+	end
+	
+	def commit(&output_block)
+		require "open3"
+		Open3.popen3("#{@svn_tool} commit --non-interactive --force-log #{@commit_args}") do |stdin, stdout, stderr|
+			stdout.each_line {|line| output_block.call(:output, line.chomp)}
+			stderr.each_line {|line| output_block.call(:error, line.chomp)}
+		end
+	end
+end
+
+
 mup.html {
 	mup.head do
 			mup.title("Subversion Commit")
@@ -94,114 +161,34 @@ mup.html {
 		
 		STDOUT.flush
 
-		# Ignore files without changes
-#puts TextMate::selected_paths_for_shell
-		status_command = %Q{"#{svn}" status #{paths_to_commit.quote_for_shell_arguments}}
-		# puts status_command + "\n" #DEBUG
-#puts status_command
-		status_output = %x{#{status_command}}
-		# puts status_output + "\n" #DEBUG
-#puts status_output
-		paths = status_output.scan(/^(.....)(\s+)(.*)\n/)
-
-		def matches_to_paths(matches)
-			paths = matches.collect {|m| m[2] }
-			paths.collect{|path| path.sub(/^#{CurrentDir}/, "") }
-		end
-
-		def matches_to_status(matches)
-			# collect the status, and replace prefix spaces with underscores so command-line argument passing works later
-			matches.collect {|m| m[0]}.map {|m| m.rstrip.gsub(/\s/, '_')}
-		end
-
-		def select_files_with_status_prefix(paths, char)
-			paths.select do |m|
-				index = m[0].index(char)
-				((index != nil) and (index == 0))
-			end
-		end
-
-		# Ignore files with '?', but report them to the user String.index
-		# unknown_paths = select_files_with_status_prefix(paths, '?')
-		#         unknown_to_report_paths = unknown_paths.reject{ |m| IgnoreFilePattern =~ m[2] }
-		# if unknown_to_report_paths and unknown_to_report_paths.size > 0 then
-		# 	mup.div( :class => "warning" ) {
-		# 		mup.p "These files are not under version control, and so will not be committed:"
-		# 		mup.ul{ matches_to_paths(unknown_to_report_paths).each{ |path| mup.li(path) } }
-		# 	}
-		# end
-
-		# Ignore files with '!', but report them to the user
-		# missing_paths = select_files_with_status_prefix(paths, '!')
-		#         missing_to_report_paths = missing_paths.reject{ |m| IgnoreFilePattern =~ m[2] }
-		# if missing_to_report_paths and missing_to_report_paths.size > 0 then
-		#     
-		# 	mup.div( :class => "warning" ) {
-		# 		mup.p "These files are missing from the working copy:"
-		# 		mup.ul{ matches_to_paths(missing_to_report_paths).each{ |path| mup.li(path) } }
-		# 	}
-		# end
-
-		# Fail if we have conflicts -- svn commit will fail, so let's
-		# error out before the user gets too involved in the commit
-		# conflict_paths = select_files_with_status_prefix(paths, 'C')
-		# 
-		# if conflict_paths and conflict_paths.size > 0 then
-		# 	mup.div( :class => "error" ) {
-		# 		mup.p "Cannot continue; there are merge conflicts in files:"		
-		# 		mup.ul{ matches_to_paths(conflict_paths).each{ |path| mup.li(path) } }
-		# 		mup.text! "Canceled."
-		# 	}	
-		# 	exit -1
-		# end
-
-		# Remove the unknown paths from the commit
-		# commit_matches = paths - unknown_paths
-		# 
-		# # Remove the missing paths from the commit
-		# commit_matches = commit_matches - missing_paths
-
-		commit_matches = paths
-
-		if commit_matches.nil? or commit_matches.size == 0
+		transaction = SVNCommitTransaction.new(paths_to_commit)
+		transaction.svn_tool			= svn
+		transaction.commit_window_tool	= commit_tool
+		transaction.diff_tool			= diff_cmd
+		transaction.status_helper_tool	= status_helper
+		
+		if not transaction.preflight then 
 			mup.div( :class => "info" ) {
 				mup.text! "File(s) not modified; nothing to commit."
-#				mup.ul{ matches_to_paths(unknown_paths).each{ |path| mup.li(path) } }
+				mup.ul{ transaction.paths_to_commit.each{ |path| mup.li(path) } }
 			}
 			exit 0
+		else
+			STDOUT.flush
+			status = transaction.ask_user_for_arguments
+
+			if status != 0
+				mup.div( :class => "error" ) { mup.text! "Canceled (#{status >> 8})." }	
+				exit -1
+			end
 		end
-
-		STDOUT.flush
-
-		commit_paths_array = matches_to_paths(commit_matches)
-		commit_status = matches_to_status(commit_matches).join(":")
-
-		commit_path_text = commit_paths_array.collect{|path| path.quote_filename_for_shell }.join(" ")
-
-		# need to reexport the support directory for CommitWindow's benefit, or double-click diff doesn't work.
-		ENV['TM_SUPPORT_PATH'] = support if $console_output
-		commit_args = %x{"#{commit_tool}" --diff-cmd "#{svn},diff,--diff-cmd,#{diff_cmd}" \
-		 					--status #{commit_status} \
-							--action-cmd "?:Add,#{svn},add" \
-							--action-cmd "A:Mark Executable,#{status_helper},propset,svn:executable,true" \
-							--action-cmd "A,M,D,C:Revert,#{status_helper},revert" \
-							--action-cmd "C:Resolved,#{status_helper},resolved" \
-							#{commit_path_text}
-		}
-
-		status = $CHILD_STATUS
-		if status != 0
-			mup.div( :class => "error" ) {
-				mup.text! "Canceled (#{status >> 8})."
-			}	
-			exit -1
-		end
-
+		
 		if $console_output
 			mup.div(:class => "command"){ mup.strong(%Q{#{svn} commit}); mup.text!(commit_args) }
 		end
 
 		exit 0 if $dry_run
+		
 		mup.div( :class => 'section' ) do
 			mup.pre {
 				mup.text("...\n")
@@ -210,12 +197,8 @@ mup.html {
 				# puts "#{svn} commit --non-interactive --force-log #{commit_args}" #DEBUG
 				# puts `pwd`                                                        #DEBUG
 				
-				require "open3"
-				Open3.popen3("#{svn} commit --non-interactive --force-log #{commit_args}") do |stdin, stdout, stderr|
-					# WebKit needs <br> instead of \n inside <pre>, otherwise the text won't flush
-					stdout.each_line {|line| mup.text! line.chomp; mup << "<br>"; STDOUT.flush}
-					stderr.each_line {|line| mup.text! line.chomp; mup << "<br>"; STDERR.flush}
-				end
+				# WebKit needs <br> instead of \n inside <pre>, otherwise the text won't flush
+				transaction.commit {|stream, line| mup.text! line; mup << "<br>"; STDOUT.flush}
 			}
 		end
 

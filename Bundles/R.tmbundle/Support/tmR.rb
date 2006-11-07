cb12556cc468a7d386a561b3bc9061722e63e876
kevin <kevin@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-11-07 05:37:55 +0000

    Re-write Insert Command Template completely It now leverages R to search for the function and construct the function template. This means it will only display functions that are actually in the R default packagea list, which helps cut down on a lot of unnecessary functions Unfortunately if you're using a non-standard package it won't offer templates. Got any suggestions for the proper solution to this problem? Remove Process Help Files and commands.txt since they were no longer applicable Tweak tmR.rb to move the popen3() function out into a new lib/popen3.rb file, for use in the re-written Insert Command Template
    git-svn-id: http://macromates.com/svn/Bundles/trunk@5980 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/R.tmbundle/Support/tmR.rb
+++ Bundles/R.tmbundle/Support/tmR.rb
@@ -10,34 +10,11 @@
 require 'cgi'
 require 'fcntl'
 
+require File.join(File.dirname(__FILE__), 'lib/popen3')
+
 # HTML escaping function.
 def esc(str)
   CGI.escapeHTML(str)
-end
-
-# Open a sub-process and return 3 IO objects: stdin, stdout, stderr
-# Unlike Open3::popen3, it produces a child, not a grandchild
-def popen3(*args)
-  stdin, stdout, stderr = [IO.pipe, IO.pipe, IO.pipe]
-  fork do
-    stdin[1].close
-    STDIN.reopen(stdin[0])
-    stdin[0].close
-    
-    stdout[0].close
-    STDOUT.reopen(stdout[1])
-    stdout[1].close
-    
-    stderr[0].close
-    STDERR.reopen(stderr[1])
-    stderr[1].close
-    
-    exec(*args)
-  end
-  stdin[0].close
-  stdout[1].close
-  stderr[1].close
-  [stdin[1], stdout[0], stderr[0]]
 end
 
 # Headers...

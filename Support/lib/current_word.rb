7ff8a998b5dd99df7d993e6b9cde51f8536de603
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-11-08 09:35:28 +0000

    • use TM_LINE_INDEX instead of TM_COLUMN_NUMBER (the former is zero-based and is the actual index, which means \t counts as 1, æ counts as 2 (utf-8 multi-byte), etc.)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6011 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/current_word.rb
+++ Support/lib/current_word.rb
@@ -2,10 +2,9 @@ module Word
   def self.current_word(pat)
     word = ENV['TM_SELECTED_TEXT']
     if word.nil? or word.empty?
-      line = ENV['TM_CURRENT_LINE'].gsub(/\t/, ' '*ENV['TM_TAB_SIZE'].to_i)
-      column = ENV['TM_COLUMN_NUMBER'].to_i - 1 # columns are 1-based, ruby is 0-based
+      line, col = ENV['TM_CURRENT_LINE'], ENV['TM_LINE_INDEX'].to_i
       pat = pat.source if pat.kind_of? Regexp
-      word = line[0...column].match(/[#{pat}]*$/).to_s + line[column..-1].match(/^[#{pat}]*/).to_s
+      word = line[0...col].match(/[#{pat}]*$/).to_s + line[col..-1].match(/^[#{pat}]*/).to_s
     end
     word
   end

0638b05eb12ca24de8cc41b8b7a864e884cfd465
kevin <kevin@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-11-08 03:39:30 +0000

    Fix current_word.rb to work with tabs
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6000 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/current_word.rb
+++ Support/lib/current_word.rb
@@ -2,7 +2,7 @@ module Word
   def self.current_word(pat)
     word = ENV['TM_SELECTED_TEXT']
     if word.nil? or word.empty?
-      line = ENV['TM_CURRENT_LINE']
+      line = ENV['TM_CURRENT_LINE'].gsub(/\t/, ' '*ENV['TM_TAB_SIZE'].to_i)
       column = ENV['TM_COLUMN_NUMBER'].to_i - 1 # columns are 1-based, ruby is 0-based
       pat = pat.source if pat.kind_of? Regexp
       word = line[0...column].match(/[#{pat}]*$/).to_s + line[column..-1].match(/^[#{pat}]*/).to_s

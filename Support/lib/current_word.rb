5b881be23acc8c267e4bb86bf910531172db1a0d
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-11-16 11:29:33 +0000

    • it is now possible to give :left or :right as optional second parameter to current_word to only get the left or right part of the word (relative to the caret)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6153 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/current_word.rb
+++ Support/lib/current_word.rb
@@ -1,10 +1,18 @@
 module Word
-  def self.current_word(pat)
+  def self.current_word(pat, direction = :both)
     word = ENV['TM_SELECTED_TEXT']
     if word.nil? or word.empty?
       line, col = ENV['TM_CURRENT_LINE'], ENV['TM_LINE_INDEX'].to_i
       pat = pat.source if pat.kind_of? Regexp
-      word = line[0...col].match(/[#{pat}]*$/).to_s + line[col..-1].match(/^[#{pat}]*/).to_s
+
+      left  = line[0...col].match(/[#{pat}]*$/).to_s 
+	    right = line[col..-1].match(/^[#{pat}]*/).to_s
+
+	    case direction
+        when :both then word = left + right
+        when :left then word = left
+        when :right then word = right
+      end
     end
     word
   end

9c5c95437aec68afd710921e04bcb9974776320e
andyherbert <andyherbert@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-11-25 08:24:48 +0000

    The deploy widget script will now install the Textmate widget within the support folder if a newer version exists.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6229 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Widget Creator.tmbundle/Support/deploy_widget.sh
+++ Bundles/Widget Creator.tmbundle/Support/deploy_widget.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-open /Library/Widgets/Textmate.wdgt > /dev/null 2>&1
-if [[ $? != 0 ]]; then
-	open ~/Library/Widgets/Textmate.wdgt > /dev/null 2>&1
-	if [[ $? != 0 ]]; then
-		echo "Textmate Widget Installed."
-		cp -R "$TM_BUNDLE_SUPPORT/Textmate.wdgt" ~/Library/Widgets/ > /dev/null
-		open ~/Library/Widgets/Textmate.wdgt > /dev/null 2>&1
-	fi
+INST_VER=`paste -s ~/Library/Widgets/Textmate.wdgt/info.plist 2>&1 | grep -oE '<key>CFBundleVersion</key>.*?<string>.*?</string>' | grep -oE '<string>.*?</string>' | sed -e 's/<string>//' -e 's/<\/string>//'`
+BUND_VER=`paste -s "$TM_BUNDLE_SUPPORT/Textmate.wdgt/info.plist" | grep -oE '<key>CFBundleVersion</key>.*?<string>.*?</string>' | grep -oE '<string>.*?</string>' | sed -e 's/<string>//' -e 's/<\/string>//'`
+if [[ $INST_VER < $BUND_VER ]]; then
+	rm -fR ~/"Library/Widgets/Textmate.wdgt"
+	cp -R "$TM_BUNDLE_SUPPORT/Textmate.wdgt" ~/Library/Widgets/ > /dev/null
+	echo "Textmate Widget Installed."
 fi
+open ~/"Library/Widgets/Textmate.wdgt" > /dev/null 2>&1

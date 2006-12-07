1e8e309102a3ad4f33a00aa331b406fbf2c7c328
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-12-07 15:40:14 +0000

    • Make the standard progress dialog native to TM (replacing the external CocoaDialog progress dialog). No changes to client code required.
    • Support determinate progress bars.
    
    • Support a cancel button.
    
    You could previously get an indeterminate progress bar:
    
      TextMate.call_with_progress(:title =>'Now Testing',
              :summary => 'Starting Up...') do
    	# do something
      end
    
    But now you can do:
    
      TextMate.call_with_progress(:title =>'Now Testing',
    			:summary => 'Starting Up...',
    			:indeterminate => false,
    			:cancel => lambda {puts "Canceled!"; exit 0} ) do |dialog|
    
        #....
        dialog.parameters = {'summary' => 'Loving the alien...','progressValue' => 30 }
        #...
        dialog.parameters = {'summary' => 'Eating the pie...','progressValue' => 60 }
        #...
        dialog.parameters = {'summary' => 'Vacuuming the rug...','progressValue' => 90 }
        #...
        dialog.parameters = {'summary' => 'Complete!','progressValue' => 100 }
        #...
      end
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6310 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/progress.rb
+++ Support/lib/progress.rb
@@ -1,15 +1,98 @@
-
 require 'English'
+$LOAD_PATH << File.dirname(__FILE__)
+require 'dialog'
 
 module TextMate
 
+  # Call the given block while showing a progress dialog. By default, the progress bar is
+  # indeterminate/indefinite/'barber pole.'
+  #
+  # Note that the cancel button is only available when you provide the :cancel argument, which is
+  # expected to be a proc. To support the cancel button, the primary block is forked into a new
+  # process. When the cancel button is pressed, the cancel proc is also executed in the new process.
+  # At that point, you can Kernel.exit the process directly from the cancel block (as in the code
+  # above) or set a global variable for orderly shutdown.
+  #
+  # TODO: this may need additional work to make cancel detectable from the client script.
   def TextMate.call_with_progress( args, &block )
+    title           = args[:title] || 'Progress'
+    message         = args[:message] || args[:summary] || 'Frobbing the widget...'
+    cancel_proc     = args[:cancel]
+    indeterminate   = args[:indeterminate]
+    indeterminate   = true if indeterminate.nil?
+        
+    params = {'title' => title,
+              'summary' => message,
+              'details' => '',
+              'isIndeterminate' => indeterminate}
 
-    title         = args[:title] || 'Progress'
-    message       = args[:message] || 'Frobbing the widget...'
-    cocoa_dialog  = "#{ENV['TM_SUPPORT_PATH']}/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog"
-    IO.popen(%Q("#{cocoa_dialog}" progressbar --indeterminate --title "#{title}" --text "#{message}"), "w+", &block)
+    params['cancelButtonHidden'] = false unless cancel_proc.nil?
+    
+    run_block = Proc.new do |dialog|
+      if block.arity == 0
+        block.call
+      else
+        block.call(dialog)
+      end
+    end
+    
+    Dialog.dialog('ProgressDialog.nib', params) do |dialog|
+      if cancel_proc.nil?
+        # if there is no cancel proc, we get no input from the dialog and need not block.
+        run_block.call(dialog)
+      else # not cancel_proc.nil?
+        
+        # invoke processing block in one process
+        run_block_process = fork do
+            trap('SIGINT'){cancel_proc.call}
+            run_block.call(dialog)
+        end
 
+        # invoke ui waiting in a second process
+        ui_process = fork do
+          begin
+            dialog.wait_for_input do |params|
+              # tell the main process to run the cancel_proc
+              Process.kill('SIGINT', run_block_process) if params['returnButton'] == 'Cancel' # FIXME localization problem...
+              false
+            end
+          rescue TextMate::WindowNotFound
+              # ignore; the window was probably canceled
+          end
+        end
+        
+        # hang out until the dialog is done
+        Process.wait
+      end
+    end
+  end
+  
+end
+
+# test
+if __FILE__ == $0
+
+  # indeterminate
+  TextMate.call_with_progress(:title =>'Now Testing',
+          :summary => 'Starting Up...') do
+
+    sleep 3
   end
 
+  # finite
+  TextMate.call_with_progress(:title =>'Now Testing',
+          :summary => 'Starting Up...',
+          :indeterminate => false,
+          :cancel => lambda {puts "Canceled!"; exit 0} ) do |dialog|
+
+    sleep 1
+    dialog.parameters = {'summary' => 'Loving the alien...','progressValue' => 30 }
+    sleep 1
+    dialog.parameters = {'summary' => 'Eating the pie...','progressValue' => 60 }
+    sleep 1
+    dialog.parameters = {'summary' => 'Vacuuming the rug...','progressValue' => 90 }
+    sleep 1
+    dialog.parameters = {'summary' => 'Complete!','progressValue' => 100 }
+    sleep 2
+  end
 end

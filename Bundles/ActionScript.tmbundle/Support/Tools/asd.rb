4a8c2852cecae7a87263496198e5368bd74bc72a
ale <ale@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-12-17 19:50:05 +0000

    Improved search performance by using regexp instead of String#includes?
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6358 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/ActionScript.tmbundle/Support/Tools/asd.rb
+++ Bundles/ActionScript.tmbundle/Support/Tools/asd.rb
@@ -6,7 +6,7 @@
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/web_preview"
 require "rexml/document"
 
-WORD		= ENV['TM_CURRENT_WORD']
+WORD    = ENV['TM_CURRENT_WORD']
 HELPDIR		= ENV['TM_FLASH_HELP']
 HELPTOC		= 'help_toc.xml'
 
@@ -38,7 +38,7 @@ else
 	# ...find matching lines...
 	search_results = []
 	toc_lines.each do |line|
-		search_results << line.strip if line.include? "name=\"" + WORD
+		search_results << line.strip if line[/name=\"WORD/]
 	end
 
 	puts html_head( :title => "Documentation for ‘#{WORD}’", :sub_title => "ActionScript Dictionary" )

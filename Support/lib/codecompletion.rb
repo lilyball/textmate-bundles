507eb9f9dde9ee82bf81bff7286c259b5358a38f
subtlegradient <subtlegradient@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2006-12-18 21:45:56 +0000

    [NEW] Code Completion ruby library. Uses existing completions preferences read from the plist file or a static or dynamically generated array. Filters the completion choices based on the text you're in the middle of writing and inserts as a snippet.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6369 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Support/lib/codecompletion.rb
@@ -0,0 +1,226 @@
+# 
+# Textmate Code Completion
+# Version: 1 (version numbers are integers, no beta bologna!)
+# 
+#  By: Thomas Aylott / subtleGradient, oblivious@
+# And: nobody else yet
+#   If you make any changes, add your name and be famous! ;)
+# 
+
+=begin
+
+README
+How to use this version
+  Create a tmCommand, input selection or line, output insert as snippet, contents like this:
+
+#!/usr/bin/env ruby
+require "#{ENV['TM_SUPPORT_PATH']}/lib/codecompletion"
+TextmateCodeCompletion.simple
+
+By default, it'll look for a tmPreference in that bundle with the name "CodeCompletions".
+That preference should be like this:
+
+{	completions = ( 'fibbity', 'flabbity', 'floo' ); }
+
+Where the completions is an array of strings.
+You can overwrite that by passing in a different name for the preference like this:
+
+TextmateCodeCompletion.simple('SomeOtherNameForMyCompletions')
+
+Or you can use any random plist file like this:
+
+#!/usr/bin/env ruby
+require "#{ENV['TM_SUPPORT_PATH']}/lib/codecompletion"
+choices = TextmateCompletionsPlist.new('path_to_my_plist_file_of_doom').to_ary
+print TextmateCodeCompletion.new(choices,STDIN.read).to_s
+
+
+Or you can just pass in an array manually like so:
+
+#!/usr/bin/env ruby
+require "#{ENV['TM_SUPPORT_PATH']}/lib/codecompletion"
+print TextmateCodeCompletion.new([ 'fibbity', 'flabbity', 'floo' ],STDIN.read).to_s
+
+
+=end
+
+# TESTS are in the file codecompletion_test.rb
+# Do no commit changes to this file unless all tests pass.
+# If you add any functionality, be sure to create tests that really try to break it.
+# 
+# GOALS:
+# 
+# Release 1:
+#   Just the basics.
+#   Grab the choices from a tmPreference plist or an array
+#   Filter based on context (allowing for something external to overwrite the method used)
+#   Show the menu and select a choice
+#   Convert the selection into a snippet
+#   Return the snippet to be inserted somehow
+#   TextmateCodeCompletion.simple method to allow for really quick and simple default behavior in tmCommands
+# 
+# Release 2:
+#   I dunno, fanciness of some sort perhaps
+# 
+# Eventual goals: 
+# 
+# Allow for objects as input with snippets as output
+# Currently, the choices are simple an Array of strings.
+# We will need to extend this eventually to support objects with values that are snippets.
+# So you'll select your choice based on the key, but the calue of the key/value pair is inserted
+# 
+# Speed improvements.
+# Maybe cache the plists in ram or something
+# 
+# Line only input.
+# Currently we have to do some fancy footwork to insert the right thing in the right way 
+# since the input can either be a selection or the current line.
+# And if you're input is a selection, then the command only replaces the selectio, 
+# so you have to make sure you don't reinsert the context and the choice_partial and stuff like that.
+# 
+# 
+
+class TextmateCompletionsPlist
+  require "#{ENV['TM_SUPPORT_PATH']}/lib/plist"
+  
+  attr :plist, true
+  attr :scope, true
+  attr :choices, true
+  
+  def initialize(filepath=nil)
+    path = filepath || "#{ENV['TM_BUNDLE_PATH']}/Preferences/CodeCompletion.tmPreferences"
+    return false unless File.exist?(path)
+    self.plist = File.new(path)
+    
+    pl = PropertyList.load(self.plist, true)
+    self.scope   = pl[0]['scope'].split(/, ?/)
+    self.choices = pl[0]['settings']['completions']
+  end
+  
+  def to_ary
+    self.choices
+  end
+end
+
+class TextmateCodeCompletion
+  require "#{ENV['TM_SUPPORT_PATH']}/lib/dialog"
+  
+  class << self
+    def simple(preference='CodeCompletions')
+      choices = TextmateCompletionsPlist.new(
+        "#{ENV['TM_BUNDLE_PATH']}/Preferences/#{preference}.tmPreferences"
+      ).to_ary
+      print TextmateCodeCompletion.new(choices,STDIN.read).to_s
+    end
+  end
+  
+  def initialize(choices=nil,context=nil)
+    @debug = true
+    
+    @has_selection = ENV['TM_SELECTED_TEXT'] == context
+    
+    @context = context
+    set_context!()
+    
+    cancel() and return unless choices and !choices.empty?
+    
+    @choices = choices
+    @choice = false
+    
+    filter_choices!()
+  end
+  
+  def to_s
+    choose() unless @choice
+    completion()
+  end
+  
+  private
+  def cancel
+    @choice = ''
+    @cancel = true
+  end
+  
+  def set_context!
+    line = ENV['TM_CURRENT_LINE']
+    
+    caret_placement = 0
+    caret_placement = ENV['TM_COLUMN_NUMBER'].to_i - 2
+    caret_placement = ENV['TM_INPUT_START_COLUMN'].to_i - 2 if @has_selection
+    @context_before = line[0..caret_placement]
+    @context_before = '' if caret_placement == -1
+    @choice_partial = get_choice_partial()
+    @selection      = @context if @has_selection
+    @context_after  = line[caret_placement+1..line.length+1]
+    
+    @context_before.gsub!(/#{Regexp.escape @choice_partial}$/,'')
+    @context_after.gsub!(/^#{Regexp.escape @selection}/,'') if @selection
+    
+    
+    
+    if @debug
+      $debug_codecompletion = {}
+      $debug_codecompletion["caret_placement"] = snip caret_placement+2
+      $debug_codecompletion["context_before" ] = snip @context_before
+      $debug_codecompletion["choice_partial" ] = snip @choice_partial
+      $debug_codecompletion["selection"      ] = snip @selection
+      $debug_codecompletion["context_after"  ] = snip @context_after
+    end
+  end
+  
+  def get_choice_partial
+    return nil unless @context_before
+    
+    choice_partial  = ''
+    choice_partial  = @context_before.scan(/\w+$/)
+    choice_partial.to_s
+  end
+  
+  def filter_choices!
+    @choices = @choices.grep(/^#{Regexp.escape @choice_partial}/) if @choice_partial
+  end
+  
+  def choose
+    cancel() and return unless @choices
+    
+    val = Dialog.menu(@choices)
+    cancel() and return unless val
+    @choice = @choices[val]
+  end
+  
+  def completion
+    $debug_codecompletion["choice"] = snip @choice
+    
+    completion = ''
+    completion << snip(@context_before) unless @has_selection
+    if @cancel
+      completion << snip(@choice_partial)
+      completion << "${0:#{snip(@selection)}}"
+    else
+      if @choice
+        completion << snippetize(@choice)
+      else
+        completion << snip(@selection) if @selection
+      end
+    end
+    completion << snip(@context_after) unless @has_selection
+    completion
+  end
+  
+  def snippetize(text)
+    # TODO: make snippet safe
+    #       to keep any random characters from interfering with the function of the snippet
+    text.gsub!(/^#{Regexp.escape @choice_partial}/,'') if @has_selection # Trimoff the choice_partial if we have a selection
+    text.gsub!(/\(\)$/,'($1)')
+    
+    snippet = ''
+    snippet << '${101:'
+    snippet << snip(text)
+    snippet << '}$100$0'
+    snippet
+  end
+  
+  def snip(text) #make snippet proof
+    text.to_s.gsub(/(\$|\`)/,'\\\\\\1') if text
+  end
+end
---
+++ Support/lib/codecompletion_test.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+# 
+# CodeCompletion TESTS
+# 
+require "#{ENV['TM_SUPPORT_PATH']}/lib/codecompletion"
+require 'test/unit'
+
+class TestTextmateCodeCompletion < Test::Unit::TestCase
+  def test_blank
+    set_tm_vars({"TM_SELECTED_TEXT" => nil, "TM_CURRENT_LINE" => "", "TM_COLUMN_NUMBER" => "1", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "1", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal "${101:test}$100$0", TextmateCodeCompletion.new(['test'], %{}).to_s
+  end
+  def test_basic
+    set_tm_vars({"TM_SELECTED_TEXT" => nil, "TM_CURRENT_LINE" => "basic", "TM_COLUMN_NUMBER" => "6", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "1", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal "${101:basic}$100$0", TextmateCodeCompletion.new(['basic'], %{basic}).to_s
+  end
+  def test_a
+    set_tm_vars({"TM_SELECTED_TEXT" => nil, "TM_CURRENT_LINE" => "a", "TM_COLUMN_NUMBER" => "2", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "1", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal "${101:aaa}$100$0", TextmateCodeCompletion.new(['aaa'], %{a}).to_s
+  end
+  
+  def test_in_snippet
+    set_tm_vars({"TM_SELECTED_TEXT" => nil, "TM_CURRENT_LINE" => %{${1:snippet}}, "TM_COLUMN_NUMBER" => "12", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "1", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal '\${1:snippet${0:}}', TextmateCodeCompletion.new(['nomatch'], %{${1:snippet}}).to_s
+  end
+  def test_in_snippet_match
+    set_tm_vars({"TM_SELECTED_TEXT" => nil, "TM_CURRENT_LINE" => %{${1:snippet}}, "TM_COLUMN_NUMBER" => "12", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "1", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal '\${1:${101:snippet}$100$0}', TextmateCodeCompletion.new(['snippet'], %{${1:snippet}}).to_s
+  end
+  
+  def test_choice_partial_selection
+    set_tm_vars({"TM_SELECTED_TEXT" => "selection", "TM_CURRENT_LINE" => "choice_partialselection", "TM_COLUMN_NUMBER" => "24", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "15", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal "choice_partial${0:selection}", TextmateCodeCompletion.new(['test'], %{selection}).to_s, $debug_codecompletion.inspect
+  end
+  def test_choice_partial_selection_match
+    set_tm_vars({"TM_SELECTED_TEXT" => "selection", "TM_CURRENT_LINE" => "choice_partialselection", "TM_COLUMN_NUMBER" => "24", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "15", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal "${101:_match}$100$0", TextmateCodeCompletion.new(['choice_partial_match'], %{selection}).to_s, $debug_codecompletion.inspect
+  end
+  
+  def test_selection_no_context
+    set_tm_vars({"TM_SELECTED_TEXT" => "basic_selection", "TM_CURRENT_LINE" => "basic_selection", "TM_COLUMN_NUMBER" => "1", "TM_BUNDLE_PATH" => "/Users/taylott/Library/Application Support/TextMate/Bundles/Ruby.tmbundle", "TM_INPUT_START_COLUMN" => "1", "TM_SUPPORT_PATH" => "/Library/Application Support/TextMate/Support"})
+    assert_equal "${101:test_basic_selection}$100$0", TextmateCodeCompletion.new(['test_basic_selection'], %{basic_selection}).to_s, $debug_codecompletion.inspect
+  end
+  
+  private
+  def set_tm_vars(env)
+    ENV['TM_BUNDLE_PATH']        = env['TM_BUNDLE_PATH']
+    ENV['TM_COLUMN_NUMBER']      = env['TM_COLUMN_NUMBER']
+    ENV['TM_CURRENT_LINE']       = env['TM_CURRENT_LINE']
+    ENV['TM_INPUT_START_COLUMN'] = env['TM_INPUT_START_COLUMN']
+    ENV['TM_SELECTED_TEXT']      = env['TM_SELECTED_TEXT']
+    ENV['TM_SUPPORT_PATH']       = env['TM_SUPPORT_PATH']
+  end
+end

c3f3afadd64aa806a5ad21cb99ac37c60f7b096a
chris <chris@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-01-11 21:38:25 +0000

    More work on revision selector dialog. It works fine, but it needs a lot more error checking.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6443 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Subversion.tmbundle/Support/svn_log_parser.rb
+++ Bundles/Subversion.tmbundle/Support/svn_log_parser.rb
@@ -2,15 +2,108 @@
 #  svn_log_parser.rb
 #  Subversion.tmbundle
 #  
+#  Usage:
+#
+# 	LogParser.parse_path(path) {|hash_for one_log_entry| ... }
+# 	LogParser.parse(string_or_IO) {|hash_for one_log_entry| ... }
+#
+#  TODO: error reporting
+#
 #  Created by Chris Thomas on 2006-12-30.
 #  Copyright 2006 Chris Thomas. All rights reserved.
 # 
 
 require 'rexml/document'
-require "#{ENV["TM_SUPPORT_PATH"]}/lib/plist"
 require 'time'
+require 'uri'
+require 'yaml'
+
+require "#{ENV["TM_SUPPORT_PATH"]}/lib/plist"
+require "#{ENV["TM_SUPPORT_PATH"]}/lib/dialog"
+require "#{ENV["TM_SUPPORT_PATH"]}/lib/progress"
+require "#{ENV["TM_SUPPORT_PATH"]}/lib/escape"
+
+ListNib = File.dirname(__FILE__) + "/nibs/RevisionSelector.nib"
 
 module Subversion
+	
+	def self.svn_cmd(args)
+		result_text = %x{"#{ENV['TM_SVN']||'svn'}" #{args}}
+		raise if $CHILD_STATUS != 0		
+		result_text
+	end
+	
+	def self.human_readable_mktemp(filename, rev)
+		extname = File.extname(filename)
+		filename = File.basename(filename)
+		# TODO: Make sure the filename can fit in 255 characters, the limit on HFS+ volumes.
+		
+		"#{filename}-r#{rev}#{extname}"
+	end
+	
+	def self.view_revision(path)
+		
+		# Get the desired revision number
+		revision = choose_revision(path, "Choose a revision to view" )
+		return if revision.nil?
+
+		# Get the file at the desired revision
+		log_data = ''
+		TextMate.call_with_progress(:title => "R",
+	          :summary => "Retrieving revision #{revision}…",
+						:details => "#{File.basename(path)}") do
+			
+			temp_name = '/tmp/' + human_readable_mktemp(path, revision)
+			svn_cmd("cat -r#{revision} #{e_sh(path)} > #{temp_name}")
+			
+			# Open the file in TextMate and then delete it when it's closed.
+			%x{"#{ENV['TM_SUPPORT_PATH']}/bin/mate" -w #{e_sh(temp_name)}}
+			File.delete(temp_name)
+	  end
+		
+	end
+	
+	# on failure: returns nil
+	def self.choose_revision(path, prompt)
+		escaped_path = e_sh(path)
+		
+		# Get the server name
+	  info = YAML::load(svn_cmd("info #{escaped_path}"))
+		repository = info['Repository Root']
+		uri = URI::parse(repository)
+
+		# Display progress dialog
+		log_data = ''
+		TextMate.call_with_progress(:title => "Retrieving List of Revisions",
+	          :summary => "Reading the log from “#{uri.host}”…",
+						:details => "#{File.basename(path)}") do
+				log_data = svn_cmd("log --xml #{escaped_path}")
+	  end
+		
+		# Parse the log
+		plist = []
+		Subversion::LogParser.parse(log_data) do |hash|
+			plist << hash
+		end
+		
+		if plist.size == 0
+			TextMate::Dialog.alert("No older revisions of file “#{path}” found", "There's only one revision of this file, and you already have it.")
+		end
+		
+		# Show the log and ask for the revision number
+		revision = 0
+		TextMate::Dialog.dialog(ListNib, {'entries' => plist}) do |dialog|
+			dialog.wait_for_input do |params|
+				revision = params['returnArgument']
+				false
+			end
+			dialog.close
+		end
+		
+		# Return the revision number or nil
+		revision = nil if revision == 0
+		revision
+	end
 	
 	# Streaming 'svn log --xml' output parser.
 	class LogParser
@@ -18,7 +111,7 @@ module Subversion
 		# path may be a Subversion working copy path or a repository URL
 		def LogParser.parse_path(path, &block)
 			path = File.expand_path(path)
-			log_cmd = %Q{svn log --xml "#{path}"}
+			log_cmd = %Q{"#{ENV['TM_SVN']||svn}" log --xml "#{path}"}
 
 			IO.popen(log_cmd, "r") do |io|
 				LogParser.parse(io) do |hash|
@@ -30,6 +123,8 @@ module Subversion
 		# source may be a string or an IO subclass
 		def LogParser.parse(source, &block)
 			listener = LogParser.new(&block)
+			
+			# TODO: remove and report any text prior to the XML data
 			REXML::Document.parse_stream(source, listener)
 		end
 

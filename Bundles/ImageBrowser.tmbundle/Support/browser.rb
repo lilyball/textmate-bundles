e2177ff5678fada6b3cb7673ae1b3ff9a83d0b1d
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-02-23 17:00:06 +0000

    • move resources to Support
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6626 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/ImageBrowser.tmbundle/Support/browser.rb
+++ Bundles/ImageBrowser.tmbundle/Support/browser.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 PROJECT_ROOT = ARGV[0] # Where to browse for images
-RESOURCES_DIR = "#{ENV['TM_BUNDLE_SUPPORT']}/../Resources" # Where to find ImageBrowser's images
+RESOURCES_DIR = "#{ENV['TM_BUNDLE_SUPPORT']}/Resources" # Where to find ImageBrowser's images
 require "#{ENV['TM_BUNDLE_SUPPORT']}/image_size.rb"
 require 'erb'
 
-erb = ERB.new(IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../Resources/browser.rhtml"))
+erb = ERB.new(IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/Resources/browser.rhtml"))
 erb.run

621d7c128e0bec4eedc69670f3de3ff15b41dde0
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-03-04 20:56:10 +0000

    • proof-of-concept fetching bundle descriptions from the repository — this is horribly slow and currently doesn’t allow installing the selected bundles. It works fairly well though as a showcase for how tm_dialog can be used for “live” interfaces :)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6719 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/GetBundle.tmbundle/Support/show_bundles.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby -wKU
+
+SUPPORT = ENV['TM_SUPPORT_PATH']
+require SUPPORT + '/lib/escape.rb'
+require SUPPORT + '/lib/osx/plist'
+require 'rexml/document'
+require 'erb'
+
+include ERB::Util
+
+DIALOG = e_sh ENV['DIALOG'];
+NIB = 'Bundles'
+
+def strip_html(text)
+  text.gsub(/<[^>]+>/, '')
+end
+
+params = {
+  'isBusy'                  => true,
+  'progressText'            => 'Fetching List…',
+  'progressMinValue'        => 0,
+  'progressIsIndeterminate' => true,
+  'bundles'                 => [ ]
+}
+
+token = %x{#{DIALOG} -a #{e_sh NIB} -p #{e_sh params.to_plist}}
+
+x = Thread.new do
+
+  bundles = [ ]
+  open("|svn list --xml http://macromates.com/svn/Bundles/trunk/Bundles") do |io|
+
+    doc = REXML::Document.new io
+    doc.elements.to_a("//entry/name").each do |node|
+      bundles << { 'name' => $1, 'path' => $& } if node.text =~ /^(.*)\.tmbundle$/
+    end
+    
+    params['bundles']                 = bundles
+    params['isBusy']                  = true
+    params['progressText']            = "Fetching Descriptions (1 / #{bundles.size})…"
+    params['progressValue']           = 0
+    params['progressMinValue']        = 0
+    params['progressMaxValue']        = bundles.size
+    params['progressIsIndeterminate'] = false
+
+    open("|#{DIALOG} -t#{token}", "w") { |io| io.write params.to_plist }
+    params['isBusy']                  = true
+
+    bundles.each do |bundle|
+      puts "Load #{bundle['path']}…"
+      plist = open("|svn cat http://macromates.com/svn/Bundles/trunk/Bundles/#{url_encode bundle['path']}/info.plist") { |io| PropertyList.load(io) }
+      # puts "Got #{plist}"
+      bundle['uuid'] = plist['uuid'] unless plist['uuid'].nil?
+      bundle['name'] = plist['name'] unless plist['name'].nil?
+      bundle['bundleDescription'] = strip_html plist['description'].to_s
+      params['progressValue'] += 1
+      params['progressText'] = "Fetching Descriptions (#{params['progressValue'] + 1} / #{bundles.size})…"
+      open("|#{DIALOG} -t#{token}", "w") { |io| io.write params.to_plist }
+    end
+
+  end
+
+end
+
+puts %x{#{DIALOG} -w#{token}}
+%x{#{DIALOG} -x#{token}}
+
+x.kill; x.join

c5bdbd63675637055a8f4897fd98787a39ca3ec9
JEG2 <JEG2@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-03-09 21:15:00 +0000

    Fixing validate_uuids.rb to support multiple directories and include Disable Bundles by default.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6764 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/bin/validate_uuids.rb
+++ Support/bin/validate_uuids.rb
@@ -3,22 +3,31 @@
 require "find"
 require File.join(File.dirname(__FILE__), *%w[.. lib osx plist])
 
-ROOT_DIR = ARGV.shift || File.join(File.dirname(__FILE__), *%w[.. .. Bundles])
+ROOT_DIRS = if ARGV.empty?
+  %w[Bundles Disabled\ Bundles].map do |rel|
+    File.join(File.dirname(__FILE__), *%W[.. .. #{rel}])
+  end
+else
+  ARGV
+end
 
 uuids = Hash.new { |ids, id| ids[id] = Array.new }
 
 puts "Searching bundles ..." if $DEBUG
-Find.find(ROOT_DIR) do |path|
-  if File.file?(path) and File.extname(path) =~ /.*\.(tm[A-Z][a-zA-Z]+|plist)\Z/
-    begin
-      plist = File.open(path) { |io| PropertyList.load(io) }
-      if uuid = plist["uuid"]
-        uuids[uuid] << path
-      else
-        warn "Could not find a UUID for #{path}." if $DEBUG
+ROOT_DIRS.each do |root_dir|
+  Find.find(root_dir) do |path|
+    if File.file?(path) and
+       File.extname(path) =~ /.*\.(tm[A-Z][a-zA-Z]+|plist)\Z/
+      begin
+        plist = File.open(path) { |io| PropertyList.load(io) }
+        if uuid = plist["uuid"]
+          uuids[uuid] << path
+        else
+          warn "Could not find a UUID for #{path}." if $DEBUG
+        end
+      rescue
+        warn "Skipping #{path} due to #{$!.message}." if $DEBUG
       end
-    rescue
-      warn "Skipping #{path} due to #{$!.message}." if $DEBUG
     end
   end
 end

68e2efb3c791f46ab10ab055406b3f395cb72ba2
ale <ale@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-03-14 19:27:00 +0000

    Moved MTASC libraries to Support/bin/ to have MTASC autolink needed bits (instead of including everything)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@6802 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/ActionScript.tmbundle/Support/Tools/build_with_mtasc.rb
+++ Bundles/ActionScript.tmbundle/Support/Tools/build_with_mtasc.rb
@@ -23,74 +23,67 @@ require ENV["TM_SUPPORT_PATH"] + "/lib/web_preview"
 @q = "\""
 
 def warning(text, title)
-	html_header("Error!","Build With MTASC")
-	puts "<h1>#{title}</h1>"
-	puts "<p>#{text}</p>"
-	html_footer
-	TextMate.exit_show_html
+  html_header("Error!","Build With MTASC")
+  puts "<h1>#{title}</h1>"
+  puts "<p>#{text}</p>"
+  html_footer
+  TextMate.exit_show_html
 end
 
 def mtasc_compile
-	if !@project_path || !File.exist?("#{@project_path}/mtasc.yaml")
-		@project_path = File.dirname @file_path
-	end
-	Dir.chdir(@project_path)
-	if File.exists?(@project_path + "/mtasc.yaml")
-		yml = YAML.load(File.open('mtasc.yaml'))
-		if yml['mtasc_path']
-			@mtasc_path = yml['mtasc_path']
-		else
-			@mtasc_path = ENV['TM_BUNDLE_SUPPORT'] + "/bin/mtasc"
-		end
-		@app = yml['app']
-		@swf = yml['swf']
-		@player = yml['player']
-		@width = yml['width']
-		@height = yml['height']
-		# If width or height is a percentage. MTASC chokes because it needs a number. Fix it.
-		if @width =~ /%/ || @height =~ /%/
-			@size_is_percentage = true
-			@width.gsub!(/%/,'')
-			@height.gsub!(/%/,'')
-		end
-		@fps = yml['fps']
-		@classpaths = yml['classpaths']
-		@trace = yml['trace'] if !yml['trace'].nil?
-		@preview = yml['preview'] || "textmate"
-		@bgcolor = yml['bgcolor'] || "FFFFFF"
-		@params = yml['params'] if !yml['params'].nil?
-	else
-		@mtasc_path = ENV['TM_BUNDLE_SUPPORT'] + "/bin/mtasc"
-		@app = @file_name
-		@swf = File.basename(@file_name,".as") + ".swf"
-		@player = 8
-		@width = 800
-		@height = 600
-		@fps = 31
-		@preview = "textmate"
-		@bgcolor = "FFFFFF"
-	end
+  if !@project_path || !File.exist?("#{@project_path}/mtasc.yaml")
+    @project_path = File.dirname @file_path
+  end
+  Dir.chdir(@project_path)
+  if File.exists?(@project_path + "/mtasc.yaml")
+    yml = YAML.load(File.open('mtasc.yaml'))
+    if yml['mtasc_path']
+      @mtasc_path = yml['mtasc_path']
+    else
+      @mtasc_path = ENV['TM_BUNDLE_SUPPORT'] + "/bin/mtasc"
+    end
+    @app = yml['app']
+    @swf = yml['swf']
+    @player = yml['player']
+    @width = yml['width']
+    @height = yml['height']
+    # If width or height is a percentage. MTASC chokes because it needs a number. Fix it.
+    if @width =~ /%/ || @height =~ /%/
+      @size_is_percentage = true
+      @width.gsub!(/%/,'')
+      @height.gsub!(/%/,'')
+    end
+    @fps = yml['fps']
+    @classpaths = yml['classpaths']
+    @trace = yml['trace'] if !yml['trace'].nil?
+    @preview = yml['preview'] || "textmate"
+    @bgcolor = yml['bgcolor'] || "FFFFFF"
+    @params = yml['params'] if !yml['params'].nil?
+  else
+    @mtasc_path = ENV['TM_BUNDLE_SUPPORT'] + "/bin/mtasc"
+    @app = @file_name
+    @swf = File.basename(@file_name,".as") + ".swf"
+    @player = 8
+    @width = 800
+    @height = 600
+    @fps = 31
+    @preview = "textmate"
+    @bgcolor = "FFFFFF"
+  end
 
-	# MTASC binary
-	cmd = @q + @mtasc_path + @q
+  # MTASC binary
+  cmd = @q + @mtasc_path + @q
 
-	# App name
-	cmd += " " + @q + @app + @q
+  # App name
+  cmd += " " + @q + @app + @q
 
-	# Player version
-	cmd += " -version " + @player.to_s
+  # Player version
+  cmd += " -version " + @player.to_s
 
-	# Standard Adobe Classes
-	cmd += " -cp \"#{ENV['TM_BUNDLE_SUPPORT']}/lib/std/\" "
-	cmd += " -cp \"#{ENV['TM_BUNDLE_SUPPORT']}/lib/std8/\" "
-
-	# XTrace Classes
-	cmd += " -cp \"#{ENV['TM_BUNDLE_SUPPORT']}/lib/\" "
-
-	# User-provided Classpath
-	if @classpaths
-		cmd += " -cp \"#{@classpaths.join('" -cp "')}\" "
-	end
+  # User-provided Classpath
+  if @classpaths
+    cmd += " -cp \"#{@classpaths.join('" -cp "')}\" "
+  end
 
   # Additional parameters from mtasc.yaml
   if @params
@@ -101,6 +94,7 @@ def mtasc_compile
       # Use XTrace for debugging
       `open "$TM_BUNDLE_SUPPORT/bin/XTrace.app"`
       cmd += " -pack com/mab/util "
+      cmd += " -cp \"#{ENV['TM_BUNDLE_SUPPORT']}/lib/\" "
       cmd += " -trace com.mab.util.debug.trace "
     elsif @trace == "console"
       # do nothing
@@ -113,32 +107,32 @@ def mtasc_compile
     cmd += " -header #{@width}:#{@height}:#{@fps}"
   end
 
-	cmd += " -main "
-	cmd += " -swf #{@swf}"
+  cmd += " -main "
+  cmd += " -swf #{@swf}"
 
-	stdin, stdout, stderr = Open3.popen3(cmd)
-	warnings = []
-	errors = []
+  stdin, stdout, stderr = Open3.popen3(cmd)
+  warnings = []
+  errors = []
 
-	while err = stderr.gets
-		if err[0, 10] == 'Warning : '
-			warnings.push(err.chomp)
-		else
-			m = /(.+):([0-9]+): characters ([0-9]+)/.match(err)
-			if m != nil
-				a = "txmt://open?url=file://#{@project_path}/#{m[1]}&line=#{m[2]}&column=#{m[3].to_i + 1}"
-				err = "<a href=\"#{a}\">#{err}</a>"
-			end
-			errors.push(err.chomp)
-		end
-	end
-	if !errors.empty?
-		warning "#{errors.uniq.join('</p><p>')} <br/> <pre>#{cmd}</pre>", "Errors:"
-	end
-	if !warnings.empty?
-		warning "#{warnings.uniq.join('</p><p>')} <br/> <pre>#{cmd}</pre>", "Warnings:"
-	end
-	if errors.empty? && warnings.empty?
+  while err = stderr.gets
+    if err[0, 10] == 'Warning : '
+      warnings.push(err.chomp)
+    else
+      m = /(.+):([0-9]+): characters ([0-9]+)/.match(err)
+      if m != nil
+        a = "txmt://open?url=file://#{@project_path}/#{m[1]}&line=#{m[2]}&column=#{m[3].to_i + 1}"
+        err = "<a href=\"#{a}\">#{err}</a>"
+      end
+      errors.push(err.chomp)
+    end
+  end
+  if !errors.empty?
+    warning "#{errors.uniq.join('</p><p>')} <br/> <pre>#{cmd}</pre>", "Errors:"
+  end
+  if !warnings.empty?
+    warning "#{warnings.uniq.join('</p><p>')} <br/> <pre>#{cmd}</pre>", "Warnings:"
+  end
+  if errors.empty? && warnings.empty?
     if @trace == "console"
       `open -a Console.app "#{ENV['HOME']}/Library/Preferences/Macromedia/Flash Player/Logs/flashlog.txt"`
     end
@@ -148,32 +142,32 @@ def mtasc_compile
       if @size_is_percentage
         @width += "%"
         @height += "%"
-			end
-			puts "<html>"
-			puts "<head><title>#{@swf}</title></head>"
-			puts <<SWF_HTML
-			<body style="margin: 0; padding: 0;">
-			<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=#{@version},0,0,0" width="#{@width}" height="#{@height}" id="myApplication" align="middle">
-				<param name="allowScriptAccess" value="always">
-				<param name="movie" value="#{output_file}">
-				<param name="quality" value="high">
-				<param name="bgcolor" value="##{@bgcolor}">
-				<embed src="#{output_file}" quality="high" bgcolor="##{@bgcolor}" width="#{@width}" height="#{@height}" name="myApplication" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">
-			</object>
-			</body>
-			</html>
+      end
+      puts "<html>"
+      puts "<head><title>#{@swf}</title></head>"
+      puts <<SWF_HTML
+      <body style="margin: 0; padding: 0;">
+      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=#{@version},0,0,0" width="#{@width}" height="#{@height}" id="myApplication" align="middle">
+        <param name="allowScriptAccess" value="always">
+        <param name="movie" value="#{output_file}">
+        <param name="quality" value="high">
+        <param name="bgcolor" value="##{@bgcolor}">
+        <embed src="#{output_file}" quality="high" bgcolor="##{@bgcolor}" width="#{@width}" height="#{@height}" name="myApplication" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">
+      </object>
+      </body>
+      </html>
 SWF_HTML
-			TextMate.exit_show_html
-		else
-			# Open user-defined preview
-			`open #{@preview}`
-		end
-	else
-		TextMate.exit_show_html
-	end
+      TextMate.exit_show_html
+    else
+      # Open user-defined preview
+      `open #{@preview}`
+    end
+  else
+    TextMate.exit_show_html
+  end
 end
 
 # compile with MTASC
 TextMate.call_with_progress({:title => "MTASC", :message => "Compiling Classes"}) do
-	mtasc_compile
+  mtasc_compile
 end

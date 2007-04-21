bcc852a84d68bccede57c189ed1bc9e46ff9d7d9
ale <ale@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-04-21 08:02:18 +0000

    Added support for Flash CS3 help in the "Find in Actionscript Dictionary" (Ctrl + H) command.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@7025 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/ActionScript.tmbundle/Support/Tools/asd.rb
+++ Bundles/ActionScript.tmbundle/Support/Tools/asd.rb
@@ -2,59 +2,78 @@
 #
 # Ale Muñoz <ale@bomberstudios.com> - 2006-11-25
 # Feel free to use and improve...
+# 
 
-require "#{ENV["TM_SUPPORT_PATH"]}/lib/web_preview"
+require ENV['TM_SUPPORT_PATH'] + "/lib/exit_codes"
+require ENV['TM_SUPPORT_PATH'] + "/lib/escape"
+require ENV['TM_SUPPORT_PATH'] + "/lib/progress"
+require ENV["TM_SUPPORT_PATH"] + "/lib/web_preview"
 require "rexml/document"
 
 WORD    = ENV['TM_CURRENT_WORD']
-HELPDIR		= ENV['TM_FLASH_HELP']
-HELPTOC		= 'help_toc.xml'
+HELPTOC = 'help_toc.xml'
 
-# Help dir not set?
-if !HELPDIR
-	puts html_head(
-		:title => "Error!",
-		:subtitle => "Find in ActionScript Dictionary"
-	)
-	puts <<HEREDOC
-	<h1>Search failed for #{WORD}</h1>
-	<p>In order for this command to work TextMate needs to know where to find the ActionScript Dictionary index file. This is set in <strong>Preferences > Advanced > Shell Variables</strong>.</p>
-	<p>Click <strong>+</strong> and name the variable <code>TM_FLASH_HELP</code> with value of the path to the directory containing the help_toc.xml file on your system.</p>
-	<p>Macromedia have made this tricky and the files arent always in the same place.</p>
-	<p>Try clicking the following links:
-	<ul>
-	<li><a href="txmt://open?url=file:///Users/Shared/Library/Application Support/Macromedia/Flash 8/en/Configuration/HelpPanel/Help/ActionScriptLangRef/help_toc.xml">//Users/Shared/Library/Application Support/Macromedia/Flash 8/en/Configuration/HelpPanel/Help/ActionScriptLangRef</a></li>
-	<li><a href="txmt://open?url=file:///Users/Shared/Library/Application Support/Macromedia/Flash MX 2004/en/Configuration/HelpPanel/Help/ActionScriptLangRef/help_toc.xml">//Users/Shared/Library/Application Support/Macromedia/Flash MX 2004/en/Configuration/HelpPanel/Help/ActionScriptLangRef</a></li>
-	<li><a href="txmt://open?url=file:///Applications/Macromedia Flash MX 2004/First Run/HelpPanel/Help/ActionScriptDictionary">/Applications/Macromedia Flash MX 2004/First Run/HelpPanel/Help/ActionScriptDictionary</a></li>
-	</ul>
-	<p>If any of the links work (TextMate will open the help_toc.xml file) then copy and paste the link, omitting the /help_toc.xml on the end, to the path of the shell variable.
-HEREDOC
-	html_footer
+help_dir_options = [
+	"#{ENV['HOME']}/Library/Application Support/Adobe/Flash CS3/en/Configuration/HelpPanel/Help/ActionScriptLangRefV2",
+	"/Users/Shared/Library/Application Support/Macromedia/Flash 8/en/Configuration/HelpPanel/Help/ActionScriptLangRef",
+	"/Users/Shared/Library/Application Support/Macromedia/Flash MX 2004/en/Configuration/HelpPanel/Help/ActionScriptLangRef"
+]
+
+# Try to locate help dir
+if %x(defaults read com.macromates.textmate.actionscript FlashDocumentationPath).empty?
+	help_dir_options.each do |path|
+		if File.exist? "#{path}/#{HELPTOC}"
+			help_dir = path
+			%x(defaults write com.macromates.textmate.actionscript FlashDocumentationPath "#{help_dir}")
+			break
+		end
+	end
+	if help_dir.empty?
+		# Help dir not found...
+		puts html_head({:title => "Error!", :sub_title => "Find in ActionScript Dictionary"})
+		puts "
+		<h1>Search failed for “#{WORD}”</h1>
+		<p>In order for this command to work TextMate needs to know where to find the ActionScript Dictionary index file. The commands tries to find it in the following places:</p>
+		<ul>
+			<li>#{ENV['HOME']}/Library/Application Support/Adobe/Flash CS3/en/Configuration/HelpPanel/Help/ActionScriptLangRefV2/help_toc.xml</li>
+			<li>/Users/Shared/Library/Application Support/Macromedia/Flash 8/en/Configuration/HelpPanel/Help/ActionScriptLangRef/help_toc.xml</li>
+			<li>/Users/Shared/Library/Application Support/Macromedia/Flash MX 2004/en/Configuration/HelpPanel/Help/ActionScriptLangRef/help_toc.xml</li>
+		</ul>
+		<p>If the command can't find the index file, you can set a search path manually:
+			<ul>
+				<li>Open <strong>Preferences » Advanced » Shell Variables</strong>.</li>
+				<li>Click <strong>+</strong></li>
+				<li>Name the variable <code>TM_FLASH_HELP</code></li>
+				<li>Set the value to the path to the directory containing the help_toc.xml file on your system.</li>
+			</ul>
+		</p>
+	"
+		html_footer
+		exit
+		# TextMate.exit_show_html output
+	end
 else
-
-	# Open TOC...
-	toc_lines = IO.readlines(HELPDIR + "/" + HELPTOC)
-
-	# ...find matching lines...
-	search_results = []
-	toc_lines.each do |line|
-		search_results << line.strip if line[/name=\"#{WORD}/]
-	end
-
-	puts html_head( :title => "Documentation for ‘#{WORD}’", :sub_title => "ActionScript Dictionary" )
-
-	# ...parse results for links...
-	links = []
-	puts "<ul>"
-	search_results.each do |line|
-		xml_line = REXML::Document.new(line)
-		puts "<li><a href=\"tm-file://#{HELPDIR}/#{xml_line.root.attributes['href']}\">#{xml_line.root.attributes['name']}</a></li>"
-	end
-	puts "</ul>"
-
-	# ...or display error if no matches
-	puts "No results :(" if search_results.size == 0
-
-	html_footer
-
+	help_dir = %x(defaults read com.macromates.textmate.actionscript FlashDocumentationPath).chomp
 end
+
+# Find matching lines...
+search_results = []
+open("#{help_dir}/#{HELPTOC}").each do |line|
+	search_results << line.strip if line[/name=\"#{WORD}/]
+end
+
+puts html_head( :title => "Documentation for ‘#{WORD}’", :sub_title => "ActionScript Dictionary" )
+
+# ...parse results for links...
+links = []
+puts "<ul>"
+search_results.each do |line|
+	xml_line = REXML::Document.new(line)
+	puts "<li><a href=\"tm-file://#{help_dir}/#{xml_line.root.attributes['href']}\">#{xml_line.root.attributes['name']}</a></li>"
+end
+puts "</ul>"
+
+# ...or display error if no matches
+puts "No results :(" if search_results.size == 0
+
+html_footer

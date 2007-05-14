cfd4114928d310ebccf25270145d8c23c344592f
simon <simon@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-05-14 16:12:46 +0000

    Fixed problems in the Flex project template.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@7254 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/Flex.tmbundle/Templates/Project - MXML.tmTemplate/compile.sh
+++ Review/Bundles/Flex.tmbundle/Templates/Project - MXML.tmTemplate/compile.sh
@@ -7,12 +7,14 @@
 echo "<h2>${TM_NEW_FILE_BASENAME} Custom Compile</h2>";
 echo "<code> Started @ `date "+%H:%M:%S"`</code><br />";
 
-"$TM_FLEX_PATH/bin/mxmlc" \
+"mxmlc" \
 	-sp+="$TM_AS3_LIB_PATH" \
-	-file-specs="src/${TM_NEW_FILE_BASENAME}.as" \
+	-file-specs="src/${TM_NEW_FILE_BASENAME}.mxml" \
 	-o="deploy/${TM_NEW_FILE_BASENAME}.swf" 2>&1 | "$TM_BUNDLE_SUPPORT/bin/parse_mxmlc_out.py";
 
-if [ "$?" == "0" ]
-	then
-	open "deploy/${TM_NEW_FILE_BASENAME}.html";
-fi
+# if [ "$?" == "0" ]
+# 	then
+# 	open "deploy/${TM_NEW_FILE_BASENAME}.html";
+# fi
+
+exit 206;

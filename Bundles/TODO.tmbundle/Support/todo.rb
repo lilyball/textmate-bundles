79d1aa6971d3ba80a81577ceaa5dfa82a3370cd5
Soryu <Soryu@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-06-23 13:51:37 +0000

    • TODO Tags/Markers now configurable via Preferences item • Removed global variables and used bindings for ERB
    git-svn-id: http://macromates.com/svn/Bundles/trunk@7566 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/TODO.tmbundle/Support/todo.rb
+++ Bundles/TODO.tmbundle/Support/todo.rb
@@ -26,27 +26,25 @@ require "erb"
 require "yaml"
 include ERB::Util
 
-loaded_tags = YAML.load_file("#{ENV['TM_BUNDLE_SUPPORT']}/tags.yaml")
+tag_names = ENV['TM_TODO_MARKERS'].split(",")
+tag_trim = ENV['TM_TODO_MARKERS_TRIM'].split(",")
 
-# TODO: ok, this is kind of ugly and certainly could be done inline.
-# my attempts using inject on the hashes did not work however.
-$tags = []
-loaded_tags.each { |tag|
-  new_tag = {}
-  tag.each do |key, value|
-    if value == 'true'
-      new_tag[key.to_sym] = true
-    elsif value == 'false'
-      new_tag[key.to_sym] = false
-    elsif value =~ /^\/.*\/i?$/
-      new_tag[key.to_sym] = Regexp.new(value.sub(/^\/(.*)\/i?$/, '\1'), (value =~ /i$/) ? 129 : 128)
-    else
-      new_tag[key.to_sym] = value
-    end
-  end
-  $tags << new_tag
+trim_tags = []
+tag_trim.each { |name|
+  trim_tags << name.strip
 }
 
+tags = []
+tag_names.each { |name|
+  name.strip!
+  regexp = ENV["TM_TODO_#{name}_REGEXP"]
+  tags << {
+    :label => name,
+    :color => (ENV["TM_TODO_#{name}_COLOR"] or '#808080'),
+    :regexp => Regexp.new(regexp.sub(/^\/(.*)\/i?$/, '\1'), (regexp =~ /i$/) ? 129 : 128),
+    :trim_if_empty => trim_tags.include?(name)
+  }
+}
 ignores = ENV['TM_TODO_IGNORE']
 
 def TextMate.file_link (file, line = 0)
@@ -55,28 +53,29 @@ def TextMate.file_link (file, line = 0)
     "&amp;line=" + line.to_s
 end
 
-$tags.each do |tag|
+tags.each do |tag|
   tag[:matches] = []
   tag[:rendered] = ''
 end
 
-html_head = ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_html_header.rhtml"), 0, '<>').result
+html_head = ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_html_header.rhtml"), 0, '<>').result(binding)
 
 puts html_head(:window_title => "TODO", :page_title => "TODO List", :sub_title => ENV['TM_PROJECT_DIRECTORY'], :html_head => html_head)
-tmpl_file = "#{ENV['TM_BUNDLE_SUPPORT']}/template_head.rhtml"
-puts ERB.new(File.open(tmpl_file), 0, '<>').result
+
+puts ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_head.rhtml"), 0, '<>').result(binding)
+
 STDOUT.flush
 
 home_dir = /^#{Regexp.escape ENV['HOME']}/
-$total = 0
+total = 0
 TextMate.each_text_file do |file|
   next if (ignores != nil and file =~ /#{ignores}/) or File.symlink?(file)
-  $file_name = file.sub(home_dir, '~')
-  puts ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_update_dir.rhtml"), 0, '<>').result
-  $tags.each do |tag|
+  file_name = file.sub(home_dir, '~')
+  puts ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_update_dir.rhtml"), 0, '<>').result(binding)
+  tags.each do |tag|
     File.open(file) do |io|
       io.grep(tag[:regexp]) do |content|
-        $match = {
+        match = {
           :file => file,
           :line => io.lineno,
           :content => content,
@@ -84,15 +83,15 @@ TextMate.each_text_file do |file|
         }
         if tag[:label] == "RADAR" then
           url = "rdar://" + $2
-          $match[:match] = html_escape($1) + "<a href=\"" + url + "\">" + html_escape(url) + "</a>" + html_escape($3)
+          match[:match] = html_escape($1) + "<a href=\"" + url + "\">" + html_escape(url) + "</a>" + html_escape($3)
         else
-          $match[:match] = html_escape($1)
+          match[:match] = html_escape($1)
         end
-        tag[:matches] << $match
-        $count = tag[:matches].length
-        $total += 1
-        puts ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_update.rhtml"), 0, '<>').result
-        tag[:rendered] += ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_item.rhtml"), 0, '<>').result
+        tag[:matches] << match
+        count = tag[:matches].length
+        total += 1
+        puts ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_update.rhtml"), 0, '<>').result(binding)
+        tag[:rendered] += ERB.new(File.open("#{ENV['TM_BUNDLE_SUPPORT']}/template_item.rhtml"), 0, '<>').result binding
         STDOUT.flush
       end
     end if File.readable?(file)
@@ -100,7 +99,7 @@ TextMate.each_text_file do |file|
 end
 
 tmpl_file = "#{ENV['TM_BUNDLE_SUPPORT']}/template_tail.rhtml"
-puts ERB.new(File.open(tmpl_file), 0, '<>').result
+puts ERB.new(File.open(tmpl_file), 0, '<>').result(binding)
 
 html_footer()
 

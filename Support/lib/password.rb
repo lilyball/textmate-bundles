01c0458a7529d86757c10816c7a5249a9586c56d
ciaran <ciaran@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-06-25 10:16:31 +0000

    • Replaced CocoaDialog usage with TextMate::UI.request_secure_string • Changed the call to `security` which requests the password to be passed through exec(). This allows us to alter the argv[0] of the command, so security prompts will now say “TextMate” instead of “security” (which could be confusing)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@7571 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/password.rb
+++ Support/lib/password.rb
@@ -5,6 +5,8 @@
 #   TextMate.call_with_password({ :user => 'duff', :url => 'http://example.com/blog/xmlrpc.php' }) do |pw|
 #     pw == "foo" ? :reject_pw : :accept_pw
 #   end
+require "#{ENV["TM_SUPPORT_PATH"]}/lib/ui"
+require "#{ENV["TM_SUPPORT_PATH"]}/lib/textmate"
 
 module TextMate
 
@@ -15,20 +17,26 @@ module TextMate
 
     action = :reject_pw
 
-    res = %x{security find-internet-password -g -a "#{user}" -s "#{host}" -p "#{path}" -r #{proto} 2>&1 >/dev/null}
-    action = block.call($1) if res =~ /^password: "(.*)"$/
-    
-    while action == :reject_pw
-      cd_path = ENV['TM_SUPPORT_PATH'] + '/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog'
-      res = %x{"#{cd_path}" secure-standard-inputbox \
-        --title "Enter Password" \
-        --informative-text "Enter password for #{user} at #{proto}://#{host}#{path}"
-      }
-      break if res[0] == ?2
+    rd, wr = IO.pipe
+    if pid = fork
+      wr.close
+      Process.waitpid(pid)
+    else
+      STDERR.reopen(wr)
+      STDOUT.reopen('/dev/null', 'r')
+      rd.close; wr.close
+      exec(['/usr/bin/security', TextMate.app_path + "/Contents/MacOS/TextMate"], 'find-internet-password', '-g', '-a', user, '-s', host, '-r', proto)
+    end
 
-      action = block.call(res[2..-2])
+    action = block.call($1) if rd.gets =~ /^password: "(.*)"$/
+    rd.close
+
+    while action == :reject_pw
+      break unless res = TextMate::UI.request_secure_string(:title => "Enter Password", :prompt => "Enter password for #{user} at #{proto}://#{host}#{path}")
+
+      action = block.call(res)
       if action == :accept_pw then
-        %x{security add-internet-password -a "#{user}" -s "#{host}" -r "#{proto}" -p "#{path}" -w "#{res[2..-2]}"}
+        %x{security add-internet-password -a "#{user}" -s "#{host}" -r "#{proto}" -p "#{path}" -w "#{res}"}
       end
     end
 

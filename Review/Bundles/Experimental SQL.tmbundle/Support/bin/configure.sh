9ade6f97a7114cbeb885a0ed9e0bf8e393615a76
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-06-27 04:32:09 +0000

    • be clever and put $LOGNAME in the user name field
    git-svn-id: http://macromates.com/svn/Bundles/trunk@7594 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/Experimental SQL.tmbundle/Support/bin/configure.sh
+++ Review/Bundles/Experimental SQL.tmbundle/Support/bin/configure.sh
@@ -1,1 +1,1 @@
-"$DIALOG" -d'{sqlConnections = ( { title = "untitled"; serverType = MySQL; hostName = localhost; } ); }' -n'{ SQL_New_Connection = { title = "untitled"; serverType = MySQL; hostName = localhost; }; }' -p{} "${TM_BUNDLE_SUPPORT}/nibs/connections.nib" &>/dev/console &
+"$DIALOG" -q -d"{sqlConnections = ( { title = untitled; serverType = MySQL; hostName = localhost; userName = '$LOGNAME'; } ); }" -n"{ SQL_New_Connection = { title = untitled; serverType = MySQL; hostName = localhost; userName = '$LOGNAME'; }; }" -p'{}' "${TM_BUNDLE_SUPPORT}/nibs/connections.nib" &>/dev/console &

c4ede25a9e3a176d9d446eba66d52e82a8d1b377
fred <fred@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-08-12 16:16:15 +0000

    Add a default value in case OakTextViewNormalFontName is not set. Patch from Vitaly.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@7957 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Mercurial.tmbundle/Support/getfontpref.rb
+++ Bundles/Mercurial.tmbundle/Support/getfontpref.rb
@@ -6,12 +6,12 @@ textmate_pref_file = '~/Library/Preferences/com.macromates.textmate.plist'
 $prefs = OSX::PropertyList.load(File.open(File.expand_path(textmate_pref_file)))
  
 def getfontname
-  font_name = $prefs['OakTextViewNormalFontName']
+  font_name = $prefs['OakTextViewNormalFontName'] || 'Monaco'
   font_name = '"' + font_name + '"' if font_name.include?(' ') && !font_name.include?('"')
   return font_name
 end
 
 def getfontsize
-  font_size = ($prefs['OakTextViewNormalFontSize']).to_s
+  font_size = ($prefs['OakTextViewNormalFontSize'] || 11).to_s
 	font_size.sub!(/\.\d+$/, '')
 end

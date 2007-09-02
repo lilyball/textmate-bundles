7d624c68b9d338a9a3c86ab30c790660dc860360
aross <aross@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2007-09-02 08:48:49 +0000

    Modified TextMate::IO so that it uses a buffer to always return lines.  To turn off the line buffer, set TextMate::IO.sync = true.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@8107 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/io.rb
+++ Support/lib/io.rb
@@ -1,43 +1,91 @@
 module TextMate
   module IO
     class << self
+      
+      attr_accessor :sync
+      @sync = false
+      def sync?; @sync end
+      
       def exhaust(named_fds, &block)
+        
+        leftovers = {}
         named_fds = named_fds.dup
         named_fds.delete_if { |key, value| value.nil? }
-
+        
         until named_fds.empty? do
-          fd = select(named_fds.values)[0][0]
+          
+          fd   = select(named_fds.values)[0][0]
+          name = named_fds.find { |key, value| fd == value }.first
           data = fd.sysread(4096) rescue ""
+          
           if data.to_s.empty? then
-            named_fds.delete_if { |key, value| fd == value }
+            named_fds.delete(name)
             fd.close
-          else
+          
+          elsif not sync?
+            if data =~ /\A(.*\n|)([^\n]*)\z/m
+              lines = leftovers[name].to_s + $1
+              leftovers[name] = $2
+              case block.arity
+                when 1: lines.each { |line| block.call(line) }
+                when 2: lines.each { |line| block.call(line, name) }
+              end
+            else
+              raise "Allan's regexp did not match #{str}" 
+            end
+          
+          elsif sync?
             case block.arity
               when 1: block.call(data)
-              when 2: block.call(data, named_fds.find { |key, value| fd == value }.first)
+              when 2: block.call(data, name)
             end
+          
           end
         end
+        
+        # clean up the crumbs
+        if not sync?
+          leftovers.delete_if {|name,crumb| crumb == ""}
+          case block.arity
+            when 1: leftovers.each_pair { |name, crumb| block.call(crumb) }
+            when 2: leftovers.each_pair { |name, crumb| block.call(crumb, name) }
+          end
+        end
+        
       end
+      
     end
   end
 end
 
 # interactive unit tests
 if $0 == __FILE__
-
   require "open3"
 
-  stdin, stdout, stderr = Open3.popen3('echo foo; echo 1>&2 bar; echo fud')
+  puts "1=== Line by Line"
+  stdin, stdout, stderr = Open3.popen3("echo 'foo\nbar'; echo 1>&2 bar; echo fud")
   TextMate::IO.exhaust(:out => stdout, :err => stderr) do |data, type|
-    puts "#{type}: #{data}"
+    puts "#{type}: “#{data.rstrip}”"
   end
 
-  puts "----"
-
-  stdin, stdout, stderr = Open3.popen3('echo foo; echo 1>&2 bar; echo fud')
+  puts "2---"
+  stdin, stdout, stderr = Open3.popen3('echo oof; echo 1>&2 rab; echo duf')
   TextMate::IO.exhaust(:out => stdout, :err => stderr) do |data|
-    puts data
+    puts "“#{data.rstrip}”"
+  end
+  
+  # check that everything still works with sync enabled.
+  TextMate::IO.sync = true
+
+  puts "3==="  
+  stdin, stdout, stderr = Open3.popen3("echo 'foo\nbar'; echo 1>&2 bar; echo fud")
+  TextMate::IO.exhaust(:out => stdout, :err => stderr) do |data, type|
+    puts "#{type}: “#{data.rstrip}”"
   end
 
+  puts "4---"
+  stdin, stdout, stderr = Open3.popen3('echo oof; echo 1>&2 rab; echo duf')
+  TextMate::IO.exhaust(:out => stdout, :err => stderr) do |data|
+    puts "“#{data.rstrip}”"
+  end
 end

06cbc9ed3a029978b6eb323bcb263dd7ab4af207
ciaran <ciaran@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-02-14 19:25:00 +0000

    If an OBJROOT was not set then 'nil' would be used
    git-svn-id: http://macromates.com/svn/Bundles/trunk@8878 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Xcode.tmbundle/Support/bin/find_objroot.rb
+++ Bundles/Xcode.tmbundle/Support/bin/find_objroot.rb
@@ -4,7 +4,7 @@ require "#{ENV['TM_SUPPORT_PATH']}/lib/osx/plist"
 
 result = %x{defaults read com.apple.Xcode PBXApplicationwideBuildSettings}
 if $? == 0
-	print OSX::PropertyList::load(result)["OBJROOT"]
+	print OSX::PropertyList::load(result)["OBJROOT"].to_s
 	exit(0)
 else
 	exit($?)

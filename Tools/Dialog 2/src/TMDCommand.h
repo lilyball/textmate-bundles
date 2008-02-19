dd88432fc3795894131e096220a05ff48b4fb6f7
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-02-19 22:15:33 +0000

    • remove initial version of Dialog 2 since /branches/WIP/Tools/Dialog2 is what we are working on (and that isn’t a real branch of this version)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@8958 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/Dialog 2/src/TMDCommand.h
+++
@@ -1,7 +0,0 @@
-@interface TMDCommand : NSObject
-+ (void)registerObject:(id)anObject forCommand:(NSString*)aCommand;
-+ (id)objectForCommand:(NSString*)aCommand;
-
-+ (id)readPropertyList:(NSFileHandle*)aFileHandle;
-+ (void)writePropertyList:(id)aPlist toFileHandle:(NSFileHandle*)aFileHandle;
-@end

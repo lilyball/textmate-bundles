d4c9e185c1baa5e43d69bbf22ff897ce6235a872
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-03-05 22:21:16 +0000

    Ruby module to setup the environment for tm_dialog_read.dylib.
    Example: 'TextMate::DialogRead.init :title => "Window Title", :prompt => "The script is requesting some input:", :string => "default input", :nib => "Request String"
    
    All settings are optional. 
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9053 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Review/Tools/tmread/tm_dialog_read.rb
@@ -0,0 +1,29 @@
+module TextMate
+  module DialogRead
+    
+    @lib = ENV['TM_DIALOG_READ_DYLIB']
+    
+    class << self
+      def init(settings)
+        ENV['DIALOG_PROMPT'] = (settings.has_key? :prompt) ? settings[:prompt] : nil
+        ENV['DIALOG_TITLE'] = (settings.has_key? :title) ? settings[:title] : nil
+        ENV['DIALOG_STRING'] = (settings.has_key? :string) ? settings[:string] : nil
+        ENV['DIALOG_NIB'] = (settings.has_key? :nib) ? settings[:nib] : nil
+        insert unless inserted?
+      end
+    
+      def inserted? 
+          ENV['DYLD_INSERT_LIBRARIES'] =~ /$lib/
+      end
+
+      def insert
+        if ENV['DYLD_INSERT_LIBRARIES']
+            ENV['DYLD_INSERT_LIBRARIES'] = @lib + ':' + ENV['DYLD_INSERT_LIBRARIES']
+        else
+            ENV['DYLD_INSERT_LIBRARIES'] = @lib
+        end
+        ENV['DYLD_FORCE_FLAT_NAMESPACE'] = "1"
+      end
+    end
+  end
+end

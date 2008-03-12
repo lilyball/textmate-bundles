cabc4cdc9d27c39f7ba306f1a50dc389bb50a063
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-03-12 00:38:05 +0000

    Removed xcodeness, and categorised the files
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9092 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Review/Tools/tmread/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+dir=`dirname "$0"`
+build="$dir/build"
+src="$dir/src"
+
+if [ ! -d $build ]
+then
+    mkdir $build
+fi
+
+gcc -dynamiclib -o "$build/tm_dialog_read.dylib" -fno-common -framework CoreFoundation -framework Carbon -mmacosx-version-min=10.4 "$src/tm_dialog_read.c" 
--- Review/Tools/tmread/test/bootstrap.sh
+++ Review/Tools/tmread/test/bootstrap.sh
@@ -14,7 +14,9 @@ fi
 tm_dialog_read_init -p "Custom Prompt" -t "Custom Title" -n "/Applications/TextMate.app/Contents/SharedSupport/Support/nibs/RequestString.nib"
 
 # Does a simple gets with no DATA - this seems to work as expected
-$dir/simple-test.rb 
- 
+#$dir/simple-test.rb 
+
+nm -m /usr/lib/libruby.dylib | grep _read
+
 # Does a simple gets with DATA
 # $dir/simple-test-with-data.rb

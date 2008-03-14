e9085259a129e51184e76321f793a1aaa281a9ed
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-03-14 07:58:19 +0000

    • add quotes around use of $build shell variable
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9104 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Tools/tmread/build.sh
+++ Review/Tools/tmread/build.sh
@@ -4,9 +4,9 @@ dir=`dirname "$0"`
 build="$dir/build"
 src="$dir/src"
 
-if [ ! -d $build ]
+if [ ! -d "$build" ]
 then
-    mkdir $build
+    mkdir "$build"
 fi
 
 gcc -dynamiclib -o "$build/tm_dialog_read.dylib" -fno-common -framework CoreFoundation -mmacosx-version-min=10.4 -D__DARWIN_UNIX03=1  -D__DARWIN_NON_CANCELABLE=1 "$src/tm_dialog_read.c" 

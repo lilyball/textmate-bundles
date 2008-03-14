04d9af860a11b0319d02d8028d28f89624e2257b
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-03-14 09:14:54 +0000

    • set DIALOG to DIALOG_1 (when it exists) since presently there is a compatibility problem (which presumably needs to be fixed in D2)
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9111 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Tools/tmread/test/dylib/setup.sh
+++ Review/Tools/tmread/test/dylib/setup.sh
@@ -6,4 +6,9 @@ then
     exit 1  
 fi
 
+if [ -x "$DIALOG_1" ]; then # FIXME this is only because Dialog v2 is apparently not backwards compatible with these tests
+  export DIALOG="$DIALOG_1"
+fi
+
+
 . "$(dirname "$0")/../../helpers/tm_dialog_read.sh"

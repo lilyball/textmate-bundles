31fc6962602b03bb36ed4d3a6be005ec66c2771a
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-03-15 02:19:31 +0000

    • Normalised white space • Cleaned up some documentation
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9117 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Tools/tmread/src/tm_dialog_read.h
+++ Review/Tools/tmread/src/tm_dialog_read.h
@@ -1,14 +1,12 @@
-/*
- *  tm_dialog_read.h
- *  tm_dialog_read
- *
- *  Created by Luke Daley on 26/02/08.
- *  Copyright 2008 Code4Days Software. All rights reserved.
- *
+/**
+ * A read implementation for processes running under TextMate.
+ * 
+ * @author Luke Daley
+ * @author Allan Odgaard
  */
 
 #include <sys/types.h>
 
-ssize_t	 read(int, void *, size_t) __asm("_read");
-ssize_t	 read_unix2003(int, void *, size_t) __asm("_read$UNIX2003");
-ssize_t	 read_nocancel_unix2003(int, void *, size_t) __asm("_read$NOCANCEL$UNIX2003");
+ssize_t read(int, void *, size_t) __asm("_read");
+ssize_t read_unix2003(int, void *, size_t) __asm("_read$UNIX2003");
+ssize_t read_nocancel_unix2003(int, void *, size_t) __asm("_read$NOCANCEL$UNIX2003");

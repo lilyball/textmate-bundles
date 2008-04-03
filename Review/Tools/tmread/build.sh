b446cf2187116f0e34395959cf20a38c667c87f2
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-04-03 15:01:29 +0000

    Update build script to build for 64 bit architectures.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9276 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Tools/tmread/build.sh
+++ Review/Tools/tmread/build.sh
@@ -1,20 +1,42 @@
 #!/usr/bin/env bash
 
-dir="$(dirname "$0")"
-build="$dir/build"
-src="$dir/src"
+NDEBUG=1
 
-if [ ! -d "$build" ]
-then
-    mkdir "$build"
+SCRIPT_DIR="$(dirname "$0")"
+SRC_DIR="$SCRIPT_DIR/src"
+DST_DIR="$SCRIPT_DIR/build"
+LIB_NAME="tm_interactive_input.dylib"
+
+function build {
+  DEPLOYMENT=10.4
+  SDK=/Developer/SDKs/MacOSX10.4u.sdk
+
+  if [[ "$1" = *64 ]]; then
+  	DEPLOYMENT=10.5
+  	SDK=/Developer/SDKs/MacOSX10.5.sdk
+  fi
+
+  echo "Building ‘tm_interactive_input_$1.dylib’ (for ${DEPLOYMENT}${NDEBUG:+, no debug})…"
+  gcc -dynamiclib -Wmost -Os -fno-common \
+    -framework CoreFoundation \
+    -DDATE=\"$(date +%Y-%m-%d)\" \
+    ${NDEBUG:+-DNDEBUG=1} \
+    -mmacosx-version-min="$DEPLOYMENT" -isysroot "$SDK" \
+    -arch "$1" \
+    -o "$DST_DIR/tm_interactive_input_$1.dylib" \
+    "$SRC_DIR"/*.c
+}
+
+if [[ -d "$DST_DIR" ]]; then
+  echo "Cleaning old build dir (‘"$DST_DIR"’)…"
+  rm -rf "$DST_DIR"
 fi
 
-gcc -dynamiclib -Wmost -Os -fno-common \
- -DNDEBUG=1 \
- -DDATE=\"$(date +%Y-%m-%d)\" \
- -o "$build/tm_interactive_input.dylib" \
- -framework CoreFoundation \
- -arch ppc -arch i386 \
- -mmacosx-version-min=10.4 \
- -isysroot /Developer/SDKs/MacOSX10.4u.sdk \
- `ls $src/*.c`
+if ! mkdir "$DST_DIR"; then exit; fi
+
+for ARCH in ppc i386 ppc64 x86_64; do build "$ARCH"; done
+
+echo "Merging…"
+cd "$DST_DIR"
+lipo -create tm_interactive_input_*.dylib -output "$LIB_NAME" && rm tm_interactive_input_*.dylib
+lipo -info "$LIB_NAME"

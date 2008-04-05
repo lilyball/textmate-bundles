0c0321d0698dd05f5ca84831170edb827e1cee6e
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-04-05 07:47:53 +0000

    • Changed handling of reading stdin errors.
    If reading from stdin produced an error, then we just return the result of the syscall (previously we died fatally). Except when the error is EAGAIN. Processes running under TM may have their stdin closed, and that will cause EAGAIN which in our context is not really an error.
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9297 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Tools/tmread/src/read.c
+++ Review/Tools/tmread/src/read.c
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <CoreFoundation/CoreFoundation.h>
+#include <sys/errno.h>
 
 ssize_t read(int d, void *buffer, size_t buffer_length) {
 
@@ -29,8 +30,18 @@ ssize_t read(int d, void *buffer, size_t buffer_length) {
         ssize_t bytes_read = syscall(SYS_read, d, buffer, buffer_length);
         fcntl(d, F_SETFL, oldFlags);
 
+        /*
+            If reading from stdin produced an error, then we just return 
+            it's result. Except when the error is EAGAIN because processes
+            running under TM may have their stdin closed, and that will
+            cause EAGAIN which in our context is not really an error.
+        */
+
         D("read(): syscall returned %d bytes\n", (int)bytes_read);
-        if (bytes_read < 0) die("read syscall produced error: '%s' (%zd bytes read)", strerror(errno), bytes_read);
+        if (bytes_read < 0 && errno != EAGAIN) {
+            D("read syscall error: '%s'", strerror(errno), bytes_read);
+            return bytes_read;
+        }
 
         return (bytes_read > 0) ? bytes_read : tm_dialog_read(buffer, buffer_length);
     }

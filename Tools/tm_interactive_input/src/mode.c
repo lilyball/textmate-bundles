cefa2cd1dfe0eb1ebd9eb8bd9e80519392beec52
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-04-06 05:18:27 +0000

    • Rectified incorrect strsep() usage. • Rectified incorrect logic in tm_interactive_input_is_active()
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9320 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/tm_interactive_input/src/mode.c
+++ Tools/tm_interactive_input/src/mode.c
@@ -12,13 +12,14 @@ bool mode_contains(char *target) {
     
     // Because strsep modifies the string in place, we need to make a copy.
     char *mode_mask = get_tm_interactive_input_mode_mask();
-    char *mode_mask_copy = malloc(strlen(mode_mask) + 1);
-    strcpy(mode_mask_copy, mode_mask);
+    if (mode_mask == NULL) return false;
+    char *mode_mask_copy = strdup(mode_mask);
+    char *strsep_index = mode_mask_copy;
     
     char *mode_flag = NULL;
     bool contains = false;
     
-    while ((mode_flag = strsep(&mode_mask_copy, "|")) != NULL) {
+    while ((mode_flag = strsep(&strsep_index, "|")) != NULL) {
         if (strcmp(mode_flag, target) == 0) {
             contains = true;
             break;
@@ -30,7 +31,7 @@ bool mode_contains(char *target) {
 }
 
 bool tm_interactive_input_is_active() {
-    return (get_tm_interactive_input_mode_mask() == NULL || mode_contains("NEVER") == false);
+    return (get_tm_interactive_input_mode_mask() != NULL && mode_contains("NEVER") == false);
 }
 
 bool tm_interactive_input_is_in_always_mode() {

4d01c93b177455026389aa399066c961ad75ba27
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-04-30 07:24:36 +0000

    Rearrangement of the entire start options files
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9469 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/R Console (Rdaemon).tmbundle/Support/Rdaemon/daemon/start.r
+++ Review/Bundles/R Console (Rdaemon).tmbundle/Support/Rdaemon/daemon/start.r
@@ -5,8 +5,9 @@
 #source("~/Rdaemon/help/getSig.R")
 
 # load start options
-sys.source("~/Rdaemon/myBase.R", envir = environment(readline))
-sys.source("~/Rdaemon/myUtils.R", envir = environment(menu))
+sys.source("~/Rdaemon/daemon/myBase.R", envir = environment(readline))
+sys.source("~/Rdaemon/daemon/myUtils.R", envir = environment(menu))
+sys.source("~/Rdaemon/daemon/myStartOptions.R", envir = attach(NULL, name = "Rdaemon"))
 sys.source("~/Rdaemon/startOptions.R", envir = attach(NULL, name = "Rdaemon"))
 
 #################################################################################################

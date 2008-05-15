291db57543f59f86bcf1f5dd5a6481d6142a8723
mattfoster <mattfoster@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-05-15 08:53:38 +0000

    • Borrowed the function scope from the ruby bundle, and adjusted it. • Added preferences to control the symbol list
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9576 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/Matlab.tmbundle/Tests/test_functions.m
@@ -0,0 +1,10 @@
+function [a, b, c] = test_both(a, b, c) % Both input and output arguments
+function a = test_both(a, b, c)
+function test_input(a, b, c)
+function test_neither
+function aa = test
+
+% TODO: Highly crazy, and yet valid syntax.
+% at least it's detected as a function!
+function bb = test3 ...
+	(a, b, c)

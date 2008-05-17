414215880c78f5e35e8bc028476d8ab37ab0b717
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-05-17 02:27:35 +0000

    • Write debug output to /tmp/tm_interactive_input.log instead of stderr
    This allows us to debug uses where we need to capture the stderr (for the prompt) of the app we are injecting into.
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9591 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/tm_interactive_input/src/debug.h
+++ Tools/tm_interactive_input/src/debug.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 #ifndef NDEBUG
-#define D(format, args...) fprintf(stderr, "%s(): ", __FUNCTION__); fprintf(stderr, format ,## args)
+#define D(format, args...) ; if (true) { FILE *f = fopen("/tmp/tm_interactive_read.log","a"); fprintf(f, "%s(): ", __FUNCTION__); fprintf (f, format,## args); fclose(f); }
 #else
 #define D(format, args...) 
 #endif
--- Tools/tm_interactive_input/src/system_function_overrides.c
+++ Tools/tm_interactive_input/src/system_function_overrides.c
@@ -73,17 +73,9 @@ ssize_t read_nocancel_unix2003(int d, void *buffer, size_t buffer_length) {
 }
 
 ssize_t write(int d, const void *buffer, size_t buffer_length) {
-
-    // If we are printing debug messages, we can't capture stderr otherwise
-    // we get into a loop.
-
-    #ifdef NDEBUG
-        if (tm_interactive_input_is_active() && (d == STDOUT_FILENO || d == STDERR_FILENO)) 
-            capture_for_prompt(buffer, buffer_length);
-    #else
-        if (tm_interactive_input_is_active() && (d == STDOUT_FILENO)) 
-            capture_for_prompt(buffer, buffer_length);
-    #endif
+    if (tm_interactive_input_is_active() && (d == STDOUT_FILENO || d == STDERR_FILENO)) {
+        capture_for_prompt(buffer, buffer_length);
+    }
 
     return syscall(SYS_write, d, buffer, buffer_length);
 }

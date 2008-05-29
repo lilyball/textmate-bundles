580b52e59fd5b00c70006c447f9752f8d61eec66
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-05-29 09:45:14 +0000

    • it is now possible to give ‘:no_markdown => true’ or ‘:no_smartypants => true’ as additional parameters to new.
    So rather than a standalone smartypants API we’ll just use this with markdown disabled.
    
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9777 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/tm/markdown.rb
+++ Support/lib/tm/markdown.rb
@@ -1,18 +1,26 @@
 module TextMate
-  class Markdown
-    def initialize(str)
-      @str = str
-    end
+	class Markdown
+		def initialize(str, options = { })
+			@str = str
+			@filters = [ ]
+			@filters << '"${TM_MARKDOWN:-$TM_SUPPORT_PATH/bin/Markdown.pl}"' unless options[:no_markdown]
+			@filters << '"${TM_SMARTYPANTS:-$TM_SUPPORT_PATH/bin/SmartyPants.pl}"' unless options[:no_smartypants]
+		end
 
-    def to_html
-      IO.popen('"${TM_MARKDOWN:-$TM_SUPPORT_PATH/bin/Markdown.pl}"|"${TM_SMARTYPANTS:-$TM_SUPPORT_PATH/bin/SmartyPants.pl}"', 'r+') do |io|
-        Thread.new { io << @str; io.close_write }
-        io.read
-      end
-    end
-  end
+		def to_html
+			return @str if @filters.empty?
+
+			IO.popen(@filters.join('|'), 'r+') do |io|
+				Thread.new { io << @str; io.close_write }
+				io.read
+			end
+		end
+	end
 end
 
 if $0 == __FILE__
-  puts TextMate::Markdown.new("hello's there").to_html
+	puts TextMate::Markdown.new("who's there?").to_html
+	puts TextMate::Markdown.new("who's there?", :no_markdown => true).to_html
+	puts TextMate::Markdown.new("who's there?", :no_smartypants => true).to_html
+	puts TextMate::Markdown.new("who's there?", :no_markdown => true, :no_smartypants => true).to_html
 end

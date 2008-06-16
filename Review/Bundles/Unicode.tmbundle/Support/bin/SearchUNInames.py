8aa0d565debdbabe9db82cb4f27189e42f066e11
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-06-16 15:53:29 +0000

    -- optimized and cleaned
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9919 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/Unicode.tmbundle/Support/bin/SearchUNInames.py
+++ Review/Bundles/Unicode.tmbundle/Support/bin/SearchUNInames.py
@@ -15,30 +15,30 @@ sourceFile = "UnicodeNames.txt.zip"
 
 
 def wunichr(dec):
-   return ("\\U%08X" % dec).decode("unicode-escape")
+    return ("\\U%08X" % dec).decode("unicode-escape")
 
 
 def wuniord(s):
-   if s:
-       if u"\udc00" <= s[-1] <= u"\udfff" and len(s) >= 2 and u"\ud800" <= s[-2] <= u"\udbff":
-           return (((ord(s[-2])&0x3ff)<<10 | (ord(s[-1])&0x3ff)) + 0x10000)
-       return (ord(s[-1]))
-   return (-1)
+    if s:
+        if u"\udc00" <= s[-1] <= u"\udfff" and len(s) >= 2 and u"\ud800" <= s[-2] <= u"\udbff":
+            return (((ord(s[-2])&0x3ff)<<10 | (ord(s[-1])&0x3ff)) + 0x10000)
+        return (ord(s[-1]))
+    return (-1)
 
 
 if len(sys.argv) != 3:
-   print "Wrong number of arguments."
+    print "Wrong number of arguments."
 
 searchkind = sys.argv[1]
 if not (searchkind == 'word' or searchkind == 'full'):
-   print "Wrong first argument. Only 'word' or 'full'."
+    print "Wrong first argument. Only 'word' or 'full'."
 
 os.popen("touch /tmp/TM_db.busy")
 
 if searchkind == "full":
-   grepopt = ""
+    grepopt = ""
 else:
-   grepopt = "\\b"
+    grepopt = "\\b"
 
 pattern = sys.argv[2].upper()
 
@@ -46,35 +46,32 @@ print "<p>&nbsp;<br><br></p>"
 
 grepcmds = []
 for pat in pattern.split(' '):
-   if pat:
-       if not grepcmds:
-           grepcmds.append("zgrep -E '%s%s%s' '%s%s'" % (grepopt, pat, grepopt, bundleLibPath, sourceFile))
-       else:
-           grepcmds.append("egrep '%s%s'" % (grepopt, pat))
+    if pat:
+        if not grepcmds:
+            grepcmds.append("zgrep -E '%s%s%s' '%s%s'" % (grepopt, pat, grepopt, bundleLibPath, sourceFile))
+        else:
+            grepcmds.append("egrep '%s%s'" % (grepopt, pat))
+
 grepcmd = " | ".join(grepcmds) + " | uniq | head -n 499"
 
 suggestions = os.popen(grepcmd).read().decode("utf-8").strip()
 
 if not suggestions:
-   print "<i><small>Nothing found</small></i>"
-   os.popen("rm -f /tmp/TM_db.busy")
+    print "<i><small>Nothing found</small></i>"
+    os.popen("rm -f /tmp/TM_db.busy")
 
 print "<p class='res'>"
 cnt = 0
 for i in suggestions.split('\n'):
-   cnt += 1
-   c, n = i.split(';')
-   # cdec = codepoints(c)
-   # print "<p>%s</p>" % str(c)
-   t = ""
-   if "COMBINING" in n: t = u"<small>◌</small>"
-   print "<span onclick='insertChar(\"%s\")' onmouseout='clearName()'; onmouseover='showName(\"U+%s : %s\")' class='char'>%s%s</span> " % (hexlify(c.encode("UTF-8")),"%04X" % wuniord(c), n, t, c)
-
-os.popen("rm -f /tmp/TM_db.busy")
+    cnt += 1
+    c, n = i.split(';')
+    t = ""
+    if "COMBINING" in n: t = u"<small>◌</small>"
+    print "<span onclick='insertChar(\"%s\")' onmouseout='clearName()'; onmouseover='showName(\"U+%s : %s\")' class='char'>%s%s</span> " % (hexlify(c.encode("UTF-8")),"%04X" % wuniord(c), n, t, c)
 
 pl = ""
 if cnt > 1: pl = "es"
-if cnt>499:
+if cnt>498:
     print "</p><i><small>More than 500 matches found. Please narrow down.</small></i>"
 else:
     print "</p><i><small>"+str(cnt)+" match"+pl+"</small></i>"

58f932a2b46f822eec27d0f65e3611974155dfe5
aross <aross@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-06-17 01:04:25 +0000

    • Add `default_extension' to NuMate, so that it works with temp files.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9926 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Nu.tmbundle/Support/NuMate/run_script.rb
+++ Bundles/Nu.tmbundle/Support/NuMate/run_script.rb
@@ -3,6 +3,7 @@ require "#{ENV["TM_SUPPORT_PATH"]}/lib/scriptmate"
 class NuScript < UserScript
   def lang; "Nu" end
   def executable; @hashbang || ENV['TM_NUSH'] || 'nush' end
+  def default_extension; "nu" end
   def version_string
     version = `nush -e '(puts (version))'`
     "nush (#{version.strip!})"

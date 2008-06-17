b75eb622434374525c3a35221bde050801db3f7f
aross <aross@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-06-17 07:49:27 +0000

    Add a new method TextMate.save to the TextMate ruby module. Calling TextMate.save ensures that one of the following hold true:   • The current document is saved, if TM_FILEPATH is writeable.     In this case, TM_FILEPATH, TM_FILENAME, TM_DISPLAYNAME are left as is.   • If TM_FILEPATH is not writeable, then the contents of the current     document will be saved in a temporary file.     In this case:       TM_FILEPATH, TM_FILENAME reflect the temporary file.       TM_ORG_FILEPATH and TM_ORG_FILENAME will reflect the original unwriteable file.       TM_DISPLAYNAME will be annotated by (M) to show that the file has not been saved.   • If TM_FILEPATH is unset, the current document has never been saved.     The current document's content will be saved in a temporary     In this case:       TM_FILEPATH, TM_FILENAME reflect the temporary file.       TM_FILE_IS_UNTITLED will be set to “true”       TM_DISPLAYNAME will be set to “untitled”
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9932 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Support/lib/tm/save.rb
@@ -0,0 +1,46 @@
+module TextMate
+  class << self
+
+    # Calling TextMate.save ensures that one of the following hold true:
+    #  • The current document is saved, if TM_FILEPATH is writeable.
+    #    In this case, TM_FILEPATH, TM_FILENAME, TM_DISPLAYNAME are left as is.
+    #  • If TM_FILEPATH is not writeable, then the contents of the current
+    #    document will be saved in a temporary file.
+    #    In this case:
+    #      TM_FILEPATH, TM_FILENAME reflect the temporary file.
+    #      TM_ORG_FILEPATH and TM_ORG_FILENAME will reflect the original unwriteable file.
+    #      TM_DISPLAYNAME will be annotated by (M) to show that the file has not been saved.
+    #  • If TM_FILEPATH is unset, the current document has never been saved.  
+    #    The current document's content will be saved in a temporary
+    #    In this case: 
+    #      TM_FILEPATH, TM_FILENAME reflect the temporary file.
+    #      TM_FILE_IS_UNTITLED will be set to “true”
+    #      TM_DISPLAYNAME will be set to “untitled”
+
+    def save()
+      doc, dst = STDIN.read, ENV['TM_FILEPATH']
+      ENV['TM_DISPLAYNAME'] = ENV['TM_FILENAME']
+
+      return unless dst.nil? || File.read(dst) != doc
+
+      if dst.nil?
+        ENV['TM_FILEPATH']         = dst = TextMate::IO.tempfile("tmp").path
+        ENV['TM_FILENAME']         = File.basename dst
+        ENV['TM_FILE_IS_UNTITLED'] = "true"
+        ENV['TM_DISPLAYNAME']      = 'untitled'
+      elsif not File.writable? dst
+        ENV['TM_ORG_FILEPATH']     = dst
+        ENV['TM_ORG_FILENAME']     = File.basename dst
+        ENV['TM_FILEPATH']         = dst = TextMate::IO.tempfile("tmp").path
+        ENV['TM_FILENAME']         = File.basename dst
+        ENV['TM_DISPLAYNAME']     += ' (M)'
+      end
+      
+      begin
+        open(dst, 'w') { |io| io << doc }
+      rescue Errno::EACCES
+        # we already checked writable? so this is a real issue
+      end
+    end
+  end
+end

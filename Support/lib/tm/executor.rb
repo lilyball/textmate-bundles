7436c7f9e06398d46b2d6b2fc0fae5ba0493d585
aross <aross@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-06-19 06:10:47 +0000

    • Adding htmloutput.rb, executor.rb.
      htmloutput.rb adds an API function to the TextMate namespace:
        TextMate::HTMLOutput.show(:title => nil, :sub_title => nil) do |io| «something» end
      This function outputs an html header and an footer, for use with commands that output to html windows.
      
      executor.rb adds an API function to the TextMate namespace as well:
        TextMate::Executor.run(executable, *special_args, filepath)
      can be called with or without a block.  Basically provides the functionality of the old scriptmate
      in a more convenient package.  See the file for more documentation.
    
    • In save_current_document.rb, changed TM_ORG_* to TM_ORIG_*.
    
    
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9954 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Support/lib/tm/executor.rb
@@ -0,0 +1,185 @@
+# tm/executor.rb
+#
+# Provides some tools to create “Run Script” commands
+# that fancily HTML format stdout/stderr.
+# 
+# Nice features include:
+#  • Automatic interactive input.
+#  • Automatic Fancy HTML headers
+#  • The environment variable TM_ERROR_FD will contain a file descriptor to which HTML-formatted
+#    exceptions can be written.
+# 
+# Executor runs best if TextMate.save_current_document is called first.  Doing so ensures
+# that TM_FILEPATH contains the path to the contents of your current document, even if the
+# current document has not yet been saved, or the file is unwriteable.
+# 
+# Call it like this (you'll get rudimentary htmlification of the executable's output):
+#
+#   TextMate::Executor.run(ENV['TM_SHELL'] || ENV['SHELL'] || 'bash', ENV['TM_FILEPATH'])
+# 
+# Or like this if you want to transform the output yourself:
+#
+#   TextMate::Executor.run(ENV['TM_SHELL'] || ENV['SHELL'] || 'bash', ENV['TM_FILEPATH']) do |str, type|
+#     case type
+#     when :err
+#       $stdout << str
+#     when :out
+#       $stdout << str
+#     end
+#   end
+
+
+SUPPORT_LIB = ENV['TM_SUPPORT_PATH'] + '/lib/'
+require SUPPORT_LIB + 'tm/process'
+require SUPPORT_LIB + 'tm/tempfile'
+require SUPPORT_LIB + 'tm/htmloutput'
+require SUPPORT_LIB + 'escape'
+require SUPPORT_LIB + 'io'
+
+$KCODE = 'u'
+require 'jcode'
+
+$stdout.sync = true
+
+module TextMate
+  module Executor
+    class << self
+      
+      # Textmate::Executor.run
+      #   Provides an API function for running
+      
+      def run(*args, &block)
+        
+        args.flatten!
+        
+        options = {:version_args  => ['--version'],
+                   :version_regex => /\A(.*)$/,
+                   :env           => ENV}
+        
+        options.merge! args.pop if args.last.is_a? Hash
+        
+        args[0] = $1.chomp.split if /\A#!(.*)$/ =~ File.read(args[-1]) || args[0]
+
+        tm_error_fd_read, tm_error_fd_write = ::IO.pipe
+        tm_error_fd_read.fcntl(Fcntl::F_SETFD, 1)
+        ENV['TM_ERROR_FD'] = tm_error_fd_write.to_i.to_s
+        
+        # version = $1 if options[:version_regex] =~ Process.run(args[0], options[:version_args], :interactive_input => true)[0]
+        version = $1 if options[:version_regex] =~ `#{e_sh args[0]} #{options[:version_args].flatten.join(" ")}`
+        
+        TextMate::HTMLOutput.show(:title => "Running “#{ENV['TM_DISPLAYNAME']}”…", :sub_title => version) do |io|
+            
+          block ||= proc do |str, type|
+            str = htmlize(str).gsub(/\<br\>/, "<br>\n")
+            str = "<span style='color: red'>#{str}</span>" if type == :err
+            io << str
+          end
+
+          process_output_wrapper(io) do
+            TextMate::Process.run(args, :env => options[:env], &block)
+          end
+          
+          tm_error_fd_write.close
+          error = tm_error_fd_read.read
+          tm_error_fd_read.close
+      
+          if ENV.has_key? "TM_FILE_IS_UNTITLED"
+            # replace links to temporary file with links to current (unsaved) file, by removing
+            # the url option from any txmt:// links.
+            error.gsub!("url=file://#{ENV['TM_FILEPATH']}", '')
+            error.gsub!("url=file://#{e_url ENV['TM_FILEPATH']}", '')
+            error.gsub!(ENV['TM_FILENAME'], "untitled")
+            error.gsub!(e_url(ENV['TM_FILENAME']), "untitled")
+          elsif ENV.has_key? 'TM_ORIG_FILEPATH'
+            error.gsub!(ENV['TM_FILEPATH'], ENV['TM_ORIG_FILEPATH'])
+            error.gsub!(e_url(ENV['TM_FILEPATH']), e_url(ENV['TM_ORIG_FILEPATH']))
+            error.gsub!(ENV['TM_FILENAME'], ENV['TM_ORIG_FILENAME'])
+            error.gsub!(e_url(ENV['TM_FILENAME']), e_url(ENV['TM_ORIG_FILENAME']))
+          end
+          
+          io << error
+          io << '<div id="exception_report" class="framed">Program exited.</div>'
+        end
+      end
+      
+      private
+            
+      def process_output_wrapper(io)
+        io << <<-HTML
+<!-- executor javascripts -->
+<script type="text/javascript" charset="utf-8">
+function press(evt) {
+   if (evt.keyCode == 67 && evt.ctrlKey == true) {
+      TextMate.system("kill -s INT #{@pid}; sleep 0.5; kill -s TERM #{@pid}", null);
+   }
+}
+document.body.addEventListener('keydown', press, false);
+
+function copyOutput(link) {
+  output = document.getElementById('_scriptmate_output').innerText;
+  cmd = TextMate.system('__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 /usr/bin/pbcopy', function(){});
+  cmd.write(output);
+  cmd.close();
+  link.innerText = 'output copied to clipboard';
+}
+</script>
+<!-- end javascript -->
+<style type="text/css">
+
+  div.executor pre em
+  {
+    font-style: normal;
+    color: #FF5600;
+  }
+  
+  div.executor p#exception strong
+  {
+    color: #E4450B;
+  }
+
+  div.executor p#traceback
+  {
+    font-size: 8pt;
+  }
+
+  div.executor blockquote {
+    font-style: normal;
+    border: none;
+  }
+
+  div.executor table {
+    margin: 0;
+    padding: 0;
+  }
+
+  div.executor td {
+    margin: 0;
+    padding: 2px 2px 2px 5px;
+    font-size: 10pt;
+  }
+
+  div.executor a {
+    color: #FF5600;
+  }
+
+  div#exception_report pre.snippet {
+    margin:4pt;
+    padding:4pt;
+  }
+</style>
+<div class="executor">
+<div class="controls" style="text-align:right;">
+  <a style="text-decoration: none;" href="#" onclick="copyOutput(document.getElementById('_executor_output'))">copy output</a>
+</div>
+<!-- first box containing version info and script output -->
+<pre>
+<div id="_executor_output" style="white-space: normal; -khtml-nbsp-mode: space; -khtml-line-break: after-white-space;"> <!-- Script output -->
+HTML
+        yield
+        io << '</div></pre></div>'
+      end
+    end
+  end
+end
+
+
--- Support/lib/tm/save_current_document.rb
+++ Support/lib/tm/save_current_document.rb
@@ -2,35 +2,37 @@ module TextMate
   class << self
     # 
     # Calling TextMate.save ensures that one of the following hold true:
-    #  • The current document is saved, if TM_FILEPATH is writeable.
+    #  • If TM_FILEPATH is writeable, then the current document is saved.
     #    In this case, TM_FILEPATH, TM_FILENAME, TM_DISPLAYNAME are left as is.
     #  • If TM_FILEPATH is not writeable, then the contents of the current
     #    document will be saved in a temporary file.
     #    In this case:
     #      TM_FILEPATH, TM_FILENAME reflect the temporary file.
-    #      TM_ORG_FILEPATH and TM_ORG_FILENAME will reflect the original unwriteable file.
+    #      TM_ORIG_FILEPATH and TM_ORIG_FILENAME will reflect the original unwriteable file.
     #      TM_DISPLAYNAME will be annotated by (M) to show that the file has not been saved.
     #  • If TM_FILEPATH is unset, the current document has never been saved.  
-    #    The current document's content will be saved in a temporary
+    #    The current document's content will be saved in a temporary file.
     #    In this case: 
     #      TM_FILEPATH, TM_FILENAME reflect the temporary file.
     #      TM_FILE_IS_UNTITLED will be set to “true”
     #      TM_DISPLAYNAME will be set to “untitled”
+    # Note that this method calls STDIN.read.  If you want to access the contents of the
+    # current document after you've called this method, do File.read(ENV['TM_FILEPATH']).
 
     def save_current_document()
+      
       doc, dst = STDIN.read, ENV['TM_FILEPATH']
-      ENV['TM_DISPLAYNAME'] = ENV['TM_FILENAME']
-
+      
       return unless dst.nil? || File.read(dst) != doc
-
+      
       if dst.nil?
         ENV['TM_FILEPATH']         = dst = TextMate::IO.tempfile("tmp").path
         ENV['TM_FILENAME']         = File.basename dst
         ENV['TM_FILE_IS_UNTITLED'] = "true"
         ENV['TM_DISPLAYNAME']      = 'untitled'
       elsif not File.writable? dst
-        ENV['TM_ORG_FILEPATH']     = dst
-        ENV['TM_ORG_FILENAME']     = File.basename dst
+        ENV['TM_ORIG_FILEPATH']    = dst
+        ENV['TM_ORIG_FILENAME']    = File.basename dst
         ENV['TM_FILEPATH']         = dst = TextMate::IO.tempfile("tmp").path
         ENV['TM_FILENAME']         = File.basename dst
         ENV['TM_DISPLAYNAME']     += ' (M)'

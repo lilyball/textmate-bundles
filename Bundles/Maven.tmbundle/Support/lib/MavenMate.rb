866cbc92b52d4da8995a1f85932c14ef26f11507
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-06-19 09:26:25 +0000

    • Divorced MavenMate from scriptmate and use TextMate::Process.run() directly.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@9960 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Maven.tmbundle/Support/lib/MavenMate.rb
+++ Bundles/Maven.tmbundle/Support/lib/MavenMate.rb
@@ -1,158 +1,28 @@
 #!/usr/bin/env ruby -w
 
-require ENV["TM_SUPPORT_PATH"] + "/lib/scriptmate"
 require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
 require ENV['TM_SUPPORT_PATH'] + '/lib/tm/process'
+require ENV['TM_SUPPORT_PATH'] + '/lib/tm/htmloutput'
 
 require "pstore"
 require "yaml"
+require "shellwords"
 
 $SCRIPTMATE_VERSION = "$Revision: 8136 $"
 $VERBOSE = nil
 
 class MavenCommand
   
-  attr_reader :location
-  
-  def initialize(mvn, location, task)
-    @mvn = mvn
-    @task = task
-    @location = location
-    @command = nil
-    @previous_args_pref_key = "#{@location}::#{@task}::previousargs"
-    @previous_command_pref_key = "#{@location}::previouscommand"
-  end
-  
-  def to_s
-    command
-  end
-  
-  def command
-    if @command.nil?
-      previous_args_for_task = MavenMate.get_pref(@previous_args_pref_key)
-      
-      args_for_task = TextMate::UI.request_string( 
-        :title => (@task.empty? ? "MavenMate" : "MavenMate - #{@task}"),
-        :prompt => 'Enter any command options',
-        :default => previous_args_for_task
-      )
-      exit if args_for_task.nil?
-      
-      MavenMate.set_pref(@previous_args_pref_key, args_for_task) if args_for_task != previous_args_for_task
-
-      @command = []
-      @command << @task unless @task.empty?
-      @command << "#{args_for_task}" unless args_for_task.empty?
-    
-      MavenMate.set_pref(@previous_command_pref_key, @command.to_yaml)
-      @command = MavenMate.add_profiles_to_command(@location, @command)
-    end
-    @command
-  end
-  
-  def full_command
-    command.unshift(@mvn)
-  end
-
-  def run(&block)
-    Dir.chdir(@location)
-    rd, wr = IO.pipe
-    rd.fcntl(Fcntl::F_SETFD, 1)
-    stdin, stdout, stderr, pid = my_popen3(full_command)
-    wr.close
-    block.call(stdout, stderr, rd, pid)
-  end
-
-  def html
-    mavenmate = MavenMate.new(self)
-    mavenmate.emit_html
-    mavenmate
-  end
-  
-  def shell
-    mavenmate = MavenMate.new(self)
-    mavenmate.shell
-    mavenmate    
-  end
-  
-end
-
-class PreviousMavenCommand < MavenCommand
-  def initialize(mvn, location)
-    super(mvn, location, '')
-  end
-  
-  def command
-    if @command.nil?
-      @command = YAML::load(MavenMate.get_pref(@previous_command_pref_key))
-      @command = MavenMate.add_profiles_to_command(@location, @command)
-    end
-    @command
-  end
-end
-
-class MavenFocusedTestCommand < MavenCommand
-  def initialize(mvn, location, test)
-    super(mvn, location, ['test', '-Dtest=' + test.sub(/\.\w+$/, '')])
-  end
-end
-
-class MavenMate < CommandMate
-  
   @@prefs = PStore.new(File.expand_path( "~/Library/Preferences/com.macromates.textmate.mavenmate"))
+  
   def self.get_pref(key) 
     @@prefs.transaction { @@prefs[key] }
   end
+  
   def self.set_pref(key, value) 
     @@prefs.transaction { @@prefs[key] = value }
   end
 
-  attr_reader :colorisations, :command
-    
-  def initialize(command) 
-    super(command)
-    setup_default_colorisations
-  end
-
-  def setup_default_colorisations
-    @colorisations = {
-      "green" => [/BUILD SUCCESSFUL/, /Tests run: ([^0]\d*), Failures: 0, Errors: 0/],
-      "red" => [/\[ERROR\]/, /Tests run: ([^0]\d*)(.)+(Failures: ([^0]\d*)|Errors: ([^0]\d*)+)/],
-      "orange" => [/\[WARNING\]/, /There are no tests to run\./, /Tests run: 0,/],
-      "blue" => [/\[INFO\] \[(.)+:(.)+\]/]
-    }
-  end
-  
-  def build_command(task, location)
-    
-    MavenCommand(@command, @location)
-  end
-  
-  def emit_header
-    puts html_head(:window_title => "MavenMate", :page_title => "MavenMate", :sub_title => "#{@command.location}")
-    puts "<pre>"
-    puts "<strong>mvn " + htmlize(@command.command.join(' ')) + "</strong><br>\n"
-  end
-  
-  def filter_stdout(line)
-    line.chomp!
-    
-    if line =~ /^Running (\S+)/ 
-      line = "Running <a href='txmt://open?url=file://#{command.location}/target/surefire-reports/#{$1}.txt'>#{$1}</a>"
-    else
-      line = htmlize(line)
-    end
-    
-    match = false
-    @colorisations.each do | color, patterns |
-      if match == false and patterns.detect { |pattern| line =~ pattern }
-        match = "<span style=\"color: #{color}\">#{line}</span><br>"
-      end
-    end
-    match = (line) + "<br>" unless match 
-    return match
-  end
-  
   def self.profiles_pref_key(location)
     "#{location}::profiles"
   end
@@ -171,24 +41,108 @@ class MavenMate < CommandMate
     end
     set_pref(profiles_pref_key(location), newprofiles)
   end
-  
+
   def self.profiles(location)
     get_pref(profiles_pref_key(location))
   end
-  
+
   def self.add_profiles_to_command(location, command)
     p = profiles(location)
     return command if p.nil? or p.empty?
-      
+
     command << "-P"
     command << p
   end
+
+  attr_reader :location, :colorisations, :tasks
   
-  def shell
-    system "osascript -e 'tell application \"Terminal\"\n do script \"cd #{@command.location}; clear; echo #{@command.full_command}; echo;  #{@command.full_command}\"\n activate\n end tell'"
+  def initialize(mvn, location, *tasks)
+    @mvn = mvn
+    @location = location
+    @tasks = tasks
+    @previous_args_pref_key = "#{@location}::#{@tasks}::previousargs"
+    @previous_command_pref_key = "#{@location}::previouscommand"
+    @colorisations = {
+      "green" => [/BUILD SUCCESSFUL/, /Tests run: ([^0]\d*), Failures: 0, Errors: 0/],
+      "red" => [/\[ERROR\]/, /Tests run: ([^0]\d*)(.)+(Failures: ([^0]\d*)|Errors: ([^0]\d*)+)/],
+      "orange" => [/\[WARNING\]/, /There are no tests to run\./, /Tests run: 0,/],
+      "blue" => [/\[INFO\] \[(.)+:(.)+\]/]
+    }
+  end
+  
+  def command
+    previous_args = self.class.get_pref(@previous_args_pref_key)
+    
+    args = TextMate::UI.request_string( 
+      :title => (@tasks.empty? ? "MavenMate" : "MavenMate - #{@tasks.join(' ')}"),
+      :prompt => 'Enter any command options',
+      :default => previous_args
+    )
+    exit if args.nil?
+    
+    self.class.set_pref(@previous_args_pref_key, args) if args != previous_args
+
+    command = []
+    command = command + @tasks unless @tasks.empty?
+    command = command + Shellwords.shellwords(args)
+  
+    self.class.set_pref(@previous_command_pref_key, command.to_yaml)
+    self.class.add_profiles_to_command(@location, command)
+  end
+   
+  def run
+    Dir.chdir(@location) do 
+      TextMate::HTMLOutput.show(:window_title => "MavenMate", :page_title => "MavenMate", :sub_title => "#{location}") do |io|
+        cmd = command
+        io << "<pre>"
+        io << "<strong>mvn " + htmlize(cmd.join(' ')) + "</strong><br/>\n"
+        TextMate::Process.run(@mvn, *cmd) do |line, type|
+          case type
+          when :out
+            line.chomp!
+
+            if line =~ /^Running (\S+)/ 
+              line = "Running <a href='txmt://open?url=file://#{@location}/target/surefire-reports/#{$1}.txt'>#{$1}</a>"
+            else
+              line = htmlize(line)
+            end
+
+            match = false
+            @colorisations.each do | color, patterns |
+              if match == false and patterns.detect { |pattern| line =~ pattern }
+                match = "<span style=\"color: #{color}\">#{line}</span><br>"
+              end
+            end
+            match = "<span>#{line}</span><br>" unless match 
+            io << match
+          when :err
+            io << "<span style='color: red'>#{htmlize(line).gsub(/\<br\>/, "<br>\n")}</span>"
+          end
+        end
+        io << "</pre>"
+      end
+    end
   end
   
 end
+
+class PreviousMavenCommand < MavenCommand
+  def initialize(mvn, location)
+    super(mvn, location)
+  end
+  
+  def command
+    command = YAML::load(self.class.get_pref(@previous_command_pref_key))
+    self.class.add_profiles_to_command(@location, command)
+  end
+end
+
+class MavenFocusedTestCommand < MavenCommand
+  def initialize(mvn, location, test)
+    super(mvn, location, ['test', '-Dtest=' + test.sub(/\.\w+$/, '')])
+  end
+end
+
 
 # =========================
 # = Parse Args and invoke =
@@ -210,12 +164,12 @@ opts.on("-m", "--mvn MVN", "MVN Binary") do |m|
 end
 opts.on("-P", "--profiles", "Set the profiles") do
   raise "Location (-l) not set" if location.nil?
-  MavenMate.set_profiles(location)
+  MavenCommand.set_profiles(location)
   exit
 end
 opts.on("-t", "--task [TASK]", "Execute task") do |t|
   if t.nil?
-    command = MavenCommand.new(mvn, location, '')
+    command = MavenCommand.new(mvn, location)
   else
     command = MavenCommand.new(mvn, location, t)
   end
@@ -233,4 +187,4 @@ raise "Missing -p or -t arguments" if command.nil?
 raise "Missing -l" if location.nil?
 raise "Missing -m" if mvn.nil?
 
-command.html
+command.run

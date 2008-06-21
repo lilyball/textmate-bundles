af2a1dee3ce598ba0361a6c63b9a51058da019f7
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-06-21 12:25:29 +0000

    outsourced ShowCharacterInventory.py
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10011 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Review/Bundles/Unicode.tmbundle/Support/bin/ShowCharacterInventory.py
@@ -0,0 +1,560 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import sys
+import os
+import codecs
+import unicodedata
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+sys.stdin  = codecs.getreader('utf-8')(sys.stdin)
+
+bundleLibPath = os.environ["TM_BUNDLE_SUPPORT"] + "/lib/"
+
+def wunichr(dec):
+    return ("\\U%08X" % dec).decode("unicode-escape")
+
+def codepoints(s):
+    hs = 0
+    for c in s:
+        c = ord(c)
+        if 0xdc00 <= c <= 0xdfff and hs:
+            yield ((hs&0x3ff)<<10 | (c&0x3ff)) + 0x10000
+            hs = 0
+        elif 0xd800 <= c <= 0xdbff:
+            hs = c
+        else:
+            yield c
+    if hs:
+        yield hs
+
+
+def getBlockName(s):
+    if 0x0000 <= s <= 0x007F:
+        return "Basic Latin"
+    elif 0x0080 <= s <= 0x00FF:
+        return "Latin-1 Supplement"
+    elif 0x0100 <= s <= 0x017F:
+        return "Latin Extended-A"
+    elif 0x0180 <= s <= 0x024F:
+        return "Latin Extended-B"
+    elif 0x0250 <= s <= 0x02AF:
+        return "IPA Extensions"
+    elif 0x02B0 <= s <= 0x02FF:
+        return "Spacing Modifier Letters"
+    elif 0x0300 <= s <= 0x036F:
+        return "Combining Diacritical Marks"
+    elif 0x0370 <= s <= 0x03FF:
+        return "Greek and Coptic"
+    elif 0x0400 <= s <= 0x04FF:
+        return "Cyrillic"
+    elif 0x0500 <= s <= 0x052F:
+        return "Cyrillic Supplement"
+    elif 0x0530 <= s <= 0x058F:
+        return "Armenian"
+    elif 0x0590 <= s <= 0x05FF:
+        return "Hebrew"
+    elif 0x0600 <= s <= 0x06FF:
+        return "Arabic"
+    elif 0x0700 <= s <= 0x074F:
+        return "Syriac"
+    elif 0x0750 <= s <= 0x077F:
+        return "Arabic Supplement"
+    elif 0x0780 <= s <= 0x07BF:
+        return "Thaana"
+    elif 0x07C0 <= s <= 0x07FF:
+        return "NKo"
+    elif 0x0900 <= s <= 0x097F:
+        return "Devanagari"
+    elif 0x0980 <= s <= 0x09FF:
+        return "Bengali"
+    elif 0x0A00 <= s <= 0x0A7F:
+        return "Gurmukhi"
+    elif 0x0A80 <= s <= 0x0AFF:
+        return "Gujarati"
+    elif 0x0B00 <= s <= 0x0B7F:
+        return "Oriya"
+    elif 0x0B80 <= s <= 0x0BFF:
+        return "Tamil"
+    elif 0x0C00 <= s <= 0x0C7F:
+        return "Telugu"
+    elif 0x0C80 <= s <= 0x0CFF:
+        return "Kannada"
+    elif 0x0D00 <= s <= 0x0D7F:
+        return "Malayalam"
+    elif 0x0D80 <= s <= 0x0DFF:
+        return "Sinhala"
+    elif 0x0E00 <= s <= 0x0E7F:
+        return "Thai"
+    elif 0x0E80 <= s <= 0x0EFF:
+        return "Lao"
+    elif 0x0F00 <= s <= 0x0FFF:
+        return "Tibetan"
+    elif 0x1000 <= s <= 0x109F:
+        return "Myanmar"
+    elif 0x10A0 <= s <= 0x10FF:
+        return "Georgian"
+    elif 0x1100 <= s <= 0x11FF:
+        return "Hangul Jamo"
+    elif 0x1200 <= s <= 0x137F:
+        return "Ethiopic"
+    elif 0x1380 <= s <= 0x139F:
+        return "Ethiopic Supplement"
+    elif 0x13A0 <= s <= 0x13FF:
+        return "Cherokee"
+    elif 0x1400 <= s <= 0x167F:
+        return "Unified Canadian Aboriginal Syllabics"
+    elif 0x1680 <= s <= 0x169F:
+        return "Ogham"
+    elif 0x16A0 <= s <= 0x16FF:
+        return "Runic"
+    elif 0x1700 <= s <= 0x171F:
+        return "Tagalog"
+    elif 0x1720 <= s <= 0x173F:
+        return "Hanunoo"
+    elif 0x1740 <= s <= 0x175F:
+        return "Buhid"
+    elif 0x1760 <= s <= 0x177F:
+        return "Tagbanwa"
+    elif 0x1780 <= s <= 0x17FF:
+        return "Khmer"
+    elif 0x1800 <= s <= 0x18AF:
+        return "Mongolian"
+    elif 0x1900 <= s <= 0x194F:
+        return "Limbu"
+    elif 0x1950 <= s <= 0x197F:
+        return "Tai Le"
+    elif 0x1980 <= s <= 0x19DF:
+        return "New Tai Lue"
+    elif 0x19E0 <= s <= 0x19FF:
+        return "Khmer Symbols"
+    elif 0x1A00 <= s <= 0x1A1F:
+        return "Buginese"
+    elif 0x1B00 <= s <= 0x1B7F:
+        return "Balinese"
+    elif 0x1B80 <= s <= 0x1BBF:
+        return "Sundanese"
+    elif 0x1C00 <= s <= 0x1C4F:
+        return "Lepcha"
+    elif 0x1C50 <= s <= 0x1C7F:
+        return "Ol Chiki"
+    elif 0x1D00 <= s <= 0x1D7F:
+        return "Phonetic Extensions"
+    elif 0x1D80 <= s <= 0x1DBF:
+        return "Phonetic Extensions Supplement"
+    elif 0x1DC0 <= s <= 0x1DFF:
+        return "Combining Diacritical Marks Supplement"
+    elif 0x1E00 <= s <= 0x1EFF:
+        return "Latin Extended Additional"
+    elif 0x1F00 <= s <= 0x1FFF:
+        return "Greek Extended"
+    elif 0x2000 <= s <= 0x206F:
+        return "General Punctuation"
+    elif 0x2070 <= s <= 0x209F:
+        return "Superscripts and Subscripts"
+    elif 0x20A0 <= s <= 0x20CF:
+        return "Currency Symbols"
+    elif 0x20D0 <= s <= 0x20FF:
+        return "Combining Diacritical Marks for Symbols"
+    elif 0x2100 <= s <= 0x214F:
+        return "Letterlike Symbols"
+    elif 0x2150 <= s <= 0x218F:
+        return "Number Forms"
+    elif 0x2190 <= s <= 0x21FF:
+        return "Arrows"
+    elif 0x2200 <= s <= 0x22FF:
+        return "Mathematical Operators"
+    elif 0x2300 <= s <= 0x23FF:
+        return "Miscellaneous Technical"
+    elif 0x2400 <= s <= 0x243F:
+        return "Control Pictures"
+    elif 0x2440 <= s <= 0x245F:
+        return "Optical Character Recognition"
+    elif 0x2460 <= s <= 0x24FF:
+        return "Enclosed Alphanumerics"
+    elif 0x2500 <= s <= 0x257F:
+        return "Box Drawing"
+    elif 0x2580 <= s <= 0x259F:
+        return "Block Elements"
+    elif 0x25A0 <= s <= 0x25FF:
+        return "Geometric Shapes"
+    elif 0x2600 <= s <= 0x26FF:
+        return "Miscellaneous Symbols"
+    elif 0x2700 <= s <= 0x27BF:
+        return "Dingbats"
+    elif 0x27C0 <= s <= 0x27EF:
+        return "Miscellaneous Mathematical Symbols-A"
+    elif 0x27F0 <= s <= 0x27FF:
+        return "Supplemental Arrows-A"
+    elif 0x2800 <= s <= 0x28FF:
+        return "Braille Patterns"
+    elif 0x2900 <= s <= 0x297F:
+        return "Supplemental Arrows-B"
+    elif 0x2980 <= s <= 0x29FF:
+        return "Miscellaneous Mathematical Symbols-B"
+    elif 0x2A00 <= s <= 0x2AFF:
+        return "Supplemental Mathematical Operators"
+    elif 0x2B00 <= s <= 0x2BFF:
+        return "Miscellaneous Symbols and Arrows"
+    elif 0x2C00 <= s <= 0x2C5F:
+        return "Glagolitic"
+    elif 0x2C60 <= s <= 0x2C7F:
+        return "Latin Extended-C"
+    elif 0x2C80 <= s <= 0x2CFF:
+        return "Coptic"
+    elif 0x2D00 <= s <= 0x2D2F:
+        return "Georgian Supplement"
+    elif 0x2D30 <= s <= 0x2D7F:
+        return "Tifinagh"
+    elif 0x2D80 <= s <= 0x2DDF:
+        return "Ethiopic Extended"
+    elif 0x2DE0 <= s <= 0x2DFF:
+        return "Cyrillic Extended-A"
+    elif 0x2E00 <= s <= 0x2E7F:
+        return "Supplemental Punctuation"
+    elif 0x2E80 <= s <= 0x2EFF:
+        return "CJK Radicals Supplement"
+    elif 0x2F00 <= s <= 0x2FDF:
+        return "Kangxi Radicals"
+    elif 0x2FF0 <= s <= 0x2FFF:
+        return "Ideographic Description Characters"
+    elif 0x3000 <= s <= 0x303F:
+        return "CJK Symbols and Punctuation"
+    elif 0x3040 <= s <= 0x309F:
+        return "Hiragana"
+    elif 0x30A0 <= s <= 0x30FF:
+        return "Katakana"
+    elif 0x3100 <= s <= 0x312F:
+        return "Bopomofo"
+    elif 0x3130 <= s <= 0x318F:
+        return "Hangul Compatibility Jamo"
+    elif 0x3190 <= s <= 0x319F:
+        return "Kanbun"
+    elif 0x31A0 <= s <= 0x31BF:
+        return "Bopomofo Extended"
+    elif 0x31C0 <= s <= 0x31EF:
+        return "CJK Strokes"
+    elif 0x31F0 <= s <= 0x31FF:
+        return "Katakana Phonetic Extensions"
+    elif 0x3200 <= s <= 0x32FF:
+        return "Enclosed CJK Letters and Months"
+    elif 0x3300 <= s <= 0x33FF:
+        return "CJK Compatibility"
+    elif 0x3400 <= s <= 0x4DBF:
+        return "CJK Unified Ideographs Extension A"
+    elif 0x4DC0 <= s <= 0x4DFF:
+        return "Yijing Hexagram Symbols"
+    elif 0x4E00 <= s <= 0x9FFF:
+        return "CJK Unified Ideographs"
+    elif 0xA000 <= s <= 0xA48F:
+        return "Yi Syllables"
+    elif 0xA490 <= s <= 0xA4CF:
+        return "Yi Radicals"
+    elif 0xA500 <= s <= 0xA63F:
+        return "Vai"
+    elif 0xA640 <= s <= 0xA69F:
+        return "Cyrillic Extended-B"
+    elif 0xA700 <= s <= 0xA71F:
+        return "Modifier Tone Letters"
+    elif 0xA720 <= s <= 0xA7FF:
+        return "Latin Extended-D"
+    elif 0xA800 <= s <= 0xA82F:
+        return "Syloti Nagri"
+    elif 0xA840 <= s <= 0xA87F:
+        return "Phags-pa"
+    elif 0xA880 <= s <= 0xA8DF:
+        return "Saurashtra"
+    elif 0xA900 <= s <= 0xA92F:
+        return "Kayah Li"
+    elif 0xA930 <= s <= 0xA95F:
+        return "Rejang"
+    elif 0xAA00 <= s <= 0xAA5F:
+        return "Cham"
+    elif 0xAC00 <= s <= 0xD7AF:
+        return "Hangul Syllables"
+    elif 0xD800 <= s <= 0xDB7F:
+        return "High Surrogates"
+    elif 0xDB80 <= s <= 0xDBFF:
+        return "High Private Use Surrogates"
+    elif 0xDC00 <= s <= 0xDFFF:
+        return "Low Surrogates"
+    elif 0xE000 <= s <= 0xF8FF:
+        return "Private Use Area"
+    elif 0xF900 <= s <= 0xFAFF:
+        return "CJK Compatibility Ideographs"
+    elif 0xFB00 <= s <= 0xFB4F:
+        return "Alphabetic Presentation Forms"
+    elif 0xFB50 <= s <= 0xFDFF:
+        return "Arabic Presentation Forms-A"
+    elif 0xFE00 <= s <= 0xFE0F:
+        return "Variation Selectors"
+    elif 0xFE10 <= s <= 0xFE1F:
+        return "Vertical Forms"
+    elif 0xFE20 <= s <= 0xFE2F:
+        return "Combining Half Marks"
+    elif 0xFE30 <= s <= 0xFE4F:
+        return "CJK Compatibility Forms"
+    elif 0xFE50 <= s <= 0xFE6F:
+        return "Small Form Variants"
+    elif 0xFE70 <= s <= 0xFEFF:
+        return "Arabic Presentation Forms-B"
+    elif 0xFF00 <= s <= 0xFFEF:
+        return "Halfwidth and Fullwidth Forms"
+    elif 0xFFF0 <= s <= 0xFFFF:
+        return "Specials"
+    elif 0x10000 <= s <= 0x1007F:
+        return "Linear B Syllabary"
+    elif 0x10080 <= s <= 0x100FF:
+        return "Linear B Ideograms"
+    elif 0x10100 <= s <= 0x1013F:
+        return "Aegean Numbers"
+    elif 0x10140 <= s <= 0x1018F:
+        return "Ancient Greek Numbers"
+    elif 0x10190 <= s <= 0x101CF:
+        return "Ancient Symbols"
+    elif 0x101D0 <= s <= 0x101FF:
+        return "Phaistos Disc"
+    elif 0x10280 <= s <= 0x1029F:
+        return "Lycian"
+    elif 0x102A0 <= s <= 0x102DF:
+        return "Carian"
+    elif 0x10300 <= s <= 0x1032F:
+        return "Old Italic"
+    elif 0x10330 <= s <= 0x1034F:
+        return "Gothic"
+    elif 0x10380 <= s <= 0x1039F:
+        return "Ugaritic"
+    elif 0x103A0 <= s <= 0x103DF:
+        return "Old Persian"
+    elif 0x10400 <= s <= 0x1044F:
+        return "Deseret"
+    elif 0x10450 <= s <= 0x1047F:
+        return "Shavian"
+    elif 0x10480 <= s <= 0x104AF:
+        return "Osmanya"
+    elif 0x10800 <= s <= 0x1083F:
+        return "Cypriot Syllabary"
+    elif 0x10900 <= s <= 0x1091F:
+        return "Phoenician"
+    elif 0x10920 <= s <= 0x1093F:
+        return "Lydian"
+    elif 0x10A00 <= s <= 0x10A5F:
+        return "Kharoshthi"
+    elif 0x12000 <= s <= 0x123FF:
+        return "Cuneiform"
+    elif 0x12400 <= s <= 0x1247F:
+        return "Cuneiform Numbers and Punctuation"
+    elif 0x1D000 <= s <= 0x1D0FF:
+        return "Byzantine Musical Symbols"
+    elif 0x1D100 <= s <= 0x1D1FF:
+        return "Musical Symbols"
+    elif 0x1D200 <= s <= 0x1D24F:
+        return "Ancient Greek Musical Notation"
+    elif 0x1D300 <= s <= 0x1D35F:
+        return "Tai Xuan Jing Symbols"
+    elif 0x1D360 <= s <= 0x1D37F:
+        return "Counting Rod Numerals"
+    elif 0x1D400 <= s <= 0x1D7FF:
+        return "Mathematical Alphanumeric Symbols"
+    elif 0x1F000 <= s <= 0x1F02F:
+        return "Mahjong Tiles"
+    elif 0x1F030 <= s <= 0x1F09F:
+        return "Domino Tiles"
+    elif 0x20000 <= s <= 0x2A6DF:
+        return "CJK Unified Ideographs Extension B"
+    elif 0x2F800 <= s <= 0x2FA1F:
+        return "CJK Compatibility Ideographs Supplement"
+    elif 0xE0000 <= s <= 0xE007F:
+        return "Tags"
+    elif 0xE0100 <= s <= 0xE01EF:
+        return "Variation Selectors Supplement"
+    elif 0xF0000 <= s <= 0xFFFFF:
+        return "Supplementary Private Use Area-A"
+    elif 0x100000 <= s <= 0x10FFFF:
+        return "Supplementary Private Use Area-B"
+    else:
+        return "unknown"
+
+def rangeName(dec):
+    if 0x3400 <= dec <= 0x4DB5:
+        return  "CJK UNIFIED IDEOGRAPH"
+    elif 0x4E00 <= dec <= 0x9FC3:
+        return  "CJK UNIFIED IDEOGRAPH"
+    elif 0xAC00 <= dec <= 0xD7A3: # Hangul
+        return  unicodedata.name(unichr(dec), "U+%04X" % dec)
+    elif 0xD800 <= dec <= 0xDB7F:
+        return  "Non Private Use High Surrogate"
+    elif 0xDB80 <= dec <= 0xDBFF:
+        return  "Private Use High Surrogate"
+    elif 0xDC00 <= dec <= 0xDFFF:
+        return  "Low Surrogate"
+    elif 0xE000 <= dec <= 0xF8FF:
+        return  "Private Use"
+    elif 0x20000 <= dec <= 0x2A6D6:
+        return  "CJK UNIFIED IDEOGRAPH"
+    elif 0xF0000 <= dec <= 0xFFFFD:
+        return  "Plane 15 Private Use"
+    elif 0x100000 <= dec <= 0x10FFFD:
+        return  "Plane 16 Private Use"
+    else:
+        return  "not defined"
+
+
+text = list(codepoints(sys.stdin.read()))
+if not text: sys.exit(200)
+
+print "<html> \
+<head><title>Character Inventory</title> \
+<script type='text/javascript'>//<![CDATA[ \n\
+function sortTable2(col) { \n\
+  var tblEl = document.getElementById('theTable'); \n\
+  if (tblEl.reverseSort == null) \n\
+    tblEl.reverseSort = new Array(); \n\
+  if (col == tblEl.lastColumn) \n\
+    tblEl.reverseSort[col] = !tblEl.reverseSort[col]; \n\
+  tblEl.lastColumn = col; \n\
+  var oldDsply = tblEl.style.display; \n\
+  tblEl.style.display = 'none'; \n\
+  var tmpEl; \n\
+  var i, j; \n\
+  var minVal, minIdx; \n\
+  var testVal; \n\
+  var cmp; \n\
+  for (i = 0; i < tblEl.rows.length - 1; i++) { \n\
+    minIdx = i; \n\
+    minVal = getTextValue(tblEl.rows[i].cells[col]); \n\
+    for (j = i + 1; j < tblEl.rows.length; j++) { \n\
+      testVal = getTextValue(tblEl.rows[j].cells[col]); \n\
+      cmp = compareValues(minVal, testVal); \n\
+      if (tblEl.reverseSort[col]) \n\
+        cmp = -cmp; \n\
+      if (cmp > 0) { \n\
+        minIdx = j; \n\
+        minVal = testVal; \n\
+      } \n\
+    } \n\
+    if (minIdx > i) { \n\
+      tmpEl = tblEl.removeChild(tblEl.rows[minIdx]); \n\
+      tblEl.insertBefore(tmpEl, tblEl.rows[i]); \n\
+    } \n\
+  } \n\
+  tblEl.style.display = oldDsply; \n\
+  return false; \n\
+} \n\
+if (document.ELEMENT_NODE == null) { \n\
+  document.ELEMENT_NODE = 1; \n\
+  document.TEXT_NODE = 3; \n\
+} \n\
+function getTextValue(el) { \n\
+  var i; \n\
+  var s; \n\
+  s = ''; \n\
+  for (i = 0; i < el.childNodes.length; i++) \n\
+    if (el.childNodes[i].nodeType == document.TEXT_NODE) \n\
+      s += el.childNodes[i].nodeValue; \n\
+    else if (el.childNodes[i].nodeType == document.ELEMENT_NODE && \n\
+             el.childNodes[i].tagName == 'BR') \n\
+      s += ' '; \n\
+    else \n\
+      s += getTextValue(el.childNodes[i]); \n\
+  return normalizeString(s); \n\
+} \n\
+function compareValues(v1, v2) { \n\
+  var f1, f2; \n\
+  f1 = parseFloat(v1); \n\
+  f2 = parseFloat(v2); \n\
+  if (!isNaN(f1) && !isNaN(f2)) { \n\
+    v1 = f1; \n\
+    v2 = f2; \n\
+  } \n\
+  if (v1 == v2) \n\
+    return 0; \n\
+  if (v1 > v2) \n\
+    return 1 \n\
+  return -1; \n\
+} \n\
+var whtSpEnds = new RegExp('^\\s*|\\s*$', 'g'); \n\
+var whtSpMult = new RegExp('\\s\\s+', 'g'); \n\
+function normalizeString(s) { \n\
+  s = s.replace(whtSpMult, ' ');  // Collapse any multiple whites space. \n\
+  s = s.replace(whtSpEnds, '');   // Remove leading or trailing white space. \n\
+  return s; \n\
+} \n\
+//]]></script> \n\
+<style type='text/css'> \n\
+th { \n\
+font-size:8pt; \n\
+text-align:left; \n\
+} \n\
+table {border:1px solid #silver;border-collapse: collapse;} \n\
+td {padding:1mm;} \n\
+.a { \n\
+text-align:center; \n\
+} \n\
+.b { \n\
+text-align:center; \n\
+cursor:pointer; \n\
+} \n\
+</style> \n\
+</head> \n\
+<body> \n\
+"
+
+chKeys = {}
+for c in text:
+    try:
+        chKeys[c] += 1
+    except:
+        chKeys[c] = 1
+
+keys = chKeys.keys()
+keys.sort()
+
+
+print "<table border=1><tr>"
+
+if len(keys)<400:
+    print "<th><a href='' onclick='return sortTable2(0)'>Character</a></th><th><a href='' onclick='return sortTable2(1)'>Occurrences</a></th><th><a href='' onclick='return sortTable2(0)'>UCS</a></th><th><a href='' onclick='return sortTable2(3)'>Unicode Block</a></th><th><a href='' onclick='return sortTable2(4)'>Unicode Name</a></th>"
+else:
+    print "<th>Character</th><th>Occurrences</th><th>UCS</th><th>Unicode Block</th><th>Unicode Name</th>"
+
+print "</tr><tbody id='theTable'>"
+#len(text) and len(keys) don't work caused by uni chars > U+FFFF
+total = 0
+distinct = 0
+regExp = {}
+data = {}
+for ch in keys:
+    try:
+        data["%04X" % int(ch)] = unicodedata.name(wunichr(ch))
+    except:
+        regExp["%04X" % int(ch)] = 1
+
+UnicodeData = os.popen("zgrep -E '^(" + "|".join(regExp.keys()) + ");' '" + bundleLibPath + "UnicodeData.txt.zip'").read().decode("UTF-8")
+
+for c in UnicodeData.split('\n'):
+    uniData = c.strip().split(';')
+    if len(uniData) > 1: data[uniData[0]] = uniData[1]
+
+for c in keys:
+    if c != 10:
+        total += chKeys[c]
+        distinct += 1
+        t = wunichr(c)
+        try:
+            name = data["%04X" % int(c)]
+        except:
+            name = rangeName(c) + "-%04X" % int(c)
+        if name[0] == '<': name = rangeName(c) + "-%04X" % int(c)
+        if "COMBINING" in name: t = u"◌" + t
+        print "<tr><td class='a'>", t, "</td><td class='a'>", chKeys[c], "</td><td>", "U+%04X" % (int(c)), "</td><td>", getBlockName(c), "</td><td>", name, "</tr>"
+
+print "</tbody></table>"
+
+print "<p style='font-size:8pt;'><i>"
+pl = "s"
+if total < 2: pl = ""
+print str(total) + " character%s in total (without '\\n')<br>" % pl
+print str(distinct) + " distinct characters</i></p>"
+
+print "</body></html>"

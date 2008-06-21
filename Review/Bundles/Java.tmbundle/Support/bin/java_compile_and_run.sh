117c84cd19178454f53ab43747f0776b2f854de4
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-06-21 04:40:15 +0000

    • Adding missing quotes around TMP var.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10004 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/Java.tmbundle/Support/bin/java_compile_and_run.sh
+++ Review/Bundles/Java.tmbundle/Support/bin/java_compile_and_run.sh
@@ -9,9 +9,9 @@ fi
 SOURCE="$1"
 shift
 TMP=/tmp/tm_javamate 
-mkdir -p $TMP
-cd $TMP
-"$TM_JAVAC" -d $TMP -encoding UTF8 "$SOURCE"
+mkdir -p "$TMP"
+cd "$TMP"
+"$TM_JAVAC" -d "$TMP" -encoding UTF8 "$SOURCE"
 if (($? >= 1)); then exit; fi
 	
 "$TM_JAVA" -Dfile.encoding=utf-8 $(basename -s .java "$SOURCE") $@

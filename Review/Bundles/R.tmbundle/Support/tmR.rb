bd6266bdceddd8682187e6632f5b3e7c67f6cf96
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-08-14 12:46:56 +0000

    Modified Rmate approach: - usage of scriptmate my_popen3 method - R slave's stderr is redirected to stdout -- in order to synchronize warning messages for instances -- error messages and warning messages are caught by regexps - the prompts '>' and '+' and now hyperlinked to the source code line -- this only works for source code coming from the doc; imported R code lines destroys up to now the linkage - if an error occurs now the last line is: -- Execution halted at line ?? (implemented as hyperlink) - comments are shown in italics blue - recursive deleting of a path function is now an UNIX shell command - ESC now cancels the current job by closing the output window - STDOUT is again set to sync = TRUE - restructuring of the entire source code
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10439 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/R.tmbundle/Support/tmR.rb
+++ Review/Bundles/R.tmbundle/Support/tmR.rb
@@ -2,25 +2,77 @@
 #
 # RMate v0.1, 2006-05-03.
 # Copied, by Charilaos Skiadas, from RubyMate by Sune Foldager.
-#
 # v0.1  (2005-08-12): Initial version.
 # v0.9  Heavily modified by Kevin Ballard
 # v0.91 Plots are displayed inside of the HTML window; using a thread for sending R code to R by Hans-Jörg Bibiko
-#
-# We need this for HTML escaping.
+
 require 'cgi'
 require 'fcntl'
+SUPPORT_LIB = ENV['TM_SUPPORT_PATH'] + '/lib/'
 
-require File.join(File.dirname(__FILE__), 'lib/popen3')
+# JavaScript to hide the start message on runtime
+hideStartMessageJS = %{<script type="text/javascript">document.getElementById('start_message').className='hidden'</script>}
+# line counter for hyperlinking R's prompt to jump back to the doc
+linecounter = 1
 
 # HTML escaping function.
 def esc(str)
   CGI.escapeHTML(str)
 end
 
+def my_popen3(*cmd) # returns [stdin, stdout, strerr, pid]
+  pw = IO::pipe   # pipe[0] for read, pipe[1] for write
+  pr = IO::pipe
+  pe = IO::pipe
+  
+  # F_SETOWN = 6, ideally this would be under Fcntl::F_SETOWN
+  pw[0].fcntl(6, ENV['TM_PID'].to_i) if ENV.has_key? 'TM_PID'
+  
+  pid = fork{
+    pw[1].close
+    STDIN.reopen(pw[0])
+    pw[0].close
+
+    pr[0].close
+    STDOUT.reopen(pr[1])
+    pr[1].close
+
+    pe[0].close
+    STDERR.reopen(pe[1])
+    pe[1].close
+
+    # maybe there is a way to support this in future ;)
+    tm_interactive_input = SUPPORT_LIB + '/tm_interactive_input.dylib'
+    if (File.exists? tm_interactive_input) 
+      dil = ENV['DYLD_INSERT_LIBRARIES']
+      ENV['DYLD_INSERT_LIBRARIES'] = (dil) ? "#{tm_interactive_input}:#{dil}" : tm_interactive_input unless (dil =~ /#{tm_interactive_input}/)
+      ENV['DYLD_FORCE_FLAT_NAMESPACE'] = "1"
+      ENV['TM_INTERACTIVE_INPUT'] = 'AUTO|ECHO'
+    end
+    
+    exec(*cmd)
+  }
+
+  pw[0].close
+  pr[1].close
+  pe[1].close
+
+  pw[1].sync = true
+
+  [pw[1], pr[0], pe[0], pid]
+end
+
+
+def recursive_delete(path)
+  %x{rm -rf #{path}}
+end
+
+# allow the user to define its own output font
 outputFont = (ENV['TM_RMATE_OUTPUT_FONT'] == nil) ? "Monaco" : ENV['TM_RMATE_OUTPUT_FONT']
 
+# what comes in
 what = (ENV['TM_SELECTED_TEXT'] == nil) ? "document" : "selection"
+
 # Headers...
 print <<-EOS
 <html>
@@ -31,52 +83,48 @@ EOS
 # Prepare some values for later.
 myFile = __FILE__
 myDir = File.dirname(myFile) + '/'
+# include pastel.css and substitute some placeholder
 File.open(File.join(myDir, 'pastel.css')) {|f| f.each_line {|l| print l.gsub('FONTPLACEHOLDER',outputFont)} }
 print <<-HTML
 </style>
+<script type="text/javascript">
+document.onkeyup = function keyPress(event) {
+  if ( typeof event == "undefined" ) event = window.event;
+  wkey = event.keyCode;
+  if ( wkey == 27 ) window.close();
+}
+</script>
+
 </head>
 <body>
-<div id="script_output" class="framed">
-<div id="start_message"><pre><strong>RMate. Executing #{what} in R. This may take a while...</strong></pre></div>
-<pre><div id="actual_output" style="-khtml-line-break: after-white-space;">
+  <div id="script_output" class="framed">
+    <div id="start_message"><pre><strong>RMate. Executing #{what} in R. This may take a while...</strong></pre></div>
+    <pre><div id="actual_output" style="-khtml-line-break: after-white-space;">
 HTML
-
-def recursive_delete(path)
-  if (File.directory?(path)) then
-    Dir.foreach(path) { |file| recursive_delete(File.join(path,file)) unless [".",".."].include?(file) }
-    Dir.unlink(path)
-  else
-    File.unlink(path)
-  end
-end
 
 tmpDir = File.join(ENV['TMP'] || "/tmp", "TM_R")
 recursive_delete(tmpDir) if File.exists?(tmpDir) # remove the temp dir if it's already there
 Dir::mkdir(tmpDir)
-# Mechanism for dynamic reading inspired by RubyMate
-stdin, stdout, stderr = popen3("R", "--vanilla", "--no-readline", "--slave", "--encoding=UTF-8")
 
+# Mechanism for dynamic reading
+# stdin, stdout, stderr = popen3("R", "--vanilla", "--no-readline", "--slave", "--encoding=UTF-8")
+stdin, stdout, stderr, pid = my_popen3("R --vanilla --no-readline --slave --encoding=UTF-8 2>&1")
+# init the R slave
 stdin.puts(%{options(device="pdf")})
 stdin.puts(%{formals(pdf)[c("file","onefile","width","height")] <- list("#{tmpDir}/Rplot%03d.pdf", FALSE, 8, 8)})
 stdin.puts(%{if(getRversion()>="2.7") pdf.options(onefile=FALSE)})
 stdin.puts(%{options(pager="/bin/cat")})
 stdin.puts("options(echo=T)")
 
-# stdout.sync = false
-# stderr.sync = false
-
 # suggestion by Hans-J. Bibiko: if a doc is too large give R the chance to execute code, otherwise the pipe blocks (?)
 Thread.new {
-	STDIN.each do |line|
-		stdin.puts(line.chomp)
-#		stdin.print(line)
-	end
-	stdin.close
+  STDIN.each do |line|
+    stdin.puts(line.chomp)
+  end
+  stdin.close
 }
 
-#stdin.write(STDIN.read.chomp)
-
-hideStartMessageJS = %{<script type="text/javascript">document.getElementById('start_message').className='hidden'</script>}
+STDOUT.sync = true
 
 descriptors = [stdout, stderr]
 descriptors.each { |fd| fd.fcntl(Fcntl::F_SETFL, Fcntl::O_NONBLOCK) }
@@ -87,39 +135,53 @@ until descriptors.empty?
       descriptors.delete io
       io.close
     elsif io == stderr
+      # just in case
       print hideStartMessageJS
-      if str.match(/(?i)^\s*(error|erreur|fehler|errore|erro)( |:)/)
-        print %{<span style="color: red">#{esc str}</span>}
-      elsif str.match(/^Execution halted/)
-        print %{<span style="color: darkred;">#{esc str}</span>}
-      else
-        print %{<span style="color: darkgray;font-style:italic;">#{esc str}</span>}
-      end
+      print %{<span style="color: red">#{esc str}</span>}
     elsif io == stdout
       print hideStartMessageJS
-      print( str.map do |line|
-          esc(line).gsub(/^(&gt;|\+)/,'<span style="color: darkcyan">\1</span>')
-      end.join )
+      str.each_line do |line|
+        # check for comment sign at the beginning of a line
+        if line.match(/^>\s*#/)
+          print "<i><font color=blue>#{esc(line)}</font></i>"
+        # check for comment at the end of a line
+        elsif m=line.match(/(.*?)(\s#[^"']*)\n/)
+          print esc(m[1]).gsub(/^(&gt;|\+)/,'<a class="prompt" href="txmt://open?line='+linecounter.to_s+'">\1</a>')
+          print "<i><font color=blue>#{esc(m[2])}</font></i>\n"
+        # check for error messages
+        elsif m=line.match(/(?i)^\s*(error|erreur|fehler|errore|erro)( |:)/)
+          print "<span style='color: red'>#{esc str.gsub(%r{(?m).*?#{m[1]}},m[1]).chomp} at <a href='txmt://open?line=#{linecounter-1}'>line #{linecounter-1}</a></span><br />"
+          break
+        # check for warnings
+        elsif line.match(/^\s*(Warning|Warning messages?|Message d.avis|Warnmeldung|Messaggio di avvertimento|Mensagem de aviso):/)
+          print "<span style='color: gray'>#{esc line}</span>"
+        # print line simply with hyperlinked prompt if given
+        else
+          print esc(line).gsub(/^[\x0-\x9]?(&gt;|\+)/,'<a class="prompt" href="txmt://open?line='+linecounter.to_s+'">\1</a>')
+        end
+        linecounter += 1 if line.match(/^[\x0-\x9]?(>|\+) /)
+      end
     end
   end
 end
 
-#system("open -a Preview '#{tmpDir}'") unless Dir::glob("#{tmpDir}/*.pdf").empty?
-STDOUT.sync = false
 STDOUT.flush
+
+# check for generated plots; if yes, embed them into the HTML output as PDF images
 if !Dir::glob("#{tmpDir}/*.pdf").empty?
-	width = (Dir::glob("#{tmpDir}/*.pdf").size > 1) ? "50%" : "100%"
-	puts '<br /><strong><i>click on image to open or drag the image to a location as PDF</i></strong><hr />'
-	counter = 0
-	Dir::glob("#{tmpDir}/*.pdf") { |f| 
-		counter +=  1
-		print "<img width=#{width} onclick=\"TextMate.system(\'open \\'#{f}\\'\',null);\" src='file://#{f}' />"
-		print "<br>" if (counter % 2 == 0)
-	}
-	puts "<hr /><input type=button onclick=\"TextMate.system(\'open -a Preview \\'#{tmpDir}\\'\',null);\" value='Open all Images in Preview' />&nbsp;&nbsp;&nbsp;<input type=button onclick=\"TextMate.system(\'open -a Finder \\'#{tmpDir}\\'\',null);\" value='Reveal all Images in Finder' />"
+  width = (Dir::glob("#{tmpDir}/*.pdf").size > 1) ? "50%" : "100%"
+  puts '<br /><strong><i>click on image to open or drag the image to a location as PDF</i></strong><hr />'
+  counter = 0
+  Dir::glob("#{tmpDir}/*.pdf") { |f| 
+    counter +=  1
+    print "<img width=#{width} onclick=\"TextMate.system(\'open \\'#{f}\\'\',null);\" src='file://#{f}' />"
+    print "<br>" if (counter % 2 == 0)
+  }
+  puts "<hr /><input type=button onclick=\"TextMate.system(\'open -a Preview \\'#{tmpDir}\\'\',null);\" value='Open all Images in Preview' />&nbsp;&nbsp;&nbsp;<input type=button onclick=\"TextMate.system(\'open -a Finder \\'#{tmpDir}\\'\',null);\" value='Reveal all Images in Finder' />"
 end
 
 puts '</div></pre></div>'
+
 # Footer.
 print <<-HTML
 </body>

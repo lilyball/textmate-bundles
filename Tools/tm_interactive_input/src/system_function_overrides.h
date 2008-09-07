00bb23ab7ba781f7070e0e92d18723109afacd7c
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-09-07 12:00:32 +0000

    • Prevent a compile time warning about conflicting symbols when building for Leopard.
    The warning was ‘system_function_overrides.h:22: warning: asm declaration ignored due to conflict with previous rename’.
    
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10535 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/tm_interactive_input/src/system_function_overrides.h
+++ Tools/tm_interactive_input/src/system_function_overrides.h
@@ -19,7 +19,12 @@ int dup(int);
 int close(int);
 
 int system_select(char * symbol, int nfds, fd_set * __restrict readfds, fd_set * __restrict writefds, fd_set * __restrict errorfds, struct timeval * __restrict timeout);
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_5
 int select(int, fd_set * __restrict, fd_set * __restrict, fd_set * __restrict, struct timeval * __restrict) __asm("_select");
+#else
+int select(int, fd_set * __restrict, fd_set * __restrict, fd_set * __restrict, struct timeval * __restrict);
+#endif
 int select_darwinextsn(int, fd_set * __restrict, fd_set * __restrict, fd_set * __restrict, struct timeval * __restrict) __asm("_select$DARWIN_EXTSN");
 int select_darwinextsn_nocancel(int, fd_set * __restrict, fd_set * __restrict, fd_set * __restrict, struct timeval * __restrict) __asm("_select$DARWIN_EXTSN$NOCANCEL");
 int select_nocancel_unix2003(int, fd_set * __restrict, fd_set * __restrict, fd_set * __restrict, struct timeval * __restrict) __asm("_select$NOCANCEL$UNIX2003");

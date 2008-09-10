f5948a615f133806b743032fedf3921ffcc33566
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-09-10 04:20:21 +0000

    • Collection of syntax bugs found so far (but are fixed).
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10543 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/Java.tmbundle/SyntaxTests/Test.java
@@ -0,0 +1,44 @@
+import com.foo.*;
+import com.bar.*;  // comment
+import com.baz.*;
+
+  /** class foo */
+
+public class Foo
+{
+	void bar(Object baz)
+	{
+		throw new RuntimeException(baz.toString() + "; void");
+	}
+}
+
+public class Hello
+{
+	void method(Integer integer) {}
+	void method(INTEGER integer) {}
+	
+	private static final int ID = 0;
+    ID id = new ID();
+    Id id = new Id();
+    
+}
+
+class Foo // bar
+{
+}
+
+class Foo /* bar */
+{
+}
+
+interface Foo // bar
+{
+}
+
+interface Foo /* bar */
+{
+}
+
+class Foo
+{
+}

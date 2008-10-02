bed6d4944f813f294a09acd089c2d1fbfbab1e0b
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-10-02 17:32:05 +0000

    • changed showHelp.sh to use only DIALOG1
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10666 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/GetBundles.tmbundle/Support/bin/showHelp.sh
+++ Review/Bundles/GetBundles.tmbundle/Support/bin/showHelp.sh
@@ -1,16 +1,8 @@
 
-if [ `echo -n "$DIALOG" | tail -c 1` != "2" ]; then
-	CHECK=$("$DIALOG" -l | egrep -c 'GetBundles — Help')
-else
-	CHECK=$("$DIALOG" window list | egrep -c 'GetBundles — Help')
-fi
+CHECK=$("$DIALOG" -l | egrep -c 'GetBundles — Help')
 
 if [ ! $CHECK == 0 ]; then
-	if [ `echo -n "$DIALOG" | tail -c 1` != "2" ]; then
-		"$DIALOG" -l `"$DIALOG" -l | egrep 'GetBundles — Help' | awk '{print $1}'`
-	else
-		"$DIALOG" window close `"$DIALOG" window list | egrep 'GetBundles — Help' | awk '{print $1}'`
-	fi
+	"$DIALOG" -x `"$DIALOG" -l | egrep 'GetBundles — Help' | awk '{print $1}'`
 fi
 
 TMPDIR="/tmp/TM_GetBundlesTEMP"
@@ -20,10 +12,6 @@ echo "<base href='file://${TM_BUNDLE_SUPPORT// /%20}/help.markdown'>" > "$TMPDIR
 echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>" >> "$TMPDIR/help.html"
 cat "$TM_BUNDLE_SUPPORT/help.markdown" | Markdown.pl|SmartyPants.pl >> "$TMPDIR/help.html"
 
-if [ `echo -n "$DIALOG" | tail -c 1` != "2" ]; then
-	echo -en "{title='GetBundles — Help';path='$TMPDIR/help.html';}" | "$DIALOG" -a help 1> /dev/null
-else
-	echo -en "{title='GetBundles — Help';path='$TMPDIR/help.html';}" | "$DIALOG" window create help 1> /dev/null
-fi
+echo -en "{title='GetBundles — Help';path='$TMPDIR/help.html';}" | "$DIALOG" -a help 1> /dev/null
 
 rm -rf "$TMPDIR"

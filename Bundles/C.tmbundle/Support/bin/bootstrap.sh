3b73d16bbe2b940b42ff73fcc52eb376c54c7598
aross <aross@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-10-11 20:02:40 +0000

    • Moved as much logic as possible from bootstrap.sh to Run.tmcommand. • Use the new :bootstrap option to Executor instead of embedding it at the front of the other arguments to Executor. • Also, escape spaces in ENV["TM_FILEPATH"] before passing it to Executor.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10708 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/C.tmbundle/Support/bin/bootstrap.sh
+++ Bundles/C.tmbundle/Support/bin/bootstrap.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-if [ $TM_GCC_MODE = "c" ]; then
-  compiler=${TM_GCC:-gcc};
-elif [ $TM_GCC_MODE = "c++" ]; then
-  compiler=${TM_GPP:-g++};
-fi
-
-for p in $@; do
-  if [ p = "--version" ]; then
-    $compiler $@;
-    exit;
-  fi
-done
-
 eval last=\$$#;
-$compiler $@ -o "${last}.out";
+$@ -o "${last}.out";
 
 if [ $? -eq 0 ]; then
   "${last}.out";

ef86f65d261aef459a08cfe2676de2e1170d6bd7
aross <aross@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-10-16 20:02:51 +0000

    Remove an unnecessary File.exists\?, and fix escaping of spaces in the onclick handler.
    git-svn-id: http://macromates.com/svn/Bundles/trunk@10722 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/tm/require_cmd.rb
+++ Support/lib/tm/require_cmd.rb
@@ -5,7 +5,7 @@ require ENV["TM_SUPPORT_PATH"] + "/lib/exit_codes"
 module TextMate
   class << self
     def require_cmd(cmd)
-      unless File.exists?(cmd) and File.executable?(cmd) or ENV['PATH'].split(':').any? { |dir| File.executable? File.join(dir, cmd) }
+      unless File.executable?(cmd) or ENV['PATH'].split(':').any? { |dir| File.executable? File.join(dir, cmd) }
         TextMate::HTMLOutput.show(:title => "Can't find “#{cmd}” on PATH.", :sub_title => "") do |io|
           io << "<p>The current PATH is:</p>"
           io << "<blockquote>"
@@ -15,7 +15,7 @@ module TextMate
           io << "</blockquote>"
           io << "<p>Please add the directory containing “<code>#{cmd}</code>” to <code>PATH</code> in TextMate's Shell Variables preferences.</p>"
           io << "<p>Alternatively, TextMate can retrieve the <code>PATH</code> from Terminal install it in TextMate's preferences for you. TextMate will be restarted.</p>"
-          io << "<p><button onclick=\"TextMate.system(\'bash #{e_sh(ENV["TM_SUPPORT_PATH"]+"/bin/set_tm_path.sh")}\')\">Set PATH</button></p>"
+          io << "<p><button onclick=\"TextMate.system('#{(ENV["TM_SUPPORT_PATH"]+"/bin/set_tm_path.sh").gsub(" ", "\\\\\\\\ ")}', null)\">Set PATH</button></p>"
         end
         TextMate.exit_show_html
       end

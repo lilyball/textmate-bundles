53d051c8077bd8fa190e131a7c3a915309b84cc3
msheets <msheets@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2008-11-14 00:49:34 +0000

    Add VHDL bundle from Brian Padalino.
    git-svn-id: http://svn.textmate.org/trunk@10802 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Review/Bundles/VHDL.tmbundle/Templates/Entity and Architecture.tmTemplate/template_entity.vhd
@@ -0,0 +1,15 @@
+library ieee ;
+use ieee.std_logic_1164.all ;
+use ieee.numeric_std.all ;
+
+entity ${TM_NEW_FILE_BASENAME} is
+  port (
+    
+  ) ;
+end entity ${TM_NEW_FILE_BASENAME} ;
+
+architecture arch of ${TM_NEW_FILE_BASENAME} is
+
+begin
+
+end architecture arch ;
---
+++ Review/Bundles/VHDL.tmbundle/Templates/Package and Package Body.tmTemplate/template_package.vhd
@@ -0,0 +1,15 @@
+library ieee ;
+use ieee.std_logic_1164.all ;
+use ieee.numeric_std.all ;
+
+package ${TM_NEW_FILE_BASENAME} is
+
+end package ${TM_NEW_FILE_BASENAME} ;
+
+library ieee ;
+use ieee.std_logic_1164.all ;
+use ieee.numeric_std.all ;
+
+package body ${TM_NEW_FILE_BASENAME} is
+
+end package ${TM_NEW_FILE_BASENAME} ;

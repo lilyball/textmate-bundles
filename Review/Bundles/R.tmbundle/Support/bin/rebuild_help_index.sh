ae427f5b9ab47f3cede0d39e0494eed61dcf4191
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-01-07 09:25:49 +0000

    • improved helper facilities; now rebuild_help_index looks in all directories given by the R command '.libPaths()'  - this solves the issue if the user installed packages at 'User Level'
    git-svn-id: http://svn.textmate.org/trunk@11086 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/R.tmbundle/Support/bin/rebuild_help_index.sh
+++ Review/Bundles/R.tmbundle/Support/bin/rebuild_help_index.sh
@@ -12,16 +12,29 @@ done
 #rebuild help.index?
 
 PKGS="$TM_BUNDLE_SUPPORT"/help.pkgs
+PKGSTEMP="$TM_BUNDLE_SUPPORT"/help.pkgstemp
 INDEX="$TM_BUNDLE_SUPPORT"/help.index
+LIBPATHSFILE="$TM_BUNDLE_SUPPORT"/libpaths
 
-#check for removed/installed paclages
+# get LIB paths from R
+if [ ! -e "$LIBPATHSFILE" ]; then
+	echo "cat(.libPaths())" | R --slave > "$LIBPATHSFILE"
+fi
+
+LIBPATHS=$(cat "$LIBPATHSFILE")
+
+#check for removed/installed packages
 if [ -e "$PKGS" ]; then
-	FILE=$(ls "${R_HOME:-/Library/Frameworks/R.framework/Resources}"/library/  | grep -v -F .)
-	OLD=$(cat "$PKGS")
-	if [ "$OLD" != "$FILE" ]; then
+	rm -f "$PKGSTEMP"
+	for lpath in $LIBPATHS
+	do
+		ls "$lpath" | grep -v -F . >> "$PKGSTEMP"
+	done
+	if ! cmp --silent "$PKGS" "$PKGSTEMP"; then
 		[[ -e "$INDEX" ]] && rm "$INDEX"
 		[[ -e "$PKGS" ]] && rm "$PKGS"
 	fi
+	rm "$PKGSTEMP"
 else
 	[[ -e "$INDEX" ]] && rm "$INDEX"
 fi
@@ -33,13 +46,16 @@ fi
 
 if [ ! -e "$INDEX" ]; then
 	export token=$("$DIALOG" -a ProgressDialog -p "{title=Rdaemon;isIndeterminate=1;summary='R Documentation';details='Create Help Index…';}")
-	[[ -e "$HOME"/Rdaemon/daemon ]] && echo -n "$TM_BUNDLE_SUPPORT" > "$HOME"/Rdaemon/daemon/pathToRbundle
-	ls "${R_HOME:-/Library/Frameworks/R.framework/Resources}"/library/ | grep -v -F . > "$PKGS"
-	FILE=$(find -f "${R_HOME:-/Library/Frameworks/R.framework/Resources}"/library/* -name AnIndex)
-	for f in $FILE
+	rm -f "$PKGS"
+	for lpath in $LIBPATHS
 	do
-		lib=$(echo -n "${f//\//\\/}" | sed 's/\\\/help\\\/AnIndex//')
-		cat "$f" | perl -e "while(<>){\$a=\$_;\$a=~s!(.*?)\t(.*)!\$1\t$lib/latex/\$2.tex!;print \$a;}" >> "$INDEX"
+		ls "$lpath" | grep -v -F . >> "$PKGS"
+		FILE=$(find -f "$lpath"/* -name AnIndex)
+		for f in $FILE
+		do
+			lib=$(echo -n "${f//\//\\/}" | sed 's/\\\/help\\\/AnIndex//')
+			cat "$f" | perl -e "while(<>){\$a=\$_;\$a=~s!(.*?)\t(.*)!\$1\t$lib/latex/\$2.tex!;print \$a;}" >> "$INDEX"
+		done
 	done
 	"$DIALOG" -x $token 2&>/dev/null
 fi

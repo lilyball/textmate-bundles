4196dc2125f7e98b69a0a7ad711395efc5f93ded
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-02-08 20:39:56 +0000

    • Help: full text search now searches in each lib path
    git-svn-id: http://svn.textmate.org/trunk@11262 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/R.tmbundle/Support/bin/Rsearch.sh
+++ Bundles/R.tmbundle/Support/bin/Rsearch.sh
@@ -6,6 +6,14 @@ AS="$3"
 HEAD="$BPATH"/lib/head.html
 DATA="$BPATH"/lib/data.html
 
+LIBPATHSFILE="$TM_BUNDLE_SUPPORT"/libpaths
+# get LIB paths from R
+if [ ! -e "$LIBPATHSFILE" ]; then
+	echo "cat(.libPaths())" | R --slave > "$LIBPATHSFILE"
+fi
+LIBPATHS=$(cat "$LIBPATHSFILE")
+
+
 echo "<html><body style='margin-top:5mm'><table style='border-collapse:collapse'><tr><td style='padding-right:1cm;border-bottom:1px solid black'><b>Package</b></td><td style='border-bottom:1px solid black'><b>Topic</b></td></tr>" > "$HEAD"
 
 if [ "$AS" == "1" ]; then
@@ -29,22 +37,25 @@ if [ "$AS" == "1" ]; then
 		done
 	fi
 else
-	FILE=$(egrep -m 260 -l -i -r "$TERM" "${R_HOME:-/Library/Frameworks/R.framework/Resources}"/library/*/help/ | perl -pe  's!/help/(.*)!/html/$1.html!;s/AnIndex\.html/00Index.html/')
+	for lpath in $LIBPATHS
+	do
+  	FILE=$(egrep -m 260 -l -i -r "$TERM" "$lpath"/*/help/ | perl -pe  's!/help/(.*)!/html/$1.html!;s/AnIndex\.html/00Index.html/')
 
-	CNT=$(echo "$FILE" | wc -l)
-	if [ "$CNT" -gt "250" ]; then
-		echo "<tr><td colspan=2><font style='color:red;font-size:8pt'>More than $CNT files found.<br>Please narrow your search.</td></tr>" >> "$HEAD"
-		#echo "<html><head><title>R Documentation</title></head><body></body></html>" > "$DATA"
-	else
-		for i in `echo $FILE`
-		do
-			echo "<tr><td>" >> "$HEAD"
-			echo "$i" | sed 's/\(.*\)library\/\(.*\)\/html\/\(.*\)\.html/\2/' >> "$HEAD"
-			echo "</td><td><a href='$i' target='data'>" >> "$HEAD"
-			echo "$i" | sed 's/\(.*\)library\/\(.*\)\/html\/\(.*\)\.html/\3/' >> "$HEAD"
-			echo "</a></td></tr>" >> "$HEAD"
-		done
-	fi
+  	CNT=$(echo "$FILE" | wc -l)
+  	if [ "$CNT" -gt "250" ]; then
+  		echo "<tr><td colspan=2><font style='color:red;font-size:8pt'>More than $CNT files found.<br>Please narrow your search.</td></tr>" >> "$HEAD"
+  		#echo "<html><head><title>R Documentation</title></head><body></body></html>" > "$DATA"
+  	else
+  		for i in `echo $FILE`
+  		do
+  			echo "<tr><td>" >> "$HEAD"
+  			echo "$i" | sed 's/\(.*\)library\/\(.*\)\/html\/\(.*\)\.html/\2/' >> "$HEAD"
+  			echo "</td><td><a href='$i' target='data'>" >> "$HEAD"
+  			echo "$i" | sed 's/\(.*\)library\/\(.*\)\/html\/\(.*\)\.html/\3/' >> "$HEAD"
+  			echo "</a></td></tr>" >> "$HEAD"
+  		done
+  	fi
+	done
 fi
 if [ "$CNT" -eq "1" -a "${#FILE}" -gt "0" ]; then
 	cat "$FILE" > "$DATA"

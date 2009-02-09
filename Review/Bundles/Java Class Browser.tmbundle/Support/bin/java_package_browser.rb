6d42e6441637917196d2e5fcbfb1d9fd011576ac
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-02-09 11:08:51 +0000

    • If the browser is invoked for a single file (opposed to a project) display an error dialog.
    git-svn-id: http://svn.textmate.org/trunk@11273 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Review/Bundles/Java Class Browser.tmbundle/Support/bin/java_package_browser.rb
+++ Review/Bundles/Java Class Browser.tmbundle/Support/bin/java_package_browser.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env ruby
 
-begin
-  require ENV['TM_BUNDLE_SUPPORT'] + '/lib/JavaClassBrowser'
-  require ENV['TM_BUNDLE_SUPPORT'] + '/lib/Project'
+if not ENV['TM_PROJECT_DIRECTORY'].nil?
+  begin
+    require ENV['TM_BUNDLE_SUPPORT'] + '/lib/JavaClassBrowser'
+    require ENV['TM_BUNDLE_SUPPORT'] + '/lib/Project'
 
-  p = Project.new(ENV['TM_PROJECT_DIRECTORY'])
-  p.scan
-  jcb = JavaClassBrowser.new(p)
-  jcb.show  
-rescue Exception => e
+    p = Project.new(ENV['TM_PROJECT_DIRECTORY'])
+    p.scan
+    jcb = JavaClassBrowser.new(p)
+    jcb.show  
+  rescue Exception => e
+    require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
+    TextMate::UI.alert(:critical, "Error", e.inspect + "\n\n" + e.backtrace.to_s)
+  end
+else
   require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
-  TextMate::UI.alert(:critical, "Error", e.inspect + "\n\n" + e.backtrace.to_s)
+  TextMate::UI.alert(:critical, "Not A Project", "The Java Class Browser only works for projects.")
 end
-

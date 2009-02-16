a3adcf5fa9d5d0acbea34aaf5416573415911b12
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-02-16 21:07:30 +0000

    • outsourced 'insert_package_name.sh' • replace snippets 'Load Library using "library/require"' by tmCommands • improved 'Load Library using "library/require"' snippet's syntax
    git-svn-id: http://svn.textmate.org/trunk@11311 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/R.tmbundle/Support/bin/insert_package_name.sh
@@ -0,0 +1,21 @@
+. "$TM_BUNDLE_SUPPORT"/bin/rebuild_help_index.sh
+
+
+cat "$TM_BUNDLE_SUPPORT"/help.pkgs | sort -f | ruby -e '
+	isDIALOG2 = ! ENV["DIALOG"].match(/2$/).nil?
+	exit unless isDIALOG2
+	require File.join(ENV["TM_SUPPORT_PATH"], "lib/ui.rb")
+	require File.join(ENV["TM_SUPPORT_PATH"], "lib/exit_codes.rb")
+	words = STDIN.read().split("\n")
+	if isDIALOG2
+		TextMate::UI.complete(words)
+	else
+		index=TextMate::UI.menu(words)
+		if index != nil
+			print words[index]
+		else
+			TextMate.exit_discard()
+		end
+	end
+	exit 203
+'

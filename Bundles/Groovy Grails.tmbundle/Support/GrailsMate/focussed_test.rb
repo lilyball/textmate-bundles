91ea52ffe445e1a5c5ce64aa35cf6d8da4eeb089
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-04-06 01:50:28 +0000

    • Support using focussed tests on the class under test as well as the test case class.
    git-svn-id: http://svn.textmate.org/trunk@11437 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Groovy Grails.tmbundle/Support/GrailsMate/focussed_test.rb
+++ Bundles/Groovy Grails.tmbundle/Support/GrailsMate/focussed_test.rb
@@ -4,7 +4,7 @@ potential_phases = ["unit", "integration", "functional", "other"]
 
 path = ARGV[0]
 test = File.basename(path)
-test =~ /^(.+)Tests.(.+)$/
+test =~ /^(.+)\.(.+)$/
 clazz = $1
 extension = $2
 if extension == "groovy" or extension == "java"
@@ -12,6 +12,7 @@ if extension == "groovy" or extension == "java"
   potential_phases.each do |potential_phase|
     phases << "--#{potential_phase}" if path =~ /test\/#{potential_phase}\//
   end
+  clazz.sub! /Tests$/, ""
   GrailsCommand.new("test-app #{clazz} #{phases.join(' ')}").run
 else 
   TextMate.exit_show_tool_tip "#{test} is not a test case"

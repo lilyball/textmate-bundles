1acb686387bf5be7b236973aca06c91038379ad9
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-04-06 01:14:16 +0000

    • Removed commands for ‘create-controller’ and ‘create-domain-class’ in favour of the newly added generic ‘create’ command.
    This new command takes the first provided option and appends it to ‘create-’ to form the grails command.
    
    git-svn-id: http://svn.textmate.org/trunk@11432 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Bundles/Groovy Grails.tmbundle/Support/GrailsMate/create.rb
@@ -0,0 +1,25 @@
+class GrailsCreateCommand < GrailsCommand
+  
+  def initialize()
+    super "create" do |default|
+      TextMate::UI.request_string( 
+        :title => "Create Artefact",
+        :prompt => "Enter the kind of artefact (e.g. domain-class)",
+        :default => ""
+      )
+    end
+    self.colorisations['green'] << /Created (.)+ for (\w)+/
+  end
+
+  def construct_command(task, option) 
+    if option.nil? or option.empty? 
+      return nil
+    else
+      options = Shellwords.shellwords(option)
+      ["#{task}-#{options.pop}"] + options
+    end
+  end
+  
+end
+
+GrailsCreateCommand.new.run

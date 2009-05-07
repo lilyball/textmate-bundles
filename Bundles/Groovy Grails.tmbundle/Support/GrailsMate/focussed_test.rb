095ea364e5ddfd35b22722cdcbac3115930ea8d6
ldaley <ldaley@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-05-07 02:14:49 +0000

    • You can now run multiple focussed tests by selecting more than one in the project drawer before invoking. The name of the command has been updated to reflect this.
    git-svn-id: http://svn.textmate.org/trunk@11516 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Groovy Grails.tmbundle/Support/GrailsMate/focussed_test.rb
+++ Bundles/Groovy Grails.tmbundle/Support/GrailsMate/focussed_test.rb
@@ -2,18 +2,21 @@ require "#{ENV['TM_SUPPORT_PATH']}/lib/exit_codes"
 
 potential_phases = ["unit", "integration", "functional", "other"]
 
-path = ARGV[0]
-test = File.basename(path)
-test =~ /^(.+)\.(.+)$/
-clazz = $1
-extension = $2
-if extension == "groovy" or extension == "java"
-  phases = []
-  potential_phases.each do |potential_phase|
-    phases << "--#{potential_phase}" if path =~ /test\/#{potential_phase}\//
+testclasses = []
+phases = []
+
+ARGV.each do |t|
+  test = File.basename(t)
+  test =~ /^(.+)\.(.+)$/
+  clazz = $1
+  extension = $2
+  if extension != "groovy" and extension != "java"
+    TextMate.exit_show_tool_tip "#{test} is not a test case"
   end
-  clazz.sub! /Tests$/, ""
-  GrailsCommand.new("test-app #{clazz} #{phases.join(' ')}").run
-else 
-  TextMate.exit_show_tool_tip "#{test} is not a test case"
+  potential_phases.each do |potential_phase|
+    phases << "--#{potential_phase}" if t =~ /test\/#{potential_phase}\//
+  end
+  testclasses << clazz.sub(/Tests$/, "")
 end
+
+GrailsCommand.new("test-app #{testclasses.join(' ')} #{phases.uniq.join(' ')}").run

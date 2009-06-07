f916c532ec8c2198583d807f686d896934f960e2
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-06-07 11:49:43 +0000

    • install smart SIGUSR1 handler
    Previously we would kill everything in the process group of the controlled process, and therefor, we changed the pgid of that process — this was bad because that made the “native” killing of the launched command (from TextMate) fail to find offspring.
    
    The new handler constructs a process tree and then from that, determines what the “active” process is (the rightmost/last leaf) then sends SIGINT only to this process (waits a bit, then sends SIGTERM, and moves on to SIGKILL).
    
    It does not go up the tree, but if multiple processes are “stalling” this can be achieved by repeatedly sending SIGUSR1 (which translates to pressing ^C in the Executor controlled command output window). It sends “^C: «command»” to stderr of the command being killed, so the user should get nice feedback, also incase it is somehow determining the wrong active process, or if repeated uses of ^C is needed to fully terminate Executor.
    
    A simple example of a process requiring repeated presses of ?\127^C is:
    
       #!/usr/bin/env ruby -wKU
    
       fork { sleep 200 }
       fork { sleep 200 }
    
       Process.waitall()
    
    
    git-svn-id: http://svn.textmate.org/trunk@11601 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/tm/process.rb
+++ Support/lib/tm/process.rb
@@ -72,6 +72,76 @@
 require ENV['TM_SUPPORT_PATH'] + '/lib/io'
 require 'fcntl'
 
+module PTree # Process Tree Construction
+    module_function
+
+    def insert(tree, node)
+        if tree[:pid] == node[:ppid]
+            tree[:children] << node
+        else
+            tree[:children].find { |child| insert(child, node) }
+        end
+    end
+
+    def build
+        tree = { :pid => 1, :cmd => 'System Startup', :children => [ ] }
+
+        list=%x{ps -xww -o "ppid,pid,command="|{ read header; sort -rnk2|sort -snk1; }}
+        list.each do |line|
+            abort "Syntax error: #{line}" unless line =~ /^\s*(\d+)\s+(\d+)\s+(.*)$/
+            node = { :ppid => $1.to_i, :pid => $2.to_i, :cmd => $3, :children => [ ] }
+            abort "Inconsistent Process Tree: #{line}" unless insert(tree, node)
+        end
+
+        tree
+    end
+
+    def find(tree, pid)
+        return tree if tree[:pid] == pid
+
+        tree[:children].each do |child|
+            res = find(child, pid)
+            return res unless res.nil?
+        end
+
+        nil
+    end
+
+    def traverse(tree, &block)
+        tree[:children].each { |child| traverse(child, &block) }
+        block.call(tree)
+    end
+end
+
+def pid_exists?(pid) # There must be a better way to do this…
+    return "" != %x{ps -xp #{pid}|{ read header; cat; }}.chomp
+end
+
+def kill_and_wait(pid)
+    begin
+        Process.kill("INT", pid)
+        20.times { return unless pid_exists?(pid); sleep 0.02 }
+        Process.kill("TERM", pid)
+        20.times { return unless pid_exists?(pid); sleep 0.02 }
+        Process.kill("KILL", pid)
+    rescue
+        # process doesn't exist anymore
+    end
+end
+
+def setup_kill_handler(pid, &block)
+    Signal.trap("USR1") do
+        did_kill = false
+        PTree.traverse(PTree.find(PTree.build, pid)) do |node|
+            if !did_kill && pid_exists?(node[:pid])
+                block.call("^C: #{node[:cmd]} (pid: #{node[:pid]})\n", :err)
+                kill_and_wait(node[:pid])
+                did_kill = true
+            end
+        end
+	end
+end
+
 module TextMate
   module Process
     class << self
@@ -99,7 +169,6 @@ module TextMate
         io[0][0].fcntl(6, ENV['TM_PID'].to_i) if ENV.has_key? 'TM_PID'
 
         pid = fork {
-          ::Process.setpgid(0, ::Process.pid)
           at_exit { exit! }
           
           STDIN.reopen(io[0][0])
@@ -124,20 +193,6 @@ module TextMate
 
           exec(*cmd.compact)
         }
-        
-        %w[INT TERM USR1].each do |signal|
-          Signal.trap(signal) do
-            begin
-              ::Process.kill("INT", -pid)
-              sleep 0.5
-              ::Process.kill("TERM", -pid)
-              sleep 0.5
-              ::Process.kill("KILL", -pid)
-            rescue
-              # process doesn't exist anymore
-            end
-          end
-        end
 
         [ io[0][0], io[1][1], io[2][1] ].each { |fd| fd.close }
 
@@ -166,6 +221,8 @@ module TextMate
         IO.blocksize = options[:granularity] if options[:granularity].is_a? Integer
         previous_sync = IO.sync
         IO.sync = true unless options[:granularity] == :line
+
+        setup_kill_handler(pid, &block)
 
         IO.exhaust(options[:watch_fds].merge(:out => io[1][0], :err => io[2][0]), &block)
         ::Process.waitpid(pid)

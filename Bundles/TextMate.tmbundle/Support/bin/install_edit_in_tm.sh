b923462d0fc902497bb997d730ff011e2cc24c7e
ciaran <ciaran@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-08-31 15:27:16 +0000

    Remove pre-Leopard install procedure from Edit in TextMate installer.
    Previously the install script would test the OS version and install to ~/Library/InputManagers for versions other than 10.5; the version test checked only against 10.5, rather than checking for 10.5+.
    
    
    git-svn-id: http://svn.textmate.org/trunk@11737 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/TextMate.tmbundle/Support/bin/install_edit_in_tm.sh
+++ Bundles/TextMate.tmbundle/Support/bin/install_edit_in_tm.sh
@@ -1,22 +1,14 @@
-SRC="$(ruby -r"$TM_SUPPORT_PATH/lib/textmate.rb" -e 'print TextMate.app_path')/Contents/Resources/Edit in TextMate"
+SRC="$TM_APP_PATH/Contents/Resources/Edit in TextMate"
 
 # check if we have the input manager in TextMate.app
-[[ ! -d "$SRC" ]] && { echo "<h2>Error</h2><p>The input manager (<tt>$SRC</tt>) was not found in the TextMate application bundle.<p>You may need a newer version of TextMate."; exit; }
+[[ ! -d "$SRC" ]] && { echo "<h2>Error</h2><p>The input manager (<tt>$SRC</tt>) was not found in the TextMate application bundle.</p><p>You may need a newer version of TextMate.</p>"; exit; }
 
-if [[ `defaults read /System/Library/CoreServices/SystemVersion ProductVersion` = 10.5* ]]; then
-	osascript 2>&1 <<-APPLESCRIPT
-		do shell script "mkdir -p /Library/InputManagers &&
-		cp -pR '$SRC' /Library/InputManagers &&
-		chown -R root:admin /Library/InputManagers &&
-		chmod -R go-w /Library/InputManagers/Edit\\\\ in\\\\ TextMate &&
-		echo '<strong>Success!</strong> <tt>$SRC</tt> copied to <tt>/Library/InputManagers</tt>' &&
-		[[ -d ~/Library/InputManagers ]] && mv ~/Library/InputManagers{,Disabled} && echo '<br>Your local directory (<tt>~/Library/InputManagers</tt>) has been renamed to <tt>~/Library/InputManagersDisabled</tt> to ensure there are no conflicts';
-		true; # Stops osascript outputting an error if ~/Library/InputManagers does not exist" with administrator privileges
-	APPLESCRIPT
-else
-	{
-		mkdir -p ~/Library/InputManagers &&
-		ln -s "$SRC" ~/Library/InputManagers &&
-		echo "<strong>Success!</strong> Link to <tt>$SRC</tt> created in <tt>~/Library/InputManagers</tt>.";
-	} 2>&1;
-fi
+osascript 2>&1 <<-APPLESCRIPT
+	do shell script "mkdir -p /Library/InputManagers &&
+	cp -pR '$SRC' /Library/InputManagers &&
+	chown -R root:admin /Library/InputManagers &&
+	chmod -R go-w /Library/InputManagers/Edit\\\\ in\\\\ TextMate &&
+	echo '<strong>Success!</strong> <tt>$SRC</tt> copied to <tt>/Library/InputManagers</tt>' &&
+	[[ -d ~/Library/InputManagers ]] && mv ~/Library/InputManagers{,Disabled} && echo '<br>Your local directory (<tt>~/Library/InputManagers</tt>) has been renamed to <tt>~/Library/InputManagersDisabled</tt> to ensure there are no conflicts';
+	true; # Stops osascript outputting an error if ~/Library/InputManagers does not exist" with administrator privileges
+APPLESCRIPT

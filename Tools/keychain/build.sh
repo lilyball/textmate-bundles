ed9fb2c25b3233c3365e06dca75bc632b322e6d4
ciaran <ciaran@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-09-02 11:52:02 +0000

    Add scripts to build the keychain and plist ruby extensions with 64-bit architectures.
    Since this requires multiple steps (building as both 32- and 64-bit and then folding together with lipo) I could not find a way to do this solely with ruby’s mkmf, so instead I’ve created a wrapper script which performs each step in turn.
    Prior to this commit the extconf files specified the 10.4u SDK, but due to the 10.4u CoreFoundation framework not containing 64-bit architectures I have removed this flag (so the bundles will be built against the user’s system version).
    The plist bundle has been built against the 10.5 SDK for some time now, apparently without issue from users on 10.4, so I think this required change is fine.
    
    
    git-svn-id: http://svn.textmate.org/trunk@11741 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Tools/keychain/build.sh
@@ -0,0 +1,7 @@
+TARGET=keychain
+rm -f *.bundle
+for M in 32 64; do
+	[ -f Makefile ] && make clean && rm Makefile
+	CFLAGS="-m$M" ruby ./extconf.rb && CFLAGS="-m$M" make && mv "${TARGET}.bundle" "${TARGET}_${M}.bundle" || ( echo "Failed to build ${M}-bit bundle" && exit 1 )
+done
+lipo -create "${TARGET}_32.bundle" "${TARGET}_64.bundle" -output "${TARGET}.bundle" && echo Done
--- Tools/plist/extconf.rb
+++ Tools/plist/extconf.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 require 'mkmf'
-newFlags = " -arch ppc -arch i386 -isysroot /Developer/SDKs/MacOSX10.4u.sdk"
+newFlags = " -arch ppc -arch i386 " + ENV['CFLAGS'].to_s
 $CFLAGS += newFlags
 $LDFLAGS += ' -framework CoreFoundation' + newFlags + ' -undefined suppress -flat_namespace'
 $LIBRUBYARG_SHARED=""

dd619a7ec3364f8cb1b55e36e7e2235196595fd6
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-11-16 06:08:29 +0000

    Moved to http://github.com/textmate/dialog
    git-svn-id: http://svn.textmate.org/trunk@11818 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/Dialog/Commands/Utilities/TextMate.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSObject (OakTextView)
-- (NSPoint)positionForWindowUnderCaret;
-@end
-
-void insert_text (NSString* someText);
-void insert_snippet (NSString* aSnippet);

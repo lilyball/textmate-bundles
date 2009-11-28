16ee1df0819065f712f3de45a334bb44115b372d
bibiko <bibiko@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2009-11-28 10:23:04 +0000

    • changed presets for the TM_Rdaemonxxx locale; now all default sys settings are respected. [ thanks to John Purnell ! ]
    git-svn-id: http://svn.textmate.org/trunk@11857 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/R Console (Rdaemon).tmbundle/Support/Rdaemon/daemon/startR.sh
+++ Bundles/R Console (Rdaemon).tmbundle/Support/Rdaemon/daemon/startR.sh
@@ -2,14 +2,14 @@
 
 # this starts the daemon
 cd "$HOME/Library/Application Support/Rdaemon"
-export LC_ALL=${TM_RdaemonLC_ALL:-en_US.UTF-8}
-export LC_LANG=${TM_RdaemonLC_LANG:-}
+export LC_ALL=${TM_RdaemonLC_ALL:-}
+export LC_LANG=${TM_RdaemonLC_LANG:-en_US.UTF-8}
 export LC_TIME=${TM_RdaemonLC_TIME:-}
 export LC_COLLATE=${TM_RdaemonLC_COLLATE:-}
-export LC_CTYPE=${TM_RdaemonLC_CTYPE:-en_US.UTF-8}
+export LC_CTYPE=${TM_RdaemonLC_CTYPE:-}
 export LC_MONETARY=${TM_RdaemonLC_MONETARY:-}
 export LC_NUMERIC=${TM_RdaemonLC_NUMERIC:-}
-export LC_MESSAGES=${TM_RdaemonLC_MESSAGES:-en_US.UTF-8}
+export LC_MESSAGES=${TM_RdaemonLC_MESSAGES:-}
 export LC_PAPER=${TM_RdaemonLC_PAPER:-}
 export LC_MEASUREMENT=${TM_RdaemonLC_MEASUREMENT:-}
 ruby "$HOME/Library/Application Support/Rdaemon"/daemon/Rdaemon.rb &> /dev/null &

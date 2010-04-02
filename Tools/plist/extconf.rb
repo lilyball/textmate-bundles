fe426046754d7cc1445d8f65d47a123f28916f49
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2010-04-02 15:15:20 +0000

    Build for 10.4 (G3).
    Without ‘-mmacosx-version-min=10.4’ gcc (4.x) will build for G4 when using ‘-arch ppc’.
    
    
    git-svn-id: http://svn.textmate.org/trunk@11923 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Tools/plist/extconf.rb
+++ Tools/plist/extconf.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 require 'mkmf'
-newFlags = " -arch ppc -arch i386 " + ENV['CFLAGS'].to_s
+newFlags = " -mmacosx-version-min=10.4 -arch ppc -arch i386 " + ENV['CFLAGS'].to_s
 $CFLAGS += newFlags
 $LDFLAGS += ' -framework CoreFoundation' + newFlags + ' -undefined suppress -flat_namespace'
 $LIBRUBYARG_SHARED=""

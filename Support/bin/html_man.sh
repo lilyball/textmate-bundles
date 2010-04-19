cd3c3d86d995551f60d93acb851dbfa64dcf235f
duff <duff@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2010-04-19 20:23:00 +0000

    Minor improvements to html man.
    * Use $TMPDIR when available.
    * Capture stderr from man2html so these are shown incase of error (not ideal, but better than blank page).
    * Remove the third legacy script path argument — this was required back when JS executions did not have the TM_SUPPORT_PATH environment variable.
    
    
    
    git-svn-id: http://svn.textmate.org/trunk@11929 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/bin/html_man.sh
+++ Support/bin/html_man.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-HTML_MAN=$0
 SECTION=$1
 WORD=$2
 
-DST=$(mktemp /tmp/tm_man_XXXXXX).html
-"$TM_SUPPORT_PATH/bin/man2html" >"$DST" "$SECTION" "$WORD" "$0"
+DST=$(mktemp "${TMPDIR:-/tmp}/tm_man_XXXXXX").html
+"$TM_SUPPORT_PATH/bin/man2html" &>"$DST" "$SECTION" "$WORD"
 echo -n "$DST"
 
 { sleep 300; rm "$DST"; rm "${DST%.html}"; } &>/dev/null &

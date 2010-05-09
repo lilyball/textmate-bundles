209e3ad9864709d30023946b22d2bc98201ae86b
msheets <msheets@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2010-05-09 17:03:36 +0000

    Moved to http://github.com/textmate/cpp-qt.tmbundle
    git-svn-id: http://svn.textmate.org/trunk@11937 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/C++ Qt.tmbundle/Support/lib/assistant_search/assistant_search.pro
+++
@@ -1,4 +0,0 @@
-TEMPLATE = app
-CONFIG += qt
-CONFIG -= app_bundle
-SOURCES += main.cpp
--- Bundles/C++ Qt.tmbundle/Support/lib/rails_bundle_tools.rb
+++
@@ -1,9 +0,0 @@
-bundle_lib = ENV['TM_BUNDLE_SUPPORT'] + '/lib'
-$LOAD_PATH.unshift(bundle_lib) if ENV['TM_BUNDLE_SUPPORT'] and !$LOAD_PATH.include?(bundle_lib)
-
-require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
-
-require 'rails/text_mate'
-require 'rails/unobtrusive_logger'
-require 'rails/misc'
-require 'camelcase'

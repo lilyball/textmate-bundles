cdb4280790d90fa39048c9473f3e3d411d741f5c
msheets <msheets@dfb7d73b-c2ec-0310-8fea-fb051d288c6d>
2010-11-19 23:05:23 +0000

    Remove usage of TM_RUBY.
    git-svn-id: http://svn.textmate.org/trunk@11975 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Bundles/Maven.tmbundle/Support/bin/mavenmate.sh
+++ Bundles/Maven.tmbundle/Support/bin/mavenmate.sh
@@ -2,15 +2,11 @@
 source "$TM_SUPPORT_PATH/lib/bash_init.sh"
 
 export RUBYLIB="$TM_BUNDLE_SUPPORT/lib:$RUBYLIB"
-TM_RUBY="${TM_RUBY:-ruby}"
 TM_MVN="${TM_MVN:-mvn}"
 
 require_cmd "$TM_MVN" "If you have installed maven, then you need to either <a href=\"help:anchor='search_path'%20bookID='TextMate%20Help'\">update your <tt>PATH</tt></a> or set the <tt>TM_MVN</tt> shell variable (e.g. in Preferences / Advanced)"
 
-require_cmd "$TM_RUBY" "We need Ruby to proceed."
-
 export TM_MVN=`which "$TM_MVN"`
-export TM_RUBY=`which "$TM_RUBY"`
 
 if [ "$TM_PROJECT_DIRECTORY" ]
 then
@@ -23,6 +19,6 @@ else
 fi
 
 
-$TM_RUBY  -- "$TM_BUNDLE_SUPPORT/lib/MavenMate.rb" -m "$TM_MVN" -l "$MVN_PROJECT" $@
+ruby  -- "$TM_BUNDLE_SUPPORT/lib/MavenMate.rb" -m "$TM_MVN" -l "$MVN_PROJECT" $@
 
 rescan_project
